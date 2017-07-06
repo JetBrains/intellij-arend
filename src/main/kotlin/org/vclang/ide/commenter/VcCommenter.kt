001dc2908b71c439eb3efb0e3eab23f82f77556d
mchernyavsky <chemike47@gmail.com>
2017-07-06 14:23:00 +0300

    Add commenter

---
+++ src/main/kotlin/org/vclang/ide/commenter/VcCommenter.kt
@@ -0,0 +1,15 @@
+package org.vclang.ide.commenter
+
+import com.intellij.lang.Commenter
+
+class VcCommenter : Commenter {
+    override fun getLineCommentPrefix(): String? = "--"
+
+    override fun getBlockCommentPrefix(): String? = "{-"
+
+    override fun getBlockCommentSuffix(): String? = "-}"
+
+    override fun getCommentedBlockCommentPrefix(): String? = null
+
+    override fun getCommentedBlockCommentSuffix(): String? = null
+}
