86835d29e9123b3f6a76ea5624c25d5ed1eea602
misha <chemike47@gmail.com>
2017-07-10 02:58:06 +0300

    Add an utility to search definitions

---
+++ src/main/kotlin/org/vclang/lang/core/psi/VcPsiUtil.kt
@@ -0,0 +1,27 @@
+package org.vclang.lang.core.psi
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.indexing.FileBasedIndex
+
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.FileTypeIndex
+import org.vclang.lang.VcFileType
+
+fun Project.findDefinitions(name: String): List<VcDefinition> =
+        findDefinitions().filter { it.name == name }
+
+fun Project.findDefinitions(): List<VcDefinition> {
+    val virtualFiles = FileBasedIndex.getInstance().getContainingFiles(
+            FileTypeIndex.NAME, VcFileType, GlobalSearchScope.allScope(this))
+    return virtualFiles
+            .map { PsiManager.getInstance(this).findFile(it) as VcFile? }
+            .filterNotNull()
+            .flatMap { it.children.asIterable() }
+            .map { it.lastChild }
+            .filterNotNull()
+            .map { PsiTreeUtil.getChildrenOfType(it, VcDefinition::class.java) }
+            .filterNotNull()
+            .flatMap { it.asIterable() }
+}
