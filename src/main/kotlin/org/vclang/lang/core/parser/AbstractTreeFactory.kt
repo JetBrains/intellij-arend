97a80bad0294e9cd8a3d87f2bdd2497a87b03748
mchernyavsky <chemike47@gmail.com>
2017-07-27 12:14:42 +0300

    Add AbstractTreeFactory

---
+++ src/main/kotlin/org/vclang/lang/core/parser/AbstractTreeFactory.kt
@@ -0,0 +1,58 @@
+package org.vclang.lang.core.parser
+
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.jetpad.vclang.error.CompositeErrorReporter
+import com.jetbrains.jetpad.vclang.error.CountingErrorReporter
+import com.jetbrains.jetpad.vclang.error.ErrorReporter
+import com.jetbrains.jetpad.vclang.frontend.namespace.ModuleRegistry
+import com.jetbrains.jetpad.vclang.frontend.resolving.OneshotNameResolver
+import com.jetbrains.jetpad.vclang.module.source.SourceId
+import com.jetbrains.jetpad.vclang.naming.NameResolver
+import com.jetbrains.jetpad.vclang.naming.scope.primitive.EmptyScope
+import com.jetbrains.jetpad.vclang.naming.scope.primitive.Scope
+import com.jetbrains.jetpad.vclang.term.Abstract
+import org.vclang.lang.core.Surrogate
+import org.vclang.lang.core.psi.VcStatements
+import org.vclang.lang.core.resolve.SurrogateResolveListener
+
+object AbstractTreeFactory {
+
+    fun createFromPsiFile(
+            sourceId: SourceId,
+            file: PsiFile,
+            errorReporter: ErrorReporter,
+            nameResolver: NameResolver? = null,
+            globalScope: Scope = EmptyScope(),
+            moduleRegistry: ModuleRegistry? = null
+    ): Abstract.ClassDefinition? {
+        val countingErrorReporter = CountingErrorReporter()
+        val compositeErrorReporter = CompositeErrorReporter(errorReporter, countingErrorReporter)
+
+        val tree = PsiTreeUtil.getChildOfType(file, VcStatements::class.java) ?: return null
+        val visitor = AbstractTreeBuildVisitor(sourceId, compositeErrorReporter)
+        val statements = visitor.visitStatements(tree)
+        val result = Surrogate.ClassDefinition(
+                Surrogate.Position(sourceId, 0, 0),
+                sourceId.modulePath.name,
+                statements
+        )
+
+        moduleRegistry?.registerModule(sourceId.modulePath, result)
+        if (nameResolver != null) {
+            OneshotNameResolver.visitModule(
+                    result,
+                    globalScope,
+                    nameResolver,
+                    Surrogate.NamespaceCommandStatement.GET,
+                    SurrogateResolveListener(compositeErrorReporter)
+            )
+        }
+        if (countingErrorReporter.errorsNumber > 0) {
+            moduleRegistry?.unregisterModule(sourceId.modulePath)
+            return null
+        }
+
+        return result
+    }
+}
