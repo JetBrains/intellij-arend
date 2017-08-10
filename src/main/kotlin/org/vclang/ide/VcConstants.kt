58f5d9e371f7c96404b2b68f133ecf0c98dcdf5e
mchernyavsky <chemike47@gmail.com>
2017-08-10 10:46:06 +0300

    Add constants for Vclang project layout

---
+++ src/main/kotlin/org/vclang/ide/VcConstants.kt
@@ -0,0 +1,9 @@
+package org.vclang.ide
+
+object VcConstants {
+    object ProjectLayout {
+        val sources = listOf("src")
+        val tests = listOf("test")
+        val cache = ".cache"
+    }
+}
