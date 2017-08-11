9f01793fd0e1e9d0cc31940e252cb868e0197e29
mchernyavsky <chemike47@gmail.com>
2017-08-11 13:43:14 +0300

    Add Typecheck line marker contributor

---
+++ src/main/kotlin/org/vclang/ide/typecheck/execution/lineMarker/TypecheckRunLineMarkerContributor.kt
@@ -0,0 +1,22 @@
+package org.vclang.ide.typecheck.execution.lineMarker
+
+import com.intellij.execution.lineMarker.ExecutorAction
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.icons.AllIcons
+import com.intellij.psi.PsiElement
+import com.intellij.util.Function
+import org.vclang.lang.core.parser.fullyQualifiedName
+import org.vclang.lang.core.psi.VcDefinition
+import org.vclang.lang.core.psi.VcIdentifier
+
+class TypecheckRunLineMarkerContributor : RunLineMarkerContributor() {
+    override fun getInfo(element: PsiElement): Info? {
+        val parent = element.parent
+        if (element !is VcIdentifier || parent !is VcDefinition) return null
+        return Info(
+                AllIcons.RunConfigurations.TestState.Run,
+                Function<PsiElement, String> { "Type check ${parent.fullyQualifiedName}" },
+                *ExecutorAction.getActions(1)
+        )
+    }
+}
