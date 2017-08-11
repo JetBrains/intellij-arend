14b2fcfbb1ac3b90e6d0bbe389436709ff28f176
mchernyavsky <chemike47@gmail.com>
2017-08-11 13:41:53 +0300

    Add runner for Typecheck run configuration

---
+++ src/main/kotlin/org/vclang/ide/typecheck/execution/runners/TypecheckRunner.kt
@@ -0,0 +1,14 @@
+package org.vclang.ide.typecheck.execution.runners
+
+import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.runners.DefaultProgramRunner
+import org.vclang.ide.typecheck.execution.configurations.TypecheckConfiguration
+
+class TypecheckRunner : DefaultProgramRunner() {
+
+    override fun canRun(executorId: String, profile: RunProfile): Boolean =
+            executorId == DefaultRunExecutor.EXECUTOR_ID && profile is TypecheckConfiguration
+
+    override fun getRunnerId(): String = "TypecheckRunner"
+}
