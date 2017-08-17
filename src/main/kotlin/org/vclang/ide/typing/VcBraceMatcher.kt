94e8079e91330771a69eb92e6e71b78c6b9296ce
mchernyavsky <chemike47@gmail.com>
2017-08-17 19:56:54 +0300

    Add brace matcher

---
+++ src/main/kotlin/org/vclang/ide/typing/VcBraceMatcher.kt
@@ -0,0 +1,37 @@
+package org.vclang.ide.typing
+
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+import org.vclang.lang.core.psi.VC_COMMENTS
+import org.vclang.lang.core.psi.VC_WHITE_SPACES
+import org.vclang.lang.core.psi.VcTypes.*
+
+class VcBraceMatcher : PairedBraceMatcher {
+
+    override fun getPairs() = PAIRS
+
+    override fun isPairedBracesAllowedBeforeType(
+            lbraceType: IElementType,
+            contextType: IElementType?
+    ): Boolean = contextType in InsertPairBraceBefore
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int =
+            openingBraceOffset
+
+    companion object {
+        private val PAIRS = arrayOf(
+                BracePair(LBRACE, RBRACE, true),
+                BracePair(LPAREN, RPAREN, false)
+        )
+
+        private val InsertPairBraceBefore = TokenSet.orSet(
+                VC_COMMENTS,
+                VC_WHITE_SPACES,
+                TokenSet.create(COMMA, RPAREN, LBRACE, RBRACE)
+        )
+    }
+}
