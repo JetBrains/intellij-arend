d70577ae117c1fe24920bf9bc1f962a3827b5548
mchernyavsky <chemike47@gmail.com>
2017-08-18 16:35:50 +0300

    Add declaration range handler

---
+++ src/main/kotlin/org/vclang/ide/hints/VcDeclarationRangeHandler.kt
@@ -0,0 +1,34 @@
+package org.vclang.ide.hints
+
+import com.intellij.codeInsight.hint.DeclarationRangeHandler
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import org.vclang.lang.core.psi.VcDefClass
+import org.vclang.lang.core.psi.VcDefData
+import org.vclang.lang.core.psi.VcDefFunction
+import org.vclang.lang.core.psi.VcDefInstance
+
+class VcDeclarationRangeHandler : DeclarationRangeHandler<PsiElement> {
+    override fun getDeclarationRange(container: PsiElement): TextRange {
+        val start = container.textRange.startOffset
+        return when (container) {
+            is VcDefClass -> {
+                val lastTele = container.teleList.lastOrNull()
+                TextRange(start, (lastTele ?: container.identifier)!!.textRange.endOffset)
+            }
+            is VcDefData -> {
+                val lastTele = container.teleList.lastOrNull()
+                TextRange(start, (lastTele ?: container.identifier)!!.textRange.endOffset)
+            }
+            is VcDefInstance -> {
+                val lastTele = container.teleList.lastOrNull()
+                TextRange(start, (lastTele ?: container.identifier)!!.textRange.endOffset)
+            }
+            is VcDefFunction -> {
+                val lastTele = container.teleList.lastOrNull()
+                TextRange(start, (lastTele ?: container.identifier)!!.textRange.endOffset)
+            }
+            else -> container.textRange
+        }
+    }
+}
