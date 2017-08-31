d0ae9104b011e95dc993407e26f1c00d07d54fed
mchernyavsky <chemike47@gmail.com>
2017-08-31 12:29:08 +0300

    Remove VcSketchyModuleNamespace

--- src/main/kotlin/org/vclang/lang/core/resolve/namespace/VcModuleNamespace.kt
+++ src/main/kotlin/org/vclang/lang/core/resolve/namespace/VcModuleNamespace.kt
@@ -1,12 +1,7 @@
 package org.vclang.lang.core.resolve.namespace
 
-import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiFileSystemItem
 import com.jetbrains.jetpad.vclang.naming.namespace.ModuleNamespace
 import com.jetbrains.jetpad.vclang.term.Abstract
-import org.vclang.lang.VcFileType
-import org.vclang.lang.VcLanguage
-import org.vclang.lang.core.psi.VcFile
 
 class VcModuleNamespace : ModuleNamespace {
     private val submoduleNamespaces = mutableMapOf<String, VcModuleNamespace>()
@@ -31,39 +26,4 @@ class VcModuleNamespace : ModuleNamespace {
 
     internal fun ensureSubmoduleNamespace(submodule: String): VcModuleNamespace =
             submoduleNamespaces.computeIfAbsent(submodule) { VcModuleNamespace() }
-}
-
-class VcSketchyModuleNamespace(
-        private val provider: VcSketchyModuleNamespaceProvider,
-        private val file: PsiFileSystemItem
-) : ModuleNamespace {
-
-    override fun getNames(): Set<String> {
-        val names = mutableSetOf<String>()
-        file.processChildren {
-            if (it.isDirectory) {
-                names.add(it.name)
-            } else if (it.language == VcLanguage) {
-                val name = it.name.substring(0, it.name.lastIndexOf('.'))
-                names.add(name)
-            }
-            true
-        }
-        return names
-    }
-
-    override fun getSubmoduleNamespace(submodule: String): ModuleNamespace? {
-        if (file is PsiDirectory) {
-            file.findSubdirectory(submodule)?.let {
-                return VcSketchyModuleNamespace(provider, it)
-            }
-            file.findFile("$submodule.${VcFileType.defaultExtension}")?.let {
-                return VcSketchyModuleNamespace(provider, it)
-            }
-        }
-        return null
-    }
-
-    override fun getRegisteredClass(): Abstract.ClassDefinition? =
-            if (file is VcFile && provider.isRegistered(file)) file else null
 }
--- src/main/kotlin/org/vclang/lang/core/resolve/namespace/VcModuleNamespaceProvider.kt
+++ src/main/kotlin/org/vclang/lang/core/resolve/namespace/VcModuleNamespaceProvider.kt
@@ -1,14 +1,10 @@
 package org.vclang.lang.core.resolve.namespace
 
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.psi.PsiManager
 import com.jetbrains.jetpad.vclang.frontend.namespace.ModuleRegistry
 import com.jetbrains.jetpad.vclang.module.ModulePath
 import com.jetbrains.jetpad.vclang.naming.namespace.ModuleNamespace
 import com.jetbrains.jetpad.vclang.naming.namespace.ModuleNamespaceProvider
 import com.jetbrains.jetpad.vclang.term.Abstract
-import org.vclang.lang.core.psi.VcFile
 
 class VcModuleNamespaceProvider : ModuleNamespaceProvider, ModuleRegistry {
     private val registered = mutableMapOf<Abstract.ClassDefinition, ModuleNamespace>()
@@ -58,57 +54,6 @@ class VcModuleNamespaceProvider : ModuleNamespaceProvider, ModuleRegistry {
             }
             val parentNamespace = ensureModuleNamespace(rootNamespace, modulePath.parent)
             return parentNamespace.ensureSubmoduleNamespace(modulePath.name)
-        }
-    }
-}
-
-class VcSketchyModuleNamespaceProvider(
-        project: Project,
-        sourceDir: String
-) : ModuleNamespaceProvider {
-    private var root: WithPreludeModuleNamespace
-    private val initializedModules = mutableSetOf<Abstract.Definition>()
-
-    init {
-        val virtualFile = LocalFileSystem.getInstance().findFileByPath(sourceDir)
-        val psiFile = virtualFile?.let { PsiManager.getInstance(project).findDirectory(it) }
-        val rootModuleNamespace = psiFile?.let { VcSketchyModuleNamespace(this, psiFile) }
-        root = WithPreludeModuleNamespace(rootModuleNamespace)
-    }
-
-    override fun forModule(definition: Abstract.ClassDefinition): ModuleNamespace? =
-            (definition as? VcFile)?.let { VcSketchyModuleNamespace(this, it) }
-
-    override fun root(): ModuleNamespace? = root
-
-    fun registerModule(module: Abstract.ClassDefinition) = initializedModules.add(module)
-
-    fun unregisterModule(module: Abstract.ClassDefinition) = initializedModules.remove(module)
-
-    fun unregisterAllModules() = initializedModules.clear()
-
-    fun isRegistered(module: Abstract.ClassDefinition) = initializedModules.contains(module)
-
-    fun setPrelude(prelude: VcFile) {
-        root.prelude = VcSketchyModuleNamespace(this, prelude)
-        initializedModules.add(prelude)
-    }
-
-    private class WithPreludeModuleNamespace(
-            var module: ModuleNamespace? = null,
-            var prelude: ModuleNamespace? = null
-    ) : ModuleNamespace {
-
-        override fun getRegisteredClass(): Abstract.ClassDefinition? = module?.registeredClass
-
-        override fun getNames(): Set<String> = (module?.names ?: emptySet()) + "Prelude"
-
-        override fun getSubmoduleNamespace(submodule: String?): ModuleNamespace? {
-            return if (submodule == "Prelude") {
-                prelude
-            } else {
-                module?.getSubmoduleNamespace(submodule)
-            }
         }
     }
 }
