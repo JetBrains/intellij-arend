01dc12c0f3b915d3ba27f81653e9858fc8a34d4c
valis <valery.isaev@gmail.com>
2017-10-20 01:00:25 +0300

    Fix references in ClassFieldImpl

--- src/main/kotlin/org/vclang/psi/ext/VcImplementStatementImplMixin.kt
+++ src/main/kotlin/org/vclang/psi/ext/VcImplementStatementImplMixin.kt
@@ -1,7 +1,6 @@
 package org.vclang.psi.ext
 
 import com.intellij.lang.ASTNode
-import com.jetbrains.jetpad.vclang.naming.reference.NamedUnresolvedReference
 import com.jetbrains.jetpad.vclang.naming.reference.Referable
 import com.jetbrains.jetpad.vclang.term.abs.Abstract
 import org.vclang.psi.VcImplementStatement
@@ -10,10 +9,7 @@ import org.vclang.psi.VcImplementStatement
 abstract class VcImplementStatementImplMixin(node: ASTNode) : VcCompositeElementImpl(node), VcImplementStatement {
     override fun getData(): VcImplementStatementImplMixin = this
 
-    override fun getImplementedField(): Referable {
-        val ref = refIdentifier
-        return NamedUnresolvedReference(ref, ref.text)
-    }
+    override fun getImplementedField(): Referable = longName.referent
 
     override fun getImplementation(): Abstract.Expression? = expr
 }
