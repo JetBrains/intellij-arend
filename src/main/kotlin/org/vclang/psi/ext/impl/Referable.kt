9100d14455815bc3e6a14fe8a9ebc435e9a90390
valis <valery.isaev@gmail.com>
2017-11-14 02:02:48 +0300

    Add getTypecheckable method to GlobalReferable

--- src/main/kotlin/org/vclang/psi/ext/impl/Referable.kt
+++ src/main/kotlin/org/vclang/psi/ext/impl/Referable.kt
@@ -3,8 +3,11 @@ package org.vclang.psi.ext.impl
 import com.intellij.lang.ASTNode
 import com.intellij.psi.stubs.IStubElementType
 import com.intellij.psi.stubs.StubElement
+import com.jetbrains.jetpad.vclang.naming.reference.GlobalReferable
 import com.jetbrains.jetpad.vclang.term.Precedence
+import org.vclang.psi.VcDefinition
 import org.vclang.psi.VcPrec
+import org.vclang.psi.ancestors
 import org.vclang.psi.ext.PsiConcreteReferable
 import org.vclang.psi.ext.PsiStubbedReferableImpl
 import org.vclang.psi.stubs.VcNamedStub
@@ -16,6 +19,8 @@ where StubT : VcNamedStub, StubT : StubElement<*> {
     constructor(stub: StubT, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
 
     override fun getPrecedence(): Precedence = Precedence.DEFAULT
+
+    override fun getTypecheckable(): GlobalReferable = ancestors.filterIsInstance<VcDefinition>().firstOrNull() ?: this
 
     companion object {
         fun calcPrecedence(prec: VcPrec?): Precedence {
