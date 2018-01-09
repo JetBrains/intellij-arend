2976febf6b0b1122c29b45decb4f2fd1d23e14d4
sxhya <sxh@ya.ru>
2018-01-09 11:33:52 +0300

    Implemented printing of function header in tooltips

--- src/main/kotlin/org/vclang/VcDocumentationProvider.kt
+++ src/main/kotlin/org/vclang/VcDocumentationProvider.kt
@@ -2,6 +2,10 @@ package org.vclang
 
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.psi.PsiElement
+import com.jetbrains.jetpad.vclang.error.ListErrorReporter
+import com.jetbrains.jetpad.vclang.term.Precedence
+import com.jetbrains.jetpad.vclang.term.concrete.Concrete
+import com.jetbrains.jetpad.vclang.term.prettyprint.PrettyPrintVisitor
 import org.vclang.psi.*
 import org.vclang.psi.ext.PsiGlobalReferable
 
@@ -12,6 +16,9 @@ class VcDocumentationProvider : AbstractDocumentationProvider() {
             buildString {
                 getType(element)?.let { append("<b>$it</b> ") }
                 append(element.textRepresentation())
+                when (element) {
+                    is VcDefFunction -> append (getDefFunctionInfo(element))
+                }
                 element.getContainingFile().originalFile.let {
                     append(" <i>defined in</i> ")
                     append((it as? VcFile)?.fullName ?: it.name)
@@ -20,6 +27,27 @@ class VcDocumentationProvider : AbstractDocumentationProvider() {
         } else {
             null
         }
+    }
+
+    fun getDefFunctionInfo(element : VcDefFunction) : String? {
+        val reporter = ListErrorReporter()
+        val concreteElement = element.computeConcrete(reporter)
+        val builder = StringBuilder()
+        if (reporter.errorList.isEmpty()) {
+            when (concreteElement) {
+                is Concrete.FunctionDefinition -> {
+                    builder.append(' ')
+                    val printer = PrettyPrintVisitor(builder, 0, false)
+                    printer.prettyPrintParameters(concreteElement.parameters, 0)
+                    val resultType = concreteElement.resultType
+                    if (resultType != null) {
+                        builder.append(": ")
+                        resultType.accept(printer, Precedence(0))
+                    }
+                }
+            }
+        }
+        return builder.toString()
     }
 
     override fun getQuickNavigateInfo(element: PsiElement, originalElement: PsiElement?): String? =
