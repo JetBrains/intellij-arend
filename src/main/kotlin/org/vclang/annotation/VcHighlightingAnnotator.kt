8998d334df51d0fb9549d08ae873696d72d1e074
sxhya <sxh@ya.ru>
2018-03-06 18:27:51 +0300

    Added highlighting of nameresolver errors

--- src/main/kotlin/org/vclang/annotation/VcHighlightingAnnotator.kt
+++ src/main/kotlin/org/vclang/annotation/VcHighlightingAnnotator.kt
@@ -1,5 +1,6 @@
 package org.vclang.annotation
 
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.psi.PsiElement
@@ -7,14 +8,26 @@ import org.vclang.highlight.VcHighlightingColors
 import org.vclang.psi.VcDefIdentifier
 import org.vclang.psi.VcInfixArgument
 import org.vclang.psi.VcPostfixArgument
+import org.vclang.psi.ext.VcCompositeElement
 
 class VcHighlightingAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        if (element is VcCompositeElement) {
+            val reference = element.reference
+            if (reference != null) {
+                val psiElement = reference.resolve()
+                if (psiElement == null) {
+                    holder.createErrorAnnotation(element, "Unresolved reference").highlightType = ProblemHighlightType.ERROR
+                }
+            }
+        }
+
         val color = when (element) {
             is VcDefIdentifier -> VcHighlightingColors.DECLARATION
             is VcInfixArgument, is VcPostfixArgument -> VcHighlightingColors.OPERATORS
             else -> return
         }
+
 
         holder.createInfoAnnotation(element, null).textAttributes = color.textAttributesKey
     }
