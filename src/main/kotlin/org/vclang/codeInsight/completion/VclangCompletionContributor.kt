a50b2674e02a4debd78e4f99b97dfff439af7b36
sxhya <sxh@ya.ru>
2018-05-14 22:17:26 +0300

    Added fixity completion for data constructors, class fields and class field synonyms

--- src/main/kotlin/org/vclang/codeInsight/completion/VclangCompletionContributor.kt
+++ src/main/kotlin/org/vclang/codeInsight/completion/VclangCompletionContributor.kt
@@ -5,9 +5,11 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.TextRange
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.StandardPatterns.and
 import com.intellij.patterns.StandardPatterns.or
 import com.intellij.psi.tree.IElementType
 import com.intellij.util.ProcessingContext
+import org.vclang.psi.*
 import org.vclang.psi.VcElementTypes.*
 
 class VclangCompletionContributor: CompletionContributor() {
@@ -19,21 +21,28 @@ class VclangCompletionContributor: CompletionContributor() {
     companion object {
         val PREC_CONTEXT = or(PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(FUNCTION_KW)),
                 PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(DATA_KW)),
-                PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(CLASS_KW))) //TODO: Add support for class constructors
+                PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(CLASS_KW)),
+                PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(AS_KW)),
+                and(PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(PIPE)),
+                        or(PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcConstructor::class.java)),
+                           PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcDataBody::class.java)))), //simple data type constructor
+                and(PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(FAT_ARROW)),
+                        or(PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcConstructor::class.java)),
+                           PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcConstructorClause::class.java)))), //data type constructors with patterns
+                and(PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(PIPE)),
+                        or(PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcClassField::class.java)),
+                           PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcClassStat::class.java)))), //class field
+                and(PlatformPatterns.psiElement().afterLeaf(PlatformPatterns.psiElement(FAT_ARROW)),
+                        PlatformPatterns.psiElement().withSuperParent(2, PlatformPatterns.psiElement(VcClassFieldSyn::class.java))) //class field synonym
+        )
     }
 
     class KeywordCompletionProvider(private val keywords : List<IElementType>) : CompletionProvider<CompletionParameters>() {
         override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext?, result: CompletionResultSet) {
             val insertHandler = InsertHandler<LookupElement> { insertContext, _ ->
                 val document = insertContext.document
-                val startOffset = insertContext.startOffset
-                var tailOffset = insertContext.tailOffset
 
-                if (startOffset > 0 && document.getText(TextRange(startOffset-1, startOffset+1)) == "\\\\") {
-                    document.deleteString(startOffset-1, startOffset) //fix double backslash
-                    tailOffset--
-                }
-
+                val tailOffset = insertContext.tailOffset
                 if (tailOffset < document.textLength && !Character.isWhitespace(document.getText(TextRange(tailOffset, tailOffset+1))[0])) {
                     document.insertString(tailOffset, " ") // add tail whitespace
                 }
@@ -41,8 +50,12 @@ class VclangCompletionContributor: CompletionContributor() {
                 insertContext.commitDocument()
             }
 
+            val nonEmptyPrefix = result.prefixMatcher.prefix.isNotEmpty() ||
+                                 parameters.offset > 0 && parameters.originalFile.text.substring(parameters.offset - 1, parameters.offset) == "\\" //prefix consists of single slash character
+
             for (keyword in keywords)
-                result.addElement(LookupElementBuilder.create(keyword.toString()).bold().withInsertHandler(insertHandler))
+                result.withPrefixMatcher(PlainPrefixMatcher(if (nonEmptyPrefix) "\\"+result.prefixMatcher.prefix else "")).addElement(LookupElementBuilder.create(keyword.toString()).bold().withInsertHandler(insertHandler))
+
         }
     }
 }
