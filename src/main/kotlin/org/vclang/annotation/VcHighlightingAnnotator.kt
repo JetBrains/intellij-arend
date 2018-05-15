5d1264f3e7eb55f0cb7c1f663094162f7d60bcc9
valis <valery.isaev@gmail.com>
2018-05-15 17:14:15 +0300

    Implement some checks for name clashes

--- src/main/kotlin/org/vclang/annotation/VcHighlightingAnnotator.kt
+++ src/main/kotlin/org/vclang/annotation/VcHighlightingAnnotator.kt
@@ -3,8 +3,13 @@ package org.vclang.annotation
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.jetbrains.jetpad.vclang.error.Error
+import com.jetbrains.jetpad.vclang.naming.reference.LocatedReferable
+import com.jetbrains.jetpad.vclang.naming.resolving.DuplicateNameChecker
+import com.jetbrains.jetpad.vclang.term.group.Group
 import org.vclang.highlight.VcHighlightingColors
 import org.vclang.psi.*
 import org.vclang.psi.ext.VcReferenceElement
@@ -22,6 +27,16 @@ class VcHighlightingAnnotator : Annotator {
             }
         }
 
+        if (element is Group) {
+            object : DuplicateNameChecker() {
+                override fun duplicateName(ref1: LocatedReferable, ref2: LocatedReferable, level: Error.Level) {
+                    annotateDuplicateName(ref1, level, holder)
+                    annotateDuplicateName(ref2, level, holder)
+                }
+            }.checkGroup(element)
+            return
+        }
+
         val color = when {
             element is VcDefIdentifier -> VcHighlightingColors.DECLARATION
             element is VcInfixArgument || element is VcPostfixArgument -> VcHighlightingColors.OPERATORS
@@ -29,13 +44,27 @@ class VcHighlightingAnnotator : Annotator {
                 val parent = element.parent as? VcLongName ?: return
                 if (parent.parent is VcStatCmd) return
                 val refList = parent.refIdentifierList
-                if (refList.indexOf(element) == refList.size - 1) return
+                if (!refList.isEmpty() && refList.last() == element) return
                 VcHighlightingColors.LONG_NAME
             }
             else -> return
         }
 
-
         holder.createInfoAnnotation(element, null).textAttributes = color.textAttributesKey
+    }
+
+    private fun annotateDuplicateName(ref: LocatedReferable, level: Error.Level, holder: AnnotationHolder) {
+        var psiRef = ref as? PsiElement ?: return
+        if (psiRef is VcDefinition) {
+            psiRef = psiRef.children.first { it is VcDefIdentifier } ?: psiRef
+        }
+        holder.createAnnotation(levelToSeverity(level), psiRef.textRange, "Duplicate definition name")
+    }
+
+    private fun levelToSeverity(level: Error.Level) = when (level) {
+        Error.Level.ERROR -> HighlightSeverity.ERROR
+        Error.Level.WARNING -> HighlightSeverity.WARNING
+        Error.Level.GOAL -> HighlightSeverity.WEAK_WARNING
+        Error.Level.INFO -> HighlightSeverity.INFORMATION
     }
 }
