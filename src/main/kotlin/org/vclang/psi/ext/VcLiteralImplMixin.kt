dab8ee78f6fe33ef8d8c5d9ac2fde96518ff878c
valis <valery.isaev@gmail.com>
2018-09-07 20:11:20 +0300

    Fix a bug with error messages

--- src/main/kotlin/org/vclang/psi/ext/VcLiteralImplMixin.kt
+++ src/main/kotlin/org/vclang/psi/ext/VcLiteralImplMixin.kt
@@ -7,10 +7,18 @@ import org.vclang.psi.VcLiteral
 
 abstract class VcLiteralImplMixin(node: ASTNode) : VcExprImplMixin(node), VcLiteral {
     override fun <P : Any?, R : Any?> accept(visitor: AbstractExpressionVisitor<in P, out R>, params: P?): R {
-        longName?.let { return visitor.visitReference(it, it.referent, null, null, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params) }
-        propKw?.let { return visitor.visitUniverse(it, 0, -1, null, null, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params) }
-        underscore?.let { return visitor.visitInferHole(it, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params) }
-        goal?.let { return visitor.visitGoal(it, it.defIdentifier?.textRepresentation(), it.expr, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params) }
+        longName?.let {
+            return visitor.visitReference(it, it.referent, null, null, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params)
+        }
+        if (propKw != null) {
+            return visitor.visitUniverse(this, 0, -1, null, null, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params)
+        }
+        if (underscore != null) {
+            return visitor.visitInferHole(this, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params)
+        }
+        goal?.let {
+            return visitor.visitGoal(it, it.defIdentifier?.textRepresentation(), it.expr, if (visitor.visitErrors()) org.vclang.psi.ext.getErrorData(this) else null, params)
+        }
         error("Incorrect expression: literal")
     }
 }
