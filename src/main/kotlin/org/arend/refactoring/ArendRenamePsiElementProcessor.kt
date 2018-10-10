5cb682d2fd109ecbd6beba4888d79ec91fe5a991
part-xx <fedor.part@gmail.com>
2018-10-10 16:30:38 +0300

    Fix renaming for Arend files

---
+++ src/main/kotlin/org/arend/refactoring/ArendRenamePsiElementProcessor.kt
@@ -0,0 +1,46 @@
+package org.arend.refactoring
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.options.ConfigurationException
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.rename.RenameDialog
+import com.intellij.refactoring.rename.RenamePsiElementProcessor
+import com.intellij.usageView.UsageViewUtil
+import org.arend.psi.ArendFile
+import org.arend.util.FileUtils
+
+class ArendRenamePsiElementProcessor: RenamePsiElementProcessor() {
+    override fun canProcessElement(element: PsiElement): Boolean {
+        return element is ArendFile
+    }
+
+    override fun createRenameDialog(project: Project, element: PsiElement, nameSuggestionContext: PsiElement?, editor: Editor?): RenameDialog {
+        return ArendRenameDialog(project, element, nameSuggestionContext, editor)
+    }
+
+    class ArendRenameDialog(project: Project, element: PsiElement, nameSuggestionContext: PsiElement?, editor: Editor?): RenameDialog(project, element, nameSuggestionContext, editor) {
+        override fun getSuggestedNames(): Array<String> {
+            return arrayOf(UsageViewUtil.getShortName(psiElement).substringBefore("."))
+        }
+
+        override fun getNewName(): String = patchedGetNewName() + ".ard"
+
+
+        private fun patchedGetNewName(): String = nameSuggestionsField.enteredName.trim()
+
+        override fun canRun() {
+            try {
+                super.canRun()
+            } catch (e: ConfigurationException) {
+                if (e.message != null) {
+                    throw ConfigurationException("'" + patchedGetNewName() + "'" + "is not a valid Arend module name")
+                }
+            }
+        }
+
+        override fun areButtonsValid(): Boolean {
+            return FileUtils.isModuleName(patchedGetNewName())
+        }
+    }
+}
