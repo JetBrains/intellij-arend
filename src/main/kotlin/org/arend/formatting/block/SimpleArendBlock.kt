57dd1a5c64fa184d7bfb90f82dbba4158348c359
sxhya <sxh@ya.ru>
2018-10-29 18:56:02 +0300

    Improved formatter

--- src/main/kotlin/org/arend/formatting/block/SimpleArendBlock.kt
+++ src/main/kotlin/org/arend/formatting/block/SimpleArendBlock.kt
@@ -27,7 +27,8 @@ class SimpleArendBlock(node: ASTNode, wrap: Wrap?, alignment: Alignment?, myInde
     override fun buildChildren(): MutableList<Block> {
         val blocks = ArrayList<Block>()
         var child: ASTNode? = myNode.firstChildNode
-        val pipeAlignment = Alignment.createAlignment()
+        val alignment = Alignment.createAlignment()
+        val alignment2 = Alignment.createAlignment()
 
         while (child != null) {
             if (child.elementType != WHITE_SPACE) {
@@ -37,9 +38,11 @@ class SimpleArendBlock(node: ASTNode, wrap: Wrap?, alignment: Alignment?, myInde
                         child.elementType == STATEMENT ||
                         child.elementType == CONSTRUCTOR_CLAUSE ||
                         child.elementType == FUNCTION_BODY && childPsi is ArendFunctionBody && childPsi.expr != null ||
-                        myNode.elementType == CO_CLAUSE && childPsi is ArendExpr)
+                        myNode.elementType == CO_CLAUSE && childPsi is ArendExpr ||
+                        child.psi is ArendWhere ||
+                        myNode.elementType == LET_EXPR && child.psi is ArendExpr)
                     Indent.getNormalIndent() else
-                if (child.elementType == ATOM_ARGUMENT ||
+                if (myNode.elementType == ARGUMENT_APP_EXPR ||
                         child.elementType == TUPLE_EXPR ||
                         childPsi is ArendExpr && (myNode.elementType == PI_EXPR ||
                                 myNode.elementType == SIGMA_EXPR ||
@@ -47,18 +50,21 @@ class SimpleArendBlock(node: ASTNode, wrap: Wrap?, alignment: Alignment?, myInde
                     Indent.getContinuationIndent() else
                     Indent.getNoneIndent()
 
-                val wrap = if (myNode.elementType == FUNCTION_BODY) Wrap.createWrap(WrapType.NORMAL, true) else null
+                val wrap = if (child.elementType == FUNCTION_BODY) Wrap.createWrap(WrapType.NORMAL, true) else null
 
                 if ((myNode.elementType == FUNCTION_CLAUSES || myNode.elementType == LET_EXPR) && child.elementType == PIPE) {
                     val clauseGroup = findClauseGroup(child)
                     if (clauseGroup != null) {
                         child = clauseGroup.first.treeNext
-                        blocks.add(GroupBlock(myNode, clauseGroup.second, wrap, pipeAlignment, Indent.getNormalIndent()))
+                        blocks.add(GroupBlock(myNode, clauseGroup.second, wrap, alignment, Indent.getNormalIndent()))
                         continue
                     }
                 }
 
-                val block = SimpleArendBlock(child, wrap, null, indent)
+                val align = if (myNode.elementType == LET_EXPR && (child.elementType == LET_KW || child.elementType == IN_KW))
+                    alignment2 else null
+
+                val block = SimpleArendBlock(child, wrap, align, indent)
                 blocks.add(block)
 
             }
