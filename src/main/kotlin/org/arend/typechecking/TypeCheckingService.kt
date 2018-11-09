c271d93d0781be1097e310243ac321fe86ad1cbe
valis <valery.isaev@gmail.com>
2018-11-09 15:38:50 +0300

    Do not update definitions after modifications of a \where block

--- src/main/kotlin/org/arend/typechecking/TypeCheckingService.kt
+++ src/main/kotlin/org/arend/typechecking/TypeCheckingService.kt
@@ -24,7 +24,7 @@ import org.arend.naming.reference.converter.SimpleReferableConverter
 import org.arend.psi.ArendDefinition
 import org.arend.psi.ArendElementTypes
 import org.arend.psi.ArendFile
-import org.arend.psi.ancestors
+import org.arend.psi.ArendWhere
 import org.arend.psi.ext.ArendCompositeElement
 import org.arend.psi.ext.impl.DataDefinitionAdapter
 import org.arend.resolving.ArendReferableConverter
@@ -196,6 +196,7 @@ class TypeCheckingServiceImpl(override val project: Project) : TypeCheckingServi
             val child = event.child
             if (child is PsiErrorElement ||
                 child is PsiWhiteSpace ||
+                child is ArendWhere ||
                 child is LeafPsiElement && isComment(child.node.elementType)) {
                 return
             }
@@ -203,6 +204,7 @@ class TypeCheckingServiceImpl(override val project: Project) : TypeCheckingServi
             val newChild = event.newChild
             if (oldChild is PsiErrorElement && newChild is PsiErrorElement ||
                 oldChild is PsiWhiteSpace && newChild is PsiWhiteSpace ||
+                oldChild is ArendWhere && newChild is ArendWhere ||
                 oldChild is LeafPsiElement && isComment(oldChild.node.elementType) && newChild is LeafPsiElement && isComment(newChild.node.elementType)) {
                 return
             }
@@ -224,9 +226,17 @@ class TypeCheckingServiceImpl(override val project: Project) : TypeCheckingServi
                 }
             }
 
-            val ancestors = event.parent.ancestors
-            val definition = ancestors.filterIsInstance<ArendDefinition>().firstOrNull() ?: return
-            updateDefinition(definition)
+            var elem = event.parent
+            while (elem != null) {
+                if (elem is ArendWhere || elem is ArendFile) {
+                    return
+                }
+                if (elem is ArendDefinition) {
+                    updateDefinition(elem)
+                    return
+                }
+                elem = elem.parent
+            }
         }
 
         private fun invalidateChild(element : PsiElement) {
