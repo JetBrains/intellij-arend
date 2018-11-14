7c166f4ba80dc8490342bb2732bbd0ab211dd79d
Valery Isaev <valery.isaev@jetbrains.com>
2018-11-15 02:43:13 +0300

    Fix warnings

--- src/main/kotlin/org/arend/formatting/ArendEnterAtIndentHandler.kt
+++ src/main/kotlin/org/arend/formatting/ArendEnterAtIndentHandler.kt
@@ -4,7 +4,6 @@ import com.intellij.codeInsight.editorActions.BackspaceHandler
 import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegate
 import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegateAdapter
 import com.intellij.injected.editor.EditorWindow
-import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil
 import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
 import com.intellij.openapi.actionSystem.DataContext
@@ -13,16 +12,14 @@ import com.intellij.psi.PsiFile
 import com.intellij.openapi.util.Ref
 import org.arend.psi.ArendFile
 
-class ArendEnterAtIndentHandler() : EnterHandlerDelegateAdapter() {
-    override fun preprocessEnter(file: PsiFile, editor: Editor, caretOffset: Ref<Int>, caretAdvance: Ref<Int>, dataContext: DataContext, originalHandler: EditorActionHandler?): EnterHandlerDelegate.Result {
-        var file = file
-        var editor = editor
-        var offset = caretOffset.get()
+class ArendEnterAtIndentHandler : EnterHandlerDelegateAdapter() {
+    override fun preprocessEnter(origFile: PsiFile, origEditor: Editor, caretOffset: Ref<Int>, caretAdvance: Ref<Int>, dataContext: DataContext, originalHandler: EditorActionHandler?): EnterHandlerDelegate.Result {
+        var file = origFile
+        var editor = origEditor
 
         if (editor is EditorWindow) {
             file = InjectedLanguageManager.getInstance(file.project).getTopLevelFile(file)
-            editor = InjectedLanguageUtil.getTopLevelEditor(editor)
-            offset = editor.caretModel.offset
+            editor = editor.delegate
         }
         if (file !is ArendFile) {
             return EnterHandlerDelegate.Result.Continue
