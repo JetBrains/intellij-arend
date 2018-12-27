106d33d53069f310e4381ec095de43bc2ea493f6
Valery Isaev <valery.isaev@jetbrains.com>
2018-12-27 23:21:12 +0300

    Fix another bug with \case

--- src/main/kotlin/org/arend/psi/ext/ArendCaseExprImplMixin.kt
+++ src/main/kotlin/org/arend/psi/ext/ArendCaseExprImplMixin.kt
@@ -1,18 +1,19 @@
 package org.arend.psi.ext
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.util.PsiTreeUtil
 import org.arend.term.abs.Abstract
 import org.arend.term.abs.AbstractExpressionVisitor
 import org.arend.psi.ArendCaseArg
 import org.arend.psi.ArendCaseExpr
+import org.arend.psi.ArendExpr
 
 
 abstract class ArendCaseExprImplMixin(node: ASTNode) : ArendExprImplMixin(node), ArendCaseExpr, Abstract.CaseArgumentsHolder {
     override fun getCaseArguments(): List<ArendCaseArg> = caseArgList
 
     override fun <P : Any?, R : Any?> accept(visitor: AbstractExpressionVisitor<in P, out R>, params: P?): R {
-        val returnExpr = returnExpr
-        val returnExprs = returnExpr?.atomFieldsAccList ?: returnExpr?.expr?.let { listOf(it) } ?: emptyList()
+        val returnExprs = PsiTreeUtil.getChildrenOfTypeAsList(returnExpr, ArendExpr::class.java)
         return visitor.visitCase(this, caseArgList, returnExprs.firstOrNull(), returnExprs.getOrNull(1), clauseList, if (visitor.visitErrors()) org.arend.psi.ext.getErrorData(this) else null, params)
     }
 }
