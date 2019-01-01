43636591eae6215badaa11d4726e09403146a8ed
Valery Isaev <valery.isaev@jetbrains.com>
2019-01-01 23:11:23 +0300

    Improve text selectioner

--- src/main/kotlin/org/arend/ArendImplementationTextSelectioner.kt
+++ src/main/kotlin/org/arend/ArendImplementationTextSelectioner.kt
@@ -2,8 +2,11 @@ package org.arend
 
 import com.intellij.codeInsight.hint.ImplementationTextSelectioner
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.PsiTreeUtil
 import org.arend.psi.ArendDefIdentifier
 import org.arend.psi.ArendDefinition
+import org.arend.psi.ArendWhere
 
 class ArendImplementationTextSelectioner : ImplementationTextSelectioner {
     override fun getTextStartOffset(element: PsiElement): Int {
@@ -20,7 +23,11 @@ class ArendImplementationTextSelectioner : ImplementationTextSelectioner {
         if (element is ArendDefIdentifier) {
             val parent = element.parent
             if (parent is ArendDefinition) {
-                return parent.textRange.endOffset
+                var elem = PsiTreeUtil.getChildOfType(parent, ArendWhere::class.java)?.prevSibling
+                while (elem is PsiWhiteSpace) {
+                    elem = elem.prevSibling ?: break
+                }
+                return (elem ?: parent).textRange.endOffset
             }
         }
         return element.textRange.endOffset
