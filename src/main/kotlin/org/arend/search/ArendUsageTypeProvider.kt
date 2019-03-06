29f37b2386f3846e553df7701be44660d8791ac3
Valery Isaev <valery.isaev@jetbrains.com>
2019-03-06 04:46:19 +0300

    Classify more usage types

--- src/main/kotlin/org/arend/search/ArendUsageTypeProvider.kt
+++ src/main/kotlin/org/arend/search/ArendUsageTypeProvider.kt
@@ -11,22 +11,84 @@ class ArendUsageTypeProvider: UsageTypeProviderEx {
 
     override fun getUsageType(element: PsiElement?): UsageType? {
         val parent = element?.parent
-        val grandparent = element?.parent?.parent
 
-        if (parent is ArendLongName && grandparent is ArendStatCmd) return nsUsage
-        if (parent is ArendStatCmd || (parent is ArendNsId && grandparent is ArendNsUsing)) return nsUsageInList
-        if (parent is ArendLongName && element.rightSiblings.filterIsInstance<ArendRefIdentifier>().firstOrNull() != null) return leftUsage
-        if (parent is ArendLongName && grandparent is ArendCoClause) return usagesInCoClauses
-        if (parent is ArendPattern) return usagesInPatterns
+        when {
+            parent is ArendStatCmd || (parent is ArendNsId && parent.parent is ArendNsUsing) -> return nsUsageInList
+            parent is ArendPattern || parent is ArendAtomPatternOrPrefix -> return usagesInPatterns
+        }
+
+        if (parent !is ArendLongName) {
+            return defaultUsage
+        }
+
+        val pParent = parent.parent
+        when {
+            pParent is ArendStatCmd -> return nsUsage
+            pParent is ArendCoClause -> return usagesInCoClauses
+            pParent is ArendDefClass -> return extendsUsages
+            element.rightSiblings.filterIsInstance<ArendRefIdentifier>().firstOrNull() != null -> return leftUsage
+        }
+
+        var expr: ArendExpr = ((pParent as? ArendLongNameExpr)?.parent as? ArendArgumentAppExpr)?.parent as? ArendNewExpr ?: pParent as? ArendLiteral ?: return defaultUsage
+        if (((expr as? ArendLiteral)?.parent as? ArendTypeTele)?.parent is ArendDefData) {
+            return parameters
+        }
+
+        while (true) {
+            val atomFieldsAcc = (expr.parent as? ArendAtom)?.parent as? ArendAtomFieldsAcc
+            val atomParent = atomFieldsAcc?.parent
+            if (atomParent is ArendReturnExpr) {
+                val list = atomParent.atomFieldsAccList
+                if (list.size > 1 && list[1] == atomFieldsAcc) {
+                    return levelProof
+                }
+                expr = atomFieldsAcc
+                break
+            }
+
+            val newExpr = (atomFieldsAcc?.parent as? ArendArgumentAppExpr)?.parent as? ArendNewExpr ?: expr as? ArendNewExpr ?: return defaultUsage
+            if (newExpr.newKw != null) {
+                return newInstances
+            }
+
+            val tupleExpr = newExpr.parent as? ArendTupleExpr
+            if (tupleExpr != null && tupleExpr.exprList.firstOrNull() == newExpr) {
+                val tuple = tupleExpr.parent as? ArendTuple ?: return defaultUsage
+                if (tuple.tupleExprList.size != 1) {
+                    return defaultUsage
+                }
+                expr = tuple
+            } else {
+                expr = newExpr
+                break
+            }
+        }
+
+        val exprParent = expr.parent
+        if ((exprParent is ArendNameTele || exprParent is ArendReturnExpr) && exprParent.parent?.let { it is ArendDefFunction || it is ArendDefInstance } == true ||
+                ((exprParent as? ArendTypedExpr)?.parent as? ArendTypeTele)?.parent is ArendDefData) {
+            if (exprParent !is ArendReturnExpr) {
+                return parameters
+            }
+
+            val exprPP = exprParent.parent
+            return if (exprPP is ArendDefInstance || (exprPP as? ArendDefFunction)?.functionBody?.cowithKw != null) newInstances else resultTypes
+        }
+
         return defaultUsage
     }
 
     companion object {
         val nsUsage           = UsageType("Namespace commands")
-        val nsUsageInList     = UsageType("Hiding of using lists of namespace commands")
+        val nsUsageInList     = UsageType("Hiding or using lists of namespace commands")
         val leftUsage         = UsageType("Left sides of dot expressions")
         val usagesInCoClauses = UsageType("Coclauses")
         val usagesInPatterns  = UsageType("Patterns")
+        val newInstances      = UsageType("New instances")
+        val levelProof        = UsageType("Level proof")
+        val parameters        = UsageType("Parameter types")
+        val resultTypes       = UsageType("Result types")
+        val extendsUsages     = UsageType("Extends clauses")
         val defaultUsage      = UsageType("Unclassified usages")
     }
 }
