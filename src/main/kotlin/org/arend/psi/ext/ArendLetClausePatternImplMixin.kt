39cc6b8d8dde9d572baf06d875bc9ee9de1d52eb
valis <valery.isaev@gmail.com>
2019-08-28 20:37:59 +0300

    Allow _ in let patterns

--- src/main/kotlin/org/arend/psi/ext/ArendLetClausePatternImplMixin.kt
+++ src/main/kotlin/org/arend/psi/ext/ArendLetClausePatternImplMixin.kt
@@ -3,19 +3,20 @@ package org.arend.psi.ext
 import com.intellij.lang.ASTNode
 import org.arend.psi.ArendExpr
 import org.arend.psi.ArendLetClausePattern
-import org.arend.term.abs.Abstract
 
 
 abstract class ArendLetClausePatternImplMixin(node: ASTNode) : ArendCompositeElementImpl(node), ArendLetClausePattern {
+    override fun isIgnored() = underscore != null
+
     override fun getReferable() = defIdentifier
 
     override fun getType(): ArendExpr? = typeAnnotation?.expr
 
     override fun getPatterns(): List<ArendLetClausePattern> = letClausePatternList
 
-    override fun getTopmostEquivalentSourceNode() = org.arend.psi.ext.getTopmostEquivalentSourceNode(this)
+    override fun getTopmostEquivalentSourceNode() = getTopmostEquivalentSourceNode(this)
 
-    override fun getParentSourceNode() = org.arend.psi.ext.getParentSourceNode(this)
+    override fun getParentSourceNode() = getParentSourceNode(this)
 
-    override fun getErrorData(): Abstract.ErrorData? = org.arend.psi.ext.getErrorData(this)
+    override fun getErrorData() = getErrorData(this)
 }
