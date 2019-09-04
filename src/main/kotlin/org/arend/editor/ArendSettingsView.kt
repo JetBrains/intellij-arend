8e310b136baaeb3c99b8c513ca3d645f5787bab7
valis <valery.isaev@gmail.com>
2019-09-04 16:08:34 +0300

    Refactor ArendSettingsView

--- src/main/kotlin/org/arend/editor/ArendSettingsView.kt
+++ src/main/kotlin/org/arend/editor/ArendSettingsView.kt
@@ -4,8 +4,14 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.JBIntSpinner
 import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.layout.and
 import com.intellij.ui.layout.panel
+import com.intellij.ui.layout.selected
+import com.intellij.ui.layout.selectedValueIs
 import org.arend.settings.ArendSettings
+import org.arend.util.labeled
+import org.arend.util.cellRow
+import org.arend.util.checked
 
 
 class ArendSettingsView {
@@ -16,41 +22,15 @@ class ArendSettingsView {
         ArendSettings.TypecheckingMode.SMART,
         ArendSettings.TypecheckingMode.DUMB,
         ArendSettings.TypecheckingMode.OFF))
+        .apply {
+            renderer = ToolTipListCellRenderer(listOf("Completely typecheck definitions", "Perform only basic checks", "Do not typecheck anything at all"))
+        }
     private val timeLimitSwitch = JBCheckBox("Stop typechecking after ", true)
     private val timeLimit = JBIntSpinner(5, 1, 3600)
 
     // Other settings
     private val clauseLimitSwitch = JBCheckBox("Limit the maximum number of clauses generated at once: ", true)
     private val clauseLimit = JBIntSpinner(10, 1, 1000)
-
-    init {
-        // Background typechecking
-        typecheckingMode.renderer = ToolTipListCellRenderer(listOf("Completely typecheck definitions", "Perform only basic checks", "Do not typecheck anything at all"))
-        typecheckingMode.addActionListener {
-            updateTypecheckingMode()
-        }
-        timeLimitSwitch.addActionListener {
-            updateTimeLimit()
-        }
-
-        // Other settings
-        clauseLimitSwitch.addActionListener {
-            updateClauseLimit()
-        }
-    }
-
-    private fun updateTimeLimit() {
-        timeLimit.isEnabled = typecheckingMode.selectedItem == ArendSettings.TypecheckingMode.SMART && timeLimitSwitch.isSelected
-    }
-
-    private fun updateTypecheckingMode() {
-        timeLimitSwitch.isEnabled = typecheckingMode.selectedItem == ArendSettings.TypecheckingMode.SMART
-        updateTimeLimit()
-    }
-
-    private fun updateClauseLimit() {
-        clauseLimit.isEnabled = clauseLimitSwitch.isSelected
-    }
 
     val isModified: Boolean
         get() =
@@ -80,33 +60,24 @@ class ArendSettingsView {
         typecheckingMode.selectedItem = arendSettings.typecheckingMode
         timeLimitSwitch.isSelected = arendSettings.withTimeLimit
         timeLimit.value = arendSettings.typecheckingTimeLimit
-        updateTypecheckingMode()
 
         // Other settings
         clauseLimitSwitch.isSelected = arendSettings.withClauseLimit
         clauseLimit.value = arendSettings.clauseLimit
-        updateClauseLimit()
     }
 
     fun createComponent() = panel {
         titledRow("Background typechecking") {
-            row { cell {
-                label("Typechecking mode: ")
-                typecheckingMode()
-            } }
-
-            row { cell {
-                timeLimitSwitch()
-                timeLimit()
+            labeled("Typechecking mode: ", typecheckingMode)
+            cellRow {
+                timeLimitSwitch().enableIf(typecheckingMode.selectedValueIs(ArendSettings.TypecheckingMode.SMART))
+                timeLimit().enableIf(typecheckingMode.selectedValueIs(ArendSettings.TypecheckingMode.SMART) and timeLimitSwitch.selected)
                 label("second(s)")
-            } }
+            }
         }
 
         titledRow("Other settings") {
-            row { cell {
-                clauseLimitSwitch()
-                clauseLimit()
-            } }
+            checked(clauseLimitSwitch, clauseLimit)
         }
     }
 }
--- src/main/kotlin/org/arend/util/KotlinUtils.kt
+++ src/main/kotlin/org/arend/util/KotlinUtils.kt
@@ -1,5 +1,12 @@
 package org.arend.util
 
+import com.intellij.ui.layout.Cell
+import com.intellij.ui.layout.LayoutBuilder
+import com.intellij.ui.layout.Row
+import com.intellij.ui.layout.selected
+import javax.swing.AbstractButton
+import javax.swing.JComponent
+
 inline fun <T, R> Iterable<T>.mapFirstNotNull(transform: (T) -> R?): R? {
     for (item in this) {
         val r = transform(item)
@@ -16,4 +23,57 @@ inline fun <T, R : Any> Iterable<T>.mapUntilNotNull(transform: (T) -> R?): Array
         result.add(transform(item) ?: return result)
     }
     return result
+}
+
+// UI DSL
+
+inline fun LayoutBuilder.cellRow(init: Cell.() -> Unit) {
+    row { cell(false, init) }
+}
+
+inline fun Row.cellRow(init: Cell.() -> Unit) {
+    row { cell(false, init) }
+}
+
+inline fun LayoutBuilder.labeledRow(text: String, init: () -> Unit) {
+    cellRow {
+        label(text)
+        init()
+    }
+}
+
+inline fun Row.labeledRow(text: String, init: () -> Unit) {
+    cellRow {
+        label(text)
+        init()
+    }
+}
+
+fun LayoutBuilder.labeled(text: String, component: JComponent) {
+    cellRow {
+        label(text)
+        component()
+    }
+}
+
+fun Row.labeled(text: String, component: JComponent) {
+    cellRow {
+        label(text)
+        component()
+    }
+}
+
+fun LayoutBuilder.checked(checkBox: AbstractButton, component: JComponent) {
+    cellRow {
+        checkBox()
+        component().enableIf(checkBox.selected)
+    }
+}
+
+
+fun Row.checked(checkBox: AbstractButton, component: JComponent) {
+    cellRow {
+        checkBox()
+        component().enableIf(checkBox.selected)
+    }
 }
