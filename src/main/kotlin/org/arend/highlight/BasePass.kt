e862cb853057e8458d7bf615c48e31f4cc0f3027
Valery Isaev <valery.isaev@jetbrains.com>
2019-09-07 17:49:33 +0300

    Refactor ExpectedConstructor error

--- src/main/kotlin/org/arend/highlight/BasePass.kt
+++ src/main/kotlin/org/arend/highlight/BasePass.kt
@@ -196,7 +196,7 @@ abstract class BasePass(protected val file: ArendFile, editor: Editor, name: Str
                     LEMMA_LEVEL -> if (element is ArendDefFunction) element.lemmaKw else element.ancestor<ArendReturnExpr>()?.levelKw
                     else -> null
                 }
-                is ExpectedConstructor -> (element as? ArendPattern)?.firstChild
+                is ExpectedConstructorError -> (element as? ArendPattern)?.firstChild
                 is AnotherClassifyingFieldError ->
                     (error.candidate.underlyingReferable as? ArendFieldDefIdentifier)?.let {
                         (it.parent as? ArendFieldTele)?.classifyingKw ?: it
