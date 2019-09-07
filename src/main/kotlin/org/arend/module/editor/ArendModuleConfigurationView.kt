b27a3fef74444ab88008cb326d9d7c23e8116875
Valery Isaev <valery.isaev@jetbrains.com>
2019-09-07 13:41:59 +0300

    List available libraries

--- src/main/kotlin/org/arend/module/editor/ArendModuleConfigurationView.kt
+++ src/main/kotlin/org/arend/module/editor/ArendModuleConfigurationView.kt
@@ -8,14 +8,22 @@ import com.intellij.openapi.util.io.FileUtil
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.layout.panel
+import org.arend.ArendIcons
 import org.arend.library.LibraryDependency
 import org.arend.module.config.ArendModuleConfiguration
 import org.arend.ui.DualList
+import org.arend.ui.TextFieldChangeListener
+import org.arend.ui.addBrowseAndChangeListener
 import org.arend.ui.content
+import org.arend.util.FileUtils
 import org.arend.util.checked
 import org.arend.util.labeled
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.swing.BorderFactory
 import javax.swing.JTextField
+import kotlin.streams.toList
 
 
 class ArendModuleConfigurationView(project: Project?, root: String?, name: String? = null) : ArendModuleConfiguration {
@@ -37,16 +45,26 @@ class ArendModuleConfigurationView(project: Project?, root: String?, name: Strin
     private val binariesTextField = TextFieldWithBrowseButton().apply {
         addBrowseFolderListener("Binaries directory", "Select the directory in which the binary files${if (name == null) "" else " of module $name"} will be put", project, FileChooserDescriptorFactory.createSingleFolderDescriptor(), textComponentAccessor)
     }
-    private val libRootTextField = TextFieldWithBrowseButton().apply {
-        addBrowseFolderListener("Path to libraries", "Select the directory in which dependencies${if (name == null) "" else " of module $name"} are located", project, FileChooserDescriptorFactory.createSingleFolderDescriptor())
+
+    private val dualList = object : DualList<LibraryDependency>("Available libraries:", "Module dependencies:", true) {
+        override fun isAvailable(t: LibraryDependency) = Files.isRegularFile(Paths.get(librariesRoot, t.name, FileUtils.LIBRARY_CONFIG_FILE))
+
+        override fun getIcon(t: LibraryDependency) = ArendIcons.LIBRARY_ICON
+    }.apply {
+        selectedList.setEmptyText("No dependencies")
     }
 
-    private val dualList = DualList<LibraryDependency>("Available libraries:", "Module dependencies:", true)
+    private val libRootTextField = TextFieldWithBrowseButton()
+    private val textFieldChangeListener = libRootTextField.addBrowseAndChangeListener("Path to libraries", "Select the directory in which dependencies${if (name == null) "" else " of module $name"} are located", project, FileChooserDescriptorFactory.createSingleFolderDescriptor()) {
+        dualList.availableList.content = (librariesList - dependencies).sorted()
+        dualList.selectedList.updateUI()
+    }
 
     override var librariesRoot: String
         get() = libRootTextField.text
         set(value) {
             libRootTextField.text = value
+            textFieldChangeListener.fireEvent()
         }
 
     override var sourcesDir: String
@@ -71,6 +89,18 @@ class ArendModuleConfigurationView(project: Project?, root: String?, name: Strin
         get() = dualList.selectedList.content
         set(value) {
             dualList.selectedList.content = value
+            dualList.availableList.content -= value
+        }
+
+    private val librariesList: List<LibraryDependency>
+        get() = try {
+            Files.list(Paths.get(librariesRoot)).toList().mapNotNull {
+                if (Files.isDirectory(it) && Files.isRegularFile(it.resolve(FileUtils.LIBRARY_CONFIG_FILE))) {
+                    LibraryDependency(it.fileName.toString())
+                } else null
+            }
+        } catch (e: IOException) {
+            emptyList()
         }
 
     fun createComponent() = panel {
@@ -79,7 +109,6 @@ class ArendModuleConfigurationView(project: Project?, root: String?, name: Strin
 
         titledRow("Libraries") {
             labeled("Path to libraries: ", libRootTextField)
-            dualList.selectedList.setEmptyText("No dependencies")
             row { ScrollPaneFactory.createScrollPane(dualList, true)() }
         }
     }.apply {
---
+++ src/main/kotlin/org/arend/ui/TextFieldChangeListener.kt
@@ -0,0 +1,63 @@
+package org.arend.ui
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.fileChooser.FileChooserFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ComponentWithBrowseButton.BrowseFolderActionListener
+import com.intellij.openapi.ui.TextComponentAccessor
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.vfs.VirtualFile
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
+import javax.swing.JTextField
+
+abstract class TextFieldChangeListener(private val textField: JTextField) {
+    protected var previousText: String? = null
+        private set
+
+    init {
+        textField.addActionListener {
+            fireEvent()
+        }
+        textField.addFocusListener(object : FocusListener {
+            override fun focusLost(e: FocusEvent?) {
+                fireEvent()
+            }
+
+            override fun focusGained(e: FocusEvent?) {}
+        })
+    }
+
+    fun fireEvent() {
+        val newText = textField.text
+        if (newText != previousText) {
+            textChanged()
+            previousText = newText
+        }
+    }
+
+    abstract fun textChanged()
+}
+
+fun TextFieldWithBrowseButton.addBrowseAndChangeListener(title: String?, description: String?, project: Project?, fileChooserDescriptor: FileChooserDescriptor, textChanged: (String?) -> Unit): TextFieldChangeListener {
+    val browseListener = object : BrowseFolderActionListener<JTextField>(title, description, this, project, fileChooserDescriptor, TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT) {
+        val changeListener = object : TextFieldChangeListener(textField) {
+            override fun textChanged() {
+                textChanged(previousText)
+            }
+        }
+
+        override fun onFileChosen(chosenFile: VirtualFile) {
+            super.onFileChosen(chosenFile)
+            changeListener.fireEvent()
+        }
+    }
+    addActionListener(browseListener)
+
+    ApplicationManager.getApplication()?.let { if (!it.isUnitTestMode && !it.isHeadlessEnvironment) {
+        FileChooserFactory.getInstance().installFileCompletion(textField, fileChooserDescriptor, true, null)
+    } }
+
+    return browseListener.changeListener
+}
