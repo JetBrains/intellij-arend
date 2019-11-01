3d728584c6b7b3a8eb1d84dfcea89ed036796c7a
valis <valery.isaev@gmail.com>
2019-11-01 10:51:12 +0300

    Fix a bug with an error in \data

--- src/main/kotlin/org/arend/psi/ext/ArendUniverseExprImplMixin.kt
+++ src/main/kotlin/org/arend/psi/ext/ArendUniverseExprImplMixin.kt
@@ -1,14 +1,14 @@
 package org.arend.psi.ext
 
 import com.intellij.lang.ASTNode
-import org.arend.term.abs.AbstractExpressionVisitor
 import org.arend.psi.ArendUniverseExpr
+import org.arend.term.abs.AbstractExpressionVisitor
 
 
 abstract class ArendUniverseExprImplMixin(node: ASTNode) : ArendExprImplMixin(node), ArendUniverseExpr {
     override fun <P : Any?, R : Any?> accept(visitor: AbstractExpressionVisitor<in P, out R>, params: P?): R {
         appExpr?.let { return it.accept(visitor, params) }
-        propKw?.let { return visitor.visitUniverse(it, 0, -1, null, null, if (visitor.visitErrors()) org.arend.psi.ext.getErrorData(this) else null, params) }
+        propKw?.let { return visitor.visitUniverse(this, 0, -1, null, null, if (visitor.visitErrors()) getErrorData(this) else null, params) }
         error("Incorrect expression: universeExpr")
     }
 }
