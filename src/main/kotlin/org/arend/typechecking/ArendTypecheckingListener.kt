4711e55313633290c536d49ee99f47ad34e81e00
Valery Isaev <valery.isaev@jetbrains.com>
2019-12-11 21:19:55 +0300

    Cache only pattern variables

--- src/main/kotlin/org/arend/typechecking/ArendTypecheckingListener.kt
+++ src/main/kotlin/org/arend/typechecking/ArendTypecheckingListener.kt
@@ -3,12 +3,18 @@ package org.arend.typechecking
 import com.intellij.util.containers.ContainerUtil
 import org.arend.core.context.binding.Binding
 import org.arend.naming.reference.Referable
+import org.arend.psi.ArendAtomPatternOrPrefix
+import org.arend.psi.ArendDefIdentifier
+import org.arend.psi.ArendPattern
 
 
 object ArendTypecheckingListener : TypecheckingListener {
     val referableTypeCache: MutableMap<Referable, Binding> = ContainerUtil.createWeakKeySoftValueMap<Referable, Binding>()
 
-    override fun referableTypechecked(referable: Referable?, binding: Binding?) {
-        if (referable != null && binding != null) referableTypeCache[referable.underlyingReferable] = binding
+    override fun referableTypechecked(referable: Referable, binding: Binding) {
+        val defId = referable.underlyingReferable as? ArendDefIdentifier ?: return
+        if (defId.parent?.let { it is ArendPattern || it is ArendAtomPatternOrPrefix } == true) {
+            referableTypeCache[defId] = binding
+        }
     }
 }
