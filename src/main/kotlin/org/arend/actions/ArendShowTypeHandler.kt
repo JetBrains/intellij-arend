958b614b199b4d3b90bd5ef1a1448f39ee43ed92
ice1000 <ice1000kotlin@foxmail.com>
2020-02-12 12:45:21 -0500

    Tabs -> Spaces

--- src/main/kotlin/org/arend/actions/ArendShowTypeHandler.kt
+++ src/main/kotlin/org/arend/actions/ArendShowTypeHandler.kt
@@ -29,92 +29,92 @@ import org.arend.util.BinOpExpansionVisitor
 
 
 class ArendShowTypeHandler(val requestFocus: Boolean) : CodeInsightActionHandler {
-	override fun startInWriteAction() = false
+    override fun startInWriteAction() = false
 
-	private fun displayHint(typeInfo: String, editor: Editor) {
-		ApplicationManager.getApplication().invokeLater {
-			HintManager.getInstance()
-					.apply { setRequestFocusForNextHint(requestFocus) }
-					.showInformationHint(editor, typeInfo)
-		}
-	}
+    private fun displayHint(typeInfo: String, editor: Editor) {
+        ApplicationManager.getApplication().invokeLater {
+            HintManager.getInstance()
+                    .apply { setRequestFocusForNextHint(requestFocus) }
+                    .showInformationHint(editor, typeInfo)
+        }
+    }
 
-	fun doInvoke(project: Project, editor: Editor, file: PsiFile): String? {
-		val range = EditorUtil.getSelectionInAnyMode(editor)
-		val possibleParent = PsiTreeUtil.findCommonParent(
-				file.findElementAt(range.startOffset),
-				file.findElementAt(range.endOffset - 1)
-		) ?: return "selected expr in bad position"
-		val parent = possibleParent.ancestor<ArendExpr>()?.parent
-				?: return "selected text is not an arend expression"
-		val psiDef = parent.ancestor<ArendDefinition>()
-				?: return "selected text is not in a definition"
-		val service = project.service<TypeCheckingService>()
-		// Only work for functions right now
-		val concreteDef = PsiConcreteProvider(
-				project,
-				service.newReferableConverter(false),
-				DummyErrorReporter.INSTANCE,
-				null
-		).getConcrete(psiDef)
-				as? Concrete.FunctionDefinition
-				?: return "selected text is not in a function definition"
-		val def = service.getTypechecked(psiDef) as FunctionDefinition
+    fun doInvoke(project: Project, editor: Editor, file: PsiFile): String? {
+        val range = EditorUtil.getSelectionInAnyMode(editor)
+        val possibleParent = PsiTreeUtil.findCommonParent(
+                file.findElementAt(range.startOffset),
+                file.findElementAt(range.endOffset - 1)
+        ) ?: return "selected expr in bad position"
+        val parent = possibleParent.ancestor<ArendExpr>()?.parent
+                ?: return "selected text is not an arend expression"
+        val psiDef = parent.ancestor<ArendDefinition>()
+                ?: return "selected text is not in a definition"
+        val service = project.service<TypeCheckingService>()
+        // Only work for functions right now
+        val concreteDef = PsiConcreteProvider(
+                project,
+                service.newReferableConverter(false),
+                DummyErrorReporter.INSTANCE,
+                null
+        ).getConcrete(psiDef)
+                as? Concrete.FunctionDefinition
+                ?: return "selected text is not in a function definition"
+        val def = service.getTypechecked(psiDef) as FunctionDefinition
 
-		val children = collectArendExprs(parent, range)
-				.map { ConcreteBuilder.convertExpression(IdReferableConverter.INSTANCE, it) }
-				.map(Concrete::BinOpSequenceElem)
-				.toList()
-				.takeIf { it.isNotEmpty() }
-				?: return "cannot find a suitable subexpression"
-		val parser = BinOpParser(DummyErrorReporter.INSTANCE)
-		val body = def.actualBody as? Expression
-				?: return "function body is not an expression"
-		// Only work for single clause right now
-		val concreteBody = concreteDef.body.term
-				?: return "does not yet support multiple clauses"
+        val children = collectArendExprs(parent, range)
+                .map { ConcreteBuilder.convertExpression(IdReferableConverter.INSTANCE, it) }
+                .map(Concrete::BinOpSequenceElem)
+                .toList()
+                .takeIf { it.isNotEmpty() }
+                ?: return "cannot find a suitable subexpression"
+        val parser = BinOpParser(DummyErrorReporter.INSTANCE)
+        val body = def.actualBody as? Expression
+                ?: return "function body is not an expression"
+        // Only work for single clause right now
+        val concreteBody = concreteDef.body.term
+                ?: return "does not yet support multiple clauses"
 
-		val subExpr = if (children.size == 1)
-			children.first().expression
-		else parser.parse(Concrete.BinOpSequenceExpression(null, children))
-		val subExprVisitor = CorrespondedSubExprVisitor(subExpr.accept(BinOpExpansionVisitor, parser))
-		val visited = concreteBody
-				.accept(BinOpExpansionVisitor, parser)
-				.accept(subExprVisitor, body)
-				?: return "cannot find a suitable subexpression"
-		val subCore = visited.proj1
-		val textRange = rangeOf(visited.proj2)
-		editor.selectionModel.setSelection(textRange.startOffset, textRange.endOffset)
-		displayHint(subCore.type.toString(), editor)
-		return null
-	}
+        val subExpr = if (children.size == 1)
+            children.first().expression
+        else parser.parse(Concrete.BinOpSequenceExpression(null, children))
+        val subExprVisitor = CorrespondedSubExprVisitor(subExpr.accept(BinOpExpansionVisitor, parser))
+        val visited = concreteBody
+                .accept(BinOpExpansionVisitor, parser)
+                .accept(subExprVisitor, body)
+                ?: return "cannot find a suitable subexpression"
+        val subCore = visited.proj1
+        val textRange = rangeOf(visited.proj2)
+        editor.selectionModel.setSelection(textRange.startOffset, textRange.endOffset)
+        displayHint(subCore.type.toString(), editor)
+        return null
+    }
 
-	private fun rangeOf(subConcrete: Concrete.Expression): TextRange {
-		val expr = if (subConcrete is Concrete.AppExpression) {
-			val function = subConcrete.data as PsiElement
-			PsiTreeUtil.findCommonParent(
-					function,
-					subConcrete.arguments.first().expression.data as PsiElement
-			) ?: function
-		} else subConcrete.data as PsiElement
-		return expr.textRange
-	}
+    private fun rangeOf(subConcrete: Concrete.Expression): TextRange {
+        val expr = if (subConcrete is Concrete.AppExpression) {
+            val function = subConcrete.data as PsiElement
+            PsiTreeUtil.findCommonParent(
+                    function,
+                    subConcrete.arguments.first().expression.data as PsiElement
+            ) ?: function
+        } else subConcrete.data as PsiElement
+        return expr.textRange
+    }
 
-	private fun collectArendExprs(parent: PsiElement, range: TextRange): Sequence<ArendExpr> {
-		val exprs = parent.childrenWithLeaves
-				.dropWhile { it.textRange.endOffset < range.startOffset }
-				.takeWhile { it.textRange.startOffset <= range.endOffset }
-				.toList()
-		if (exprs.isEmpty()) return emptySequence()
-		if (exprs.size == 1) {
-			val first = exprs.first()
-			return if (first is ArendExpr) return sequenceOf(first)
-			else collectArendExprs(first, range)
-		} else return exprs.asSequence().filterIsInstance<ArendExpr>()
-	}
+    private fun collectArendExprs(parent: PsiElement, range: TextRange): Sequence<ArendExpr> {
+        val exprs = parent.childrenWithLeaves
+                .dropWhile { it.textRange.endOffset < range.startOffset }
+                .takeWhile { it.textRange.startOffset <= range.endOffset }
+                .toList()
+        if (exprs.isEmpty()) return emptySequence()
+        if (exprs.size == 1) {
+            val first = exprs.first()
+            return if (first is ArendExpr) return sequenceOf(first)
+            else collectArendExprs(first, range)
+        } else return exprs.asSequence().filterIsInstance<ArendExpr>()
+    }
 
-	override fun invoke(project: Project, editor: Editor, file: PsiFile) {
-		val s = doInvoke(project, editor, file)
-		if (s != null) displayHint("Failed to obtain type because $s", editor)
-	}
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+        val s = doInvoke(project, editor, file)
+        if (s != null) displayHint("Failed to obtain type because $s", editor)
+    }
 }
