555430bfcbdff3bf5a16680d35efd84ce6b6f0f7
ice1000 <ice1000kotlin@foxmail.com>
2020-02-15 03:39:10 -0500

    Add separator, polish UI

--- src/main/kotlin/org/arend/actions/ArendToolbarGroup.kt
+++ src/main/kotlin/org/arend/actions/ArendToolbarGroup.kt
@@ -3,6 +3,7 @@ package org.arend.actions
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Separator
 import com.intellij.openapi.project.Project
 import org.arend.ArendIcons
 import org.arend.core.expr.visitor.ToAbstractVisitor
@@ -15,10 +16,12 @@ class ArendToolbarGroup : ActionGroup() {
     val project = e.project ?: return emptyArray()
     if (project.isDisposed) return emptyArray()
     return arrayOf(
+        Separator.getInstance(),
         action(project, ToAbstractVisitor.Flag.SHOW_IMPLICIT_ARGS).apply {
           templatePresentation.apply {
             icon = ArendIcons.SHOW_IMPLICITS
             text = "Show Implicits"
+            description = "Show implicit arguments in pop-ups"
           }
         }
     )
