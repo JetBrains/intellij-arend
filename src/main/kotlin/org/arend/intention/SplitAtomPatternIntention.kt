04ffe5235f0721ee7cd2f6a4b7069fb6bd9b41c5
Valery Isaev <valery.isaev@jetbrains.com>
2020-02-21 18:18:04 +0300

    Rewrite ElimTree

--- src/main/kotlin/org/arend/intention/SplitAtomPatternIntention.kt
+++ src/main/kotlin/org/arend/intention/SplitAtomPatternIntention.kt
@@ -4,7 +4,6 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import org.arend.core.context.binding.Binding
 import org.arend.core.context.binding.Variable
 import org.arend.core.context.param.DependentLink
 import org.arend.core.definition.ClassDefinition
@@ -15,34 +14,23 @@ import org.arend.core.expr.Expression
 import org.arend.core.expr.SigmaExpression
 import org.arend.core.expr.visitor.ToAbstractVisitor
 import org.arend.core.pattern.BindingPattern
-import org.arend.core.pattern.ConstructorPattern
-import org.arend.core.pattern.Pattern
-import org.arend.error.ListErrorReporter
+import org.arend.core.pattern.ConstructorExpressionPattern
+import org.arend.core.pattern.ExpressionPattern
 import org.arend.ext.core.ops.NormalizationMode
 import org.arend.ext.prettyprinting.PrettyPrinterConfig
 import org.arend.naming.reference.Referable
 import org.arend.naming.reference.UnresolvedReference
-import org.arend.naming.reference.converter.IdReferableConverter
 import org.arend.naming.renamer.StringRenamer
-import org.arend.naming.resolving.visitor.ExpressionResolveNameVisitor
-import org.arend.naming.scope.ConvertingScope
-import org.arend.naming.scope.ListScope
-import org.arend.naming.scope.Scope
 import org.arend.prelude.Prelude
 import org.arend.psi.*
 import org.arend.psi.ext.*
 import org.arend.quickfix.referenceResolve.ResolveReferenceAction.Companion.getTargetName
 import org.arend.refactoring.VariableImpl
 import org.arend.term.abs.Abstract
-import org.arend.term.abs.ConcreteBuilder
 import org.arend.term.concrete.Concrete
 import org.arend.term.prettyprint.PrettyPrintVisitor
 import org.arend.typechecking.ArendTypecheckingListener
 import org.arend.typechecking.TypeCheckingService
-import org.arend.typechecking.patternmatching.ElimTypechecking
-import org.arend.typechecking.patternmatching.PatternTypechecking
-import org.arend.typechecking.provider.EmptyConcreteProvider
-import org.arend.typechecking.visitor.DesugarVisitor
 import java.util.*
 import java.util.Collections.singletonList
 
@@ -115,7 +103,9 @@ class SplitAtomPatternIntention : SelfTargetingIntention<PsiElement>(PsiElement:
             if (definition != null) {
                 val typeCheckedDefinition = project.service<TypeCheckingService>().getTypechecked(definition)
                 if (typeCheckedDefinition != null && abstractPatterns != null) {
-                    val (patterns, isElim) = computePatterns(abstractPatterns, typeCheckedDefinition.parameters, definition as? Abstract.EliminatedExpressionsHolder, definition.scope, project)
+                    // TODO[lang_ext]
+                    val patterns: List<ExpressionPattern>? = null
+                    val isElim = false
                     if (patterns != null && indexList.size > 0) {
                         val typecheckedPattern = if (isElim) patterns.getOrNull(indexList[0]) else findMatchingPattern(abstractPatterns, typeCheckedDefinition.parameters, patterns, indexList[0])
                         if (typecheckedPattern != null) {
@@ -374,48 +364,10 @@ class SplitAtomPatternIntention : SelfTargetingIntention<PsiElement>(PsiElement:
             return Pair(clause, indexList)
         }
 
-        private fun computePatterns(abstractPatterns: List<Abstract.Pattern>, parameters: DependentLink, elimExprHolder: Abstract.EliminatedExpressionsHolder?, scope: Scope, project: Project): Pair<List<Pattern>?, Boolean> {
-            val listErrorReporter = ListErrorReporter()
-            val concreteParameters = elimExprHolder?.parameters?.let { params ->
-                ConcreteBuilder.convert(IdReferableConverter.INSTANCE, true) { it.buildTelescopeParameters(params) }
-            }
-            val elimVars = elimExprHolder?.eliminatedExpressions?.let { vars ->
-                ConcreteBuilder.convert(IdReferableConverter.INSTANCE, true) { it.buildReferences(vars) }
-            }
-            val elimParams = if (concreteParameters != null && elimVars != null) {
-                val context = LinkedHashMap<Referable, Binding>()
-                var link = parameters
-                loop@ for (parameter in concreteParameters) {
-                    for (referable in parameter.referableList) {
-                        if (!link.hasNext()) break@loop
-
-                        context[referable] = link
-                        link = link.next
-                    }
-                }
-                val listScope = ListScope(context.keys.toList())
-                for (elimVar in elimVars) ExpressionResolveNameVisitor.resolve(elimVar, listScope, null)
-
-                ElimTypechecking.getEliminatedParameters(elimVars, emptyList(), parameters, listErrorReporter, context)
-                        ?: return Pair(null, true)
-            } else {
-                emptyList()
-            }
-
-            val concretePatterns = ConcreteBuilder.convert(IdReferableConverter.INSTANCE, true) { it.buildPatterns(abstractPatterns) }
-            DesugarVisitor.desugarPatterns(concretePatterns, listErrorReporter)
-            val service = project.service<TypeCheckingService>()
-            ExpressionResolveNameVisitor(EmptyConcreteProvider.INSTANCE, ConvertingScope(service.newReferableConverter(true), scope), ArrayList(), listErrorReporter, null).visitPatterns(concretePatterns)
-            // TODO: This won't work with defined constructors
-            val patternTypechecking = PatternTypechecking(listErrorReporter, PatternTypechecking.Mode.CONSTRUCTOR, service.typecheckerState)
-            val patterns = patternTypechecking.typecheckPatterns(concretePatterns, parameters, elimParams)
-            return Pair(if (listErrorReporter.errorList.isEmpty()) patterns else null, elimParams.isNotEmpty())
-        }
-
-        private fun findPattern(indexList: List<Int>, typecheckedPattern: Pattern, abstractPattern: Abstract.Pattern): Pattern? {
+        private fun findPattern(indexList: List<Int>, typecheckedPattern: ExpressionPattern, abstractPattern: Abstract.Pattern): ExpressionPattern? {
             if (indexList.isEmpty()) return typecheckedPattern
-            if (typecheckedPattern is ConstructorPattern) {
-                val typecheckedPatternChild = findMatchingPattern(abstractPattern.arguments, typecheckedPattern.parameters, typecheckedPattern.patterns.patternList, indexList[0])
+            if (typecheckedPattern is ConstructorExpressionPattern) {
+                val typecheckedPatternChild = findMatchingPattern(abstractPattern.arguments, typecheckedPattern.parameters, typecheckedPattern.subPatterns, indexList[0])
                 val abstractPatternChild = abstractPattern.arguments.getOrNull(indexList[0])
                 if (typecheckedPatternChild != null && abstractPatternChild != null)
                     return findPattern(indexList.drop(1), typecheckedPatternChild, abstractPatternChild)
@@ -430,7 +382,7 @@ class SplitAtomPatternIntention : SelfTargetingIntention<PsiElement>(PsiElement:
             return null
         }
 
-        private fun findMatchingPattern(abstractPatterns: List<Abstract.Pattern>, parameters: DependentLink, typecheckedPatterns: List<Pattern>, index: Int): Pattern? {
+        private fun findMatchingPattern(abstractPatterns: List<Abstract.Pattern>, parameters: DependentLink, typecheckedPatterns: List<ExpressionPattern>, index: Int): ExpressionPattern? {
             var link = parameters
             var i = 0
             var j = 0
