07b758c78a2ba9a4fd98a3c1a95f984ea44f49c3
ice1000 <ice1000kotlin@foxmail.com>
2020-02-22 04:03:53 -0500

    Fix bugs

--- src/main/kotlin/org/arend/actions/ArendShowTypeAction.kt
+++ src/main/kotlin/org/arend/actions/ArendShowTypeAction.kt
@@ -33,50 +33,48 @@ class ArendShowTypeAction : ArendPopupAction() {
         val (subCore, subExpr, subPsi) = correspondedSubExpr(selected, file, project)
         fun select(range: TextRange) =
                 editor.selectionModel.setSelection(range.startOffset, range.endOffset)
-        displayHint {
-            fun hint(e: Expression?) = e?.let { showInformationHint(editor, prettyPopupExpr(project, it)) }
-                    ?: throw SubExprError("failed to synthesize type from given expr")
-
-            fun default() {
-                select(rangeOfConcrete(subExpr))
-                hint(subCore.type)
-            }
-            if (subPsi is ArendLamExprImplMixin
-                    && subExpr is Concrete.LamExpression
-                    && subCore is LamExpression) {
-                val param = subPsi.parameters
-                        .firstOrNull { selected in it.textRange }
-                        ?.identifierOrUnknownList
-                        ?.firstOrNull { selected in it.textRange }
-                        ?.defIdentifier
-                        ?: run { default(); return@displayHint }
-                hint(FindBinding.visitLam(param, subExpr, subCore).typeExpr)
-                select(param.textRange)
-            } else if (subPsi is ArendPiExprImplMixin
-                    && subExpr is Concrete.PiExpression
-                    && subCore is PiExpression) {
-                val param = typedTele(subPsi.parameters, selected)
-                        ?: run { default(); return@displayHint }
-                hint(FindBinding.visitPi(param, subExpr, subCore).typeExpr)
-                select(param.textRange)
-            } else if (subPsi is ArendSigmaExprImplMixin
-                    && subExpr is Concrete.SigmaExpression
-                    && subCore is SigmaExpression) {
-                val param = typedTele(subPsi.parameters, selected)
-                        ?: run { default(); return@displayHint }
-                hint(FindBinding.visitSigma(param, subExpr, subCore).typeExpr)
-                select(param.textRange)
-            } else if (subPsi is ArendLetExprImplMixin
-                    && subExpr is Concrete.LetExpression
-                    && subCore is LetExpression) {
-                val param = subPsi.letClauses
-                        .firstOrNull { it.letClausePattern?.let { selected in it.textRange } ?: false }
-                        ?.letClausePattern
-                        ?: run { default(); return@displayHint }
-                hint(FindBinding.visitLet(param, subExpr, subCore))
-                select(param.textRange)
-            } else default()
+        fun hint(e: Expression?) = e?.let { displayHint { showInformationHint(editor, prettyPopupExpr(project, it)) } }
+                ?: throw SubExprError("failed to synthesize type from given expr")
+        fun default() {
+            select(rangeOfConcrete(subExpr))
+            hint(subCore.type)
         }
+        if (subPsi is ArendLamExprImplMixin
+                && subExpr is Concrete.LamExpression
+                && subCore is LamExpression) {
+            val param = subPsi.parameters
+                    .firstOrNull { selected in it.textRange }
+                    ?.identifierOrUnknownList
+                    ?.firstOrNull { selected in it.textRange }
+                    ?.defIdentifier
+                    ?: run { default(); return }
+            select(param.textRange)
+            hint(FindBinding.visitLam(param, subExpr, subCore).typeExpr)
+        } else if (subPsi is ArendPiExprImplMixin
+                && subExpr is Concrete.PiExpression
+                && subCore is PiExpression) {
+            val param = typedTele(subPsi.parameters, selected)
+                    ?: run { default(); return }
+            select(param.textRange)
+            hint(FindBinding.visitPi(param, subExpr, subCore).typeExpr)
+        } else if (subPsi is ArendSigmaExprImplMixin
+                && subExpr is Concrete.SigmaExpression
+                && subCore is SigmaExpression) {
+            val param = typedTele(subPsi.parameters, selected)
+                    ?: run { default(); return }
+            select(param.textRange)
+            hint(FindBinding.visitSigma(param, subExpr, subCore).typeExpr)
+        } else if (subPsi is ArendLetExprImplMixin
+                && subExpr is Concrete.LetExpression
+                && subCore is LetExpression) {
+            val param = subPsi.letClauses
+                    .asSequence()
+                    .mapNotNull { it.defIdentifier }
+                    .firstOrNull { selected in it.textRange }
+                    ?: run { default(); return }
+            select(param.textRange)
+            hint(FindBinding.visitLet(param, subExpr, subCore))
+        } else default()
     }
 
     private fun typedTele(p: List<ArendTypeTele>, selected: TextRange) = p
