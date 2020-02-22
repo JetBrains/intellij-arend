4d657039f4d65de6e9589e684acd8a3cd3ce8b2a
ice1000 <ice1000kotlin@foxmail.com>
2020-02-21 22:02:16 -0500

    Create `ArendShowNormalFormAction`

--- src/main/kotlin/org/arend/actions/ArendShowNormalFormAction.kt
+++ src/main/kotlin/org/arend/actions/ArendShowNormalFormAction.kt
@@ -3,19 +3,20 @@ package org.arend.actions
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import org.arend.ext.core.ops.NormalizationMode
 import org.arend.refactoring.SubExprError
 import org.arend.refactoring.correspondedSubExpr
 import org.arend.refactoring.prettyPopupExpr
 import org.arend.refactoring.rangeOfConcrete
 
-class ArendShowTypeAction : ArendPopupAction() {
+class ArendShowNormalFormAction : ArendPopupAction() {
     override fun getHandler() = object : ArendPopupHandler(requestFocus) {
         override fun invoke(project: Project, editor: Editor, file: PsiFile) = try {
             val visited = correspondedSubExpr(editor, file, project)
             val subCore = visited.proj1
             val textRange = rangeOfConcrete(visited.proj2)
             editor.selectionModel.setSelection(textRange.startOffset, textRange.endOffset)
-            displayHint { showInformationHint(editor, prettyPopupExpr(project, subCore.type)) }
+            displayHint { showInformationHint(editor, prettyPopupExpr(project, subCore.normalize(NormalizationMode.NF))) }
         } catch (t: SubExprError) {
             displayHint { showErrorHint(editor, "Failed to obtain type because ${t.message}") }
         }
