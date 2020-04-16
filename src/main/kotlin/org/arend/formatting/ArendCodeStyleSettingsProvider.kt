e66f59b71633a2394440111105dcac39e42489de
Valery Isaev <valery.isaev@jetbrains.com>
2020-04-16 10:12:32 +0300

    Simplify code style settings

--- src/main/kotlin/org/arend/formatting/ArendCodeStyleSettingsProvider.kt
+++ src/main/kotlin/org/arend/formatting/ArendCodeStyleSettingsProvider.kt
@@ -1,25 +1,50 @@
 package org.arend.formatting
 
-import com.intellij.application.options.CodeStyleAbstractConfigurable
-import com.intellij.application.options.CodeStyleAbstractPanel
+import com.intellij.application.options.*
 import com.intellij.lang.Language
-import com.intellij.openapi.options.Configurable
-import com.intellij.psi.codeStyle.CodeStyleSettings
-import com.intellij.psi.codeStyle.CodeStyleSettingsProvider
-import com.intellij.psi.codeStyle.CustomCodeStyleSettings
+import com.intellij.psi.codeStyle.*
 import org.arend.ArendLanguage
 
-class ArendCodeStyleSettingsProvider: CodeStyleSettingsProvider(){
-    override fun getConfigurableDisplayName(): String? = "Arend"
+class ArendCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvider() {
+    override fun getLanguage(): Language = ArendLanguage.INSTANCE
 
-    override fun getLanguage(): Language? = ArendLanguage.INSTANCE
+    override fun getCodeSample(settingsType: SettingsType): String = """
+        \import Logic
+        \module M \where {
+        \data Dec (E : \Type)
+        | yes E
+        | no (Not E)
+        \class Decide (E : \Type)
+        | decide : Dec E
+        \func DecEq (A : \Type) => \Pi {a a' : A} ->
+        Dec (a = a')
+        }
+    """.trimIndent()
 
-    override fun createSettingsPage(settings: CodeStyleSettings?, modelSettings: CodeStyleSettings?): Configurable =
-            object : CodeStyleAbstractConfigurable(settings!!, modelSettings, "Arend") {
-                override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel =
-                        ArendCodeStyleMainPanel(currentSettings, settings)
-            };
+    override fun getIndentOptionsEditor(): IndentOptionsEditor = SmartIndentOptionsEditor(this)
 
-    override fun createCustomSettings(settings: CodeStyleSettings?): CustomCodeStyleSettings? =
-            ArendCodeStyleSettings(settings)
+    override fun customizeDefaults(commonSettings: CommonCodeStyleSettings, indentOptions: CommonCodeStyleSettings.IndentOptions) {
+        commonSettings.indentOptions?.CONTINUATION_INDENT_SIZE = 4
+        commonSettings.indentOptions?.INDENT_SIZE = 2
+    }
+
+    override fun customizeSettings(consumer: CodeStyleSettingsCustomizable, settingsType: SettingsType) {
+        if (settingsType == SettingsType.INDENT_SETTINGS) {
+            consumer.showStandardOptions(
+                CodeStyleSettingsCustomizable.IndentOption.INDENT_SIZE.name,
+                CodeStyleSettingsCustomizable.IndentOption.CONTINUATION_INDENT_SIZE.name,
+                CodeStyleSettingsCustomizable.IndentOption.TAB_SIZE.name,
+                CodeStyleSettingsCustomizable.IndentOption.USE_TAB_CHARACTER.name,
+                CodeStyleSettingsCustomizable.IndentOption.SMART_TABS.name,
+                CodeStyleSettingsCustomizable.IndentOption.KEEP_INDENTS_ON_EMPTY_LINES.name)
+        }
+    }
+
+    override fun createConfigurable(baseSettings: CodeStyleSettings, modelSettings: CodeStyleSettings) = object : CodeStyleAbstractConfigurable(baseSettings, modelSettings, "Arend") {
+        override fun createPanel(settings: CodeStyleSettings?) = object : TabbedLanguageCodeStylePanel(ArendLanguage.INSTANCE, currentSettings, settings) {
+            override fun initTabs(settings: CodeStyleSettings?) {
+                addIndentOptionsTab(settings)
+            }
+        }
+    }
 }
