8cfbded3ed03b4b221870dd9d86f3d1307753ded
ice1000 <ice1000kotlin@foxmail.com>
2020-04-18 02:24:27 +0800

    Try something but failed

--- src/main/kotlin/org/arend/psi/ext/ArendCaseArgImplMixin.kt
+++ src/main/kotlin/org/arend/psi/ext/ArendCaseArgImplMixin.kt
@@ -2,10 +2,13 @@ package org.arend.psi.ext
 
 import com.intellij.lang.ASTNode
 import org.arend.psi.ArendCaseArg
+import org.arend.psi.ArendExpr
+import org.arend.psi.impl.ArendLiteralImpl
 
 
 abstract class ArendCaseArgImplMixin(node: ASTNode) : ArendSourceNodeImpl(node), ArendCaseArg {
     override fun getExpression() = caseArgExprAs.expr
+            ?: caseArgExprAs.applyHole?.node?.let(::ArendLiteralImpl)
 
     override fun getReferable() = caseArgExprAs.defIdentifier
 
