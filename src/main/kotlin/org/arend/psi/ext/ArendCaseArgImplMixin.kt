777820b67a1332fa900afd97d2623269c7a72865
Valery Isaev <valery.isaev@jetbrains.com>
2020-04-19 21:38:16 +0300

    Fix some issues with ApplyHole

--- src/main/kotlin/org/arend/psi/ext/ArendCaseArgImplMixin.kt
+++ src/main/kotlin/org/arend/psi/ext/ArendCaseArgImplMixin.kt
@@ -2,13 +2,12 @@ package org.arend.psi.ext
 
 import com.intellij.lang.ASTNode
 import org.arend.psi.ArendCaseArg
-import org.arend.psi.ArendExpr
-import org.arend.psi.impl.ArendLiteralImpl
 
 
 abstract class ArendCaseArgImplMixin(node: ASTNode) : ArendSourceNodeImpl(node), ArendCaseArg {
+    override fun getApplyHoleData() = caseArgExprAs.applyHole
+
     override fun getExpression() = caseArgExprAs.expr
-            ?: caseArgExprAs.applyHole?.node?.let(::ArendLiteralImpl)
 
     override fun getReferable() = caseArgExprAs.defIdentifier
 
