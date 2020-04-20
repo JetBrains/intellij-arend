02a99d4c51620d3db3d75eb52a07ee61931463ce
ice1000 <ice1000kotlin@foxmail.com>
2020-04-20 18:57:22 +0800

    Fix deprecation: DefaultProgramRunner

--- src/main/kotlin/org/arend/typechecking/execution/TypeCheckRunner.kt
+++ src/main/kotlin/org/arend/typechecking/execution/TypeCheckRunner.kt
@@ -1,11 +1,12 @@
 package org.arend.typechecking.execution
 
 import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.executors.DefaultRunExecutor
-import com.intellij.execution.runners.DefaultProgramRunner
+import com.intellij.execution.runners.GenericProgramRunner
 import org.arend.typechecking.execution.configurations.TypeCheckConfiguration
 
-class TypeCheckRunner : DefaultProgramRunner() {
+class TypeCheckRunner : GenericProgramRunner<RunnerSettings>() {
 
     override fun canRun(executorId: String, profile: RunProfile): Boolean =
             executorId == DefaultRunExecutor.EXECUTOR_ID && profile is TypeCheckConfiguration
