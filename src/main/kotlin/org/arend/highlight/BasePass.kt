9b04d1b7f106d3be50e61382edc74b4d91b198c9
Valery Isaev <valery.isaev@jetbrains.com>
2020-04-21 12:13:45 +0300

    Highlight embedded goal errors in editor

--- src/main/kotlin/org/arend/highlight/BasePass.kt
+++ src/main/kotlin/org/arend/highlight/BasePass.kt
@@ -203,7 +203,7 @@ abstract class BasePass(protected val file: ArendFile, editor: Editor, name: Str
                 GeneralError.Level.INFO -> HighlightSeverity.INFORMATION
             }
 
-        private fun getCauseElement(data: Any?): PsiElement? {
+        fun getCauseElement(data: Any?): PsiElement? {
             val cause = data?.let { (it as? DataContainer)?.data ?: it }
             return ((cause as? SmartPsiElementPointer<*>)?.let { runReadAction { it.element } } ?: cause) as? PsiElement
         }
