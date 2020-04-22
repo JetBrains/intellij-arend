d2512f93008a03c3a58fefb1e0af1f1d68afddcd
Valery Isaev <valery.isaev@jetbrains.com>
2020-04-22 13:01:34 +0300

    Do not invoke intentions in a viewer

--- src/main/kotlin/org/arend/intention/SelfTargetingIntention.kt
+++ src/main/kotlin/org/arend/intention/SelfTargetingIntention.kt
@@ -35,6 +35,10 @@ abstract class SelfTargetingIntention<T : PsiElement>(
     abstract fun applyTo(element: T, project: Project, editor: Editor)
 
     private fun getTarget(editor: Editor, file: PsiFile): T? {
+        if (editor.isViewer) {
+            return null
+        }
+
         val offset = editor.caretModel.offset
         val leaf1 = file.findElementAt(offset)
         val leaf2 = file.findElementAt(offset - 1)
