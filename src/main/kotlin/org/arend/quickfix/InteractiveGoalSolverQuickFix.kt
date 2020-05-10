6ceb51b3490d65b638be647804c20483e44f4c59
Valery Isaev <valery.isaev@jetbrains.com>
2020-05-10 21:41:21 +0300

    Separate interactive and non-interactive goal solvers

--- src/main/kotlin/org/arend/quickfix/InteractiveGoalSolverQuickFix.kt
+++ src/main/kotlin/org/arend/quickfix/InteractiveGoalSolverQuickFix.kt
@@ -1,6 +1,5 @@
 package org.arend.quickfix
 
-import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runReadAction
@@ -10,6 +9,7 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import org.arend.core.expr.visitor.ScopeDefinitionRenamer
+import org.arend.ext.typechecking.InteractiveGoalSolver
 import org.arend.naming.scope.ConvertingScope
 import org.arend.psi.ArendExpr
 import org.arend.refactoring.replaceExprSmart
@@ -21,29 +21,13 @@ import org.arend.typechecking.error.local.GoalError
 import org.arend.typechecking.visitor.CheckTypeVisitor
 import org.arend.ui.impl.ArendEditorUI
 
-class SolverGoalFillingQuickFix(private val element: ArendExpr, private val goal: GoalError) : IntentionAction {
+class InteractiveGoalSolverQuickFix(private val element: ArendExpr, private val goal: GoalError, private val solver: InteractiveGoalSolver) : IntentionAction {
     override fun invoke(project: Project, editor: Editor, file: PsiFile?) {
-        if (goal.result != null) {
-            invokeOnConcrete(goal.result, project, editor)
-        } else {
-            if (goal.typecheckingContext == null) {
-                cannotSolve(editor)
-                return
-            }
-            goal.goalSolver.trySolve(CheckTypeVisitor.loadTypecheckingContext(goal.typecheckingContext, project.service<TypeCheckingService>().typecheckerState, project.service<ErrorService>()), goal.causeSourceNode, goal.expectedType, ArendEditorUI(project, editor)) {
-                if (it !is Concrete.Expression) throw IllegalArgumentException()
-                CommandProcessor.getInstance().executeCommand(project, {
-                    invokeOnConcrete(it, project, editor)
-                }, text, null, editor.document)
-            }
-        }
-    }
-
-    private fun cannotSolve(editor: Editor) {
-        ApplicationManager.getApplication().invokeLater {
-            if (!editor.isDisposed) {
-                HintManager.getInstance().showErrorHint(editor, "Cannot solve goal")
-            }
+        solver.solve(CheckTypeVisitor.loadTypecheckingContext(goal.typecheckingContext, project.service<TypeCheckingService>().typecheckerState, project.service<ErrorService>()), goal.causeSourceNode, goal.expectedType, ArendEditorUI(project, editor)) {
+            if (it !is Concrete.Expression) throw IllegalArgumentException()
+            CommandProcessor.getInstance().executeCommand(project, {
+                invokeOnConcrete(it, project, editor)
+            }, text, null, editor.document)
         }
     }
 
@@ -60,12 +44,12 @@ class SolverGoalFillingQuickFix(private val element: ArendExpr, private val goal
 
     override fun startInWriteAction() = false
 
-    override fun getElementToMakeWritable(currentFile: PsiFile) = currentFile
-
     override fun getFamilyName() = "arend.goal"
 
     override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?) =
-        element.isValid && (goal.result != null || goal.goalSolver?.willTrySolve(goal.causeSourceNode, goal.expectedType) == true)
+        element.isValid && goal.typecheckingContext != null && solver.isApplicable(goal.causeSourceNode, goal.expectedType)
 
-    override fun getText() = if (goal.result != null) "Fill goal" else "Solve goal"
+    override fun getElementToMakeWritable(currentFile: PsiFile) = currentFile
+
+    override fun getText(): String = solver.shortDescription
 }
