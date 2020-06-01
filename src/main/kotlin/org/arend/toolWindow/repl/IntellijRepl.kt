fb905b8ca49b3efcfb8dda21603a67cfb23b9014
ice1000 <ice1000kotlin@foxmail.com>
2020-06-01 12:34:49 +0800

    Use PsiInstanceProviderSet

--- src/main/kotlin/org/arend/toolWindow/repl/IntellijRepl.kt
+++ src/main/kotlin/org/arend/toolWindow/repl/IntellijRepl.kt
@@ -11,27 +11,29 @@ import org.arend.repl.Repl
 import org.arend.resolving.PsiConcreteProvider
 import org.arend.term.abs.ConcreteBuilder
 import org.arend.term.group.Group
+import org.arend.typechecking.PsiInstanceProviderSet
 import org.arend.typechecking.TypeCheckingService
 import org.arend.typechecking.execution.PsiElementComparator
-import org.arend.typechecking.instance.provider.InstanceProviderSet
 
 abstract class IntellijRepl private constructor(
     private val service: TypeCheckingService,
     private val refConverter: ReferableConverter,
-    errorReporter: ListErrorReporter
+    errorReporter: ListErrorReporter,
+    psiConcreteProvider: PsiConcreteProvider
 ) : Repl(
     errorReporter,
     service.libraryManager,
-    PsiConcreteProvider(service.project, refConverter, errorReporter, null, false),
+    psiConcreteProvider,
     PsiElementComparator,
-    InstanceProviderSet(),
+    PsiInstanceProviderSet(psiConcreteProvider, refConverter),
     service.typecheckerState
 ) {
     constructor(project: Project) : this(project.service(), ListErrorReporter())
     constructor(service: TypeCheckingService, errorReporter: ListErrorReporter) : this(
         service,
         LocatedReferableConverter(service.newReferableConverter(false)),
-        errorReporter
+        errorReporter,
+        PsiConcreteProvider(service.project, LocatedReferableConverter(service.newReferableConverter(false)), errorReporter, null, false)
     )
 
     init {
