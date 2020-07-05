aa6f113a5376df7e6e5ec4109219561abf10b493
ice1000 <ice1000kotlin@foxmail.com>
2020-07-05 23:35:33 +0800

    Cleanup ArendReplService.kt, upgrade grammar-kit

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     idea
     kotlin("jvm") version "1.3.72"
     id("org.jetbrains.intellij") version "0.4.21"
-    id("org.jetbrains.grammarkit") version "2020.1.4"
+    id("org.jetbrains.grammarkit") version "2020.2.1"
 }
 
 repositories {
