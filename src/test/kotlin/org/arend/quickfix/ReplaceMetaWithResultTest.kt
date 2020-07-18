7480e49d1f8056d59f21f914bdad53b49f6ec8e9
valis <valery.isaev@gmail.com>
2020-07-18 21:21:38 +0300

    Fix tests

--- src/test/kotlin/org/arend/quickfix/ReplaceMetaWithResultTest.kt
+++ src/test/kotlin/org/arend/quickfix/ReplaceMetaWithResultTest.kt
@@ -6,14 +6,15 @@ import org.arend.ext.module.LongName
 import org.arend.ext.module.ModulePath
 import org.arend.ext.reference.ExpressionResolver
 import org.arend.ext.reference.Precedence
-import org.arend.ext.typechecking.MetaDefinition
-import org.arend.ext.typechecking.MetaResolver
+import org.arend.ext.typechecking.*
+import org.arend.term.concrete.Concrete
 
 class ReplaceMetaWithResultTest : QuickFixTestBase() {
     fun `test meta definition`() {
         addGeneratedModules {
             declare(ModulePath("Meta"), LongName("myMeta"), "", Precedence.DEFAULT, object : MetaDefinition {
-                override fun getConcreteRepresentation(arguments: List<ConcreteArgument>) = arguments[1].expression
+                override fun invokeMeta(typechecker: ExpressionTypechecker, contextData: ContextData) =
+                    typechecker.typecheck(contextData.arguments[1].expression, null)
             })
         }
 
@@ -29,7 +30,8 @@ class ReplaceMetaWithResultTest : QuickFixTestBase() {
     fun `test meta resolver`() {
         addGeneratedModules {
             declare(ModulePath("Meta"), LongName("myMeta"), "", Precedence.DEFAULT, null, null, null, object : MetaResolver {
-                override fun resolvePrefix(resolver: ExpressionResolver, refExpr: ConcreteReferenceExpression, arguments: List<ConcreteArgument>) = arguments[1].expression
+                override fun resolvePrefix(resolver: ExpressionResolver, refExpr: ConcreteReferenceExpression, arguments: List<ConcreteArgument>) =
+                    Concrete.TupleExpression(refExpr.data, arguments.map { it.expression as Concrete.Expression })
             })
         }
 
@@ -38,7 +40,7 @@ class ReplaceMetaWithResultTest : QuickFixTestBase() {
             \func test => {-caret-}myMeta 1 2 3
         """, """
             \import Meta
-            \func test => 2
+            \func test => (1, 2, 3)
         """)
     }
 }
