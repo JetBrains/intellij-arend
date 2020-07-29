da00166c72ef85c0312ca89eb36e27ded7dcc2e7
valis <valery.isaev@gmail.com>
2020-07-29 07:35:04 +0300

    v1.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 
 val projectArend = gradle.includedBuild("Arend")
 group = "org.arend.lang"
-version = "1.4.0"
+version = "1.4.1"
 
 plugins {
     idea
@@ -53,7 +53,7 @@ idea {
 }
 
 intellij {
-    // version = "2020.1"
+    version = "2020.2"
     pluginName = "Arend"
     updateSinceUntilBuild = true
     instrumentCode = false
