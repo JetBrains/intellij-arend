cf5deb79ed3201efd797f0762b4b189c79fb53bf
sxhya <sxh@ya.ru>
2020-08-02 22:03:51 +0300

    Fix issue with highlighting

--- src/main/kotlin/org/arend/highlight/ArendTargetElementEvaluator.kt
+++ src/main/kotlin/org/arend/highlight/ArendTargetElementEvaluator.kt
@@ -2,8 +2,10 @@ package org.arend.highlight
 
 import com.intellij.codeInsight.TargetElementEvaluatorEx2
 import com.intellij.psi.PsiElement
+import org.arend.psi.ArendDefIdentifier
 import org.arend.psi.ArendRefIdentifier
 
 class ArendTargetElementEvaluator : TargetElementEvaluatorEx2() {
-    override fun isAcceptableNamedParent(parent: PsiElement) = parent !is ArendRefIdentifier
+    override fun isAcceptableNamedParent(parent: PsiElement) =
+            parent !is ArendRefIdentifier && parent !is ArendDefIdentifier
 }
