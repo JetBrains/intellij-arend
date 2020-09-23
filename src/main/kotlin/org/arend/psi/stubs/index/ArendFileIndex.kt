f8f34204f3d1e3a07aa6438597eaaeeee380baff
sxhya <sxh@ya.ru>
2020-09-23 18:45:57 +0300

    Keep file stubs in separate index

---
+++ src/main/kotlin/org/arend/psi/stubs/index/ArendFileIndex.kt
@@ -0,0 +1,18 @@
+package org.arend.psi.stubs.index
+
+import com.intellij.psi.stubs.StringStubIndexExtension
+import com.intellij.psi.stubs.StubIndexKey
+import org.arend.psi.ArendFile
+import org.arend.psi.stubs.ArendFileStub
+
+class ArendFileIndex : StringStubIndexExtension<ArendFile>() {
+
+    override fun getVersion(): Int = ArendFileStub.Type.stubVersion
+
+    override fun getKey(): StubIndexKey<String, ArendFile> = KEY
+
+    companion object {
+        val KEY: StubIndexKey<String, ArendFile> =
+                StubIndexKey.createIndexKey(ArendDefinitionIndex::class.java.canonicalName)
+    }
+}
