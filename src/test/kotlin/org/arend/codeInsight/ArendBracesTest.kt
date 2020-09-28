d8296a10d8d6012515ea4bf6dcb7ad75077d7b99
ice1000 <ice1000kotlin@foxmail.com>
2020-09-28 14:46:27 +0800

    Add test fixture for #234

---
+++ src/test/kotlin/org/arend/codeInsight/ArendBracesTest.kt
@@ -0,0 +1,19 @@
+package org.arend.codeInsight
+
+import org.arend.ArendTestBase
+
+class ArendBracesTest : ArendTestBase() {
+    private fun checkDoNothing(code: String, type: Char) {
+        val file = InlineFile(code).withCaret()
+        myFixture.type(type)
+        myFixture.checkResult(file.text)
+    }
+
+    fun `test goal braces`() = checkDoNothing("""\func tony => {?{-caret-}}""", '}')
+
+    fun `test simple braces`() = checkDoNothing("""\func beta => lamb {{-caret-}}""", '}')
+
+    fun `test parenthesis`() = checkDoNothing("""\func tonyxu => (tql{-caret-})""", ')')
+
+    fun `test goal expr braces`() = checkDoNothing("""\func tonyxty/ => {?(hStacks){-caret-}}""", '}')
+}
