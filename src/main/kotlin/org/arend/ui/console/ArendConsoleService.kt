85447bdef1ec4dda29ce857a3d9c02b882128dfe
valis <valery.isaev@gmail.com>
2020-10-01 13:21:25 +0300

    Fix synchronization of ARend console during initialization

--- src/main/kotlin/org/arend/ui/console/ArendConsoleService.kt
+++ src/main/kotlin/org/arend/ui/console/ArendConsoleService.kt
@@ -4,20 +4,38 @@ import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 import org.arend.ext.prettyprinting.doc.Doc
 import org.arend.naming.scope.Scope
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class ArendConsoleService(private val project: Project) {
     private var myView: ArendConsoleView? = null
+    private var queue: ConcurrentLinkedQueue<ArendConsoleView.() -> Unit>? = null // needed only during initialization
 
     private fun withView(runnable: ArendConsoleView.() -> Unit) {
         myView?.let {
             return runnable(it)
         }
+
+        synchronized(this) {
+            if (queue == null) {
+                queue = ConcurrentLinkedQueue()
+            }
+            queue?.add(runnable)
+        }
+
         invokeLater {
             synchronized(this) {
-                val view = myView ?: ArendConsoleView(project)
-                myView = view
-                view.toolWindow.show()
-                runnable(view)
+                if (myView == null) {
+                    val view = ArendConsoleView(project)
+                    myView = view
+                    view.toolWindow.show()
+                    while (true) {
+                        val r = queue?.poll()
+                        if (r != null) {
+                            r(view)
+                        } else break
+                    }
+                    queue = null
+                }
             }
         }
     }
