aa7964ae8ee718a2ea4473a1f2eace502881628f
valis <valery.isaev@gmail.com>
2020-10-09 20:19:08 +0300

    1.4.1 -> 1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 
 val projectArend = gradle.includedBuild("Arend")
 group = "org.arend.lang"
-version = "1.4.1"
+version = "1.5.0"
 
 plugins {
     idea
