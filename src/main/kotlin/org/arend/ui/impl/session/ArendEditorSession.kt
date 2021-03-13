2c5e12202020c3fef12a275c80b2aaad455c0e0c
valis <valery.isaev@gmail.com>
2021-03-13 13:51:19 +0300

    Show lookup instead of popup

--- src/main/kotlin/org/arend/ui/impl/session/ArendEditorSession.kt
+++ src/main/kotlin/org/arend/ui/impl/session/ArendEditorSession.kt
@@ -1,26 +1,22 @@
 package org.arend.ui.impl.session
 
+import com.intellij.codeInsight.lookup.*
+import com.intellij.codeInsight.lookup.impl.LookupImpl
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.ex.util.EditorUtil
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.ui.popup.JBPopupListener
-import com.intellij.openapi.ui.popup.LightweightWindowEvent
-import com.intellij.ui.ColoredListCellRenderer
-import org.arend.ArendIcons
 import org.arend.core.definition.Definition
 import org.arend.extImpl.ui.DelegateQuery
 import org.arend.extImpl.ui.SimpleQuery
+import org.arend.naming.reference.Referable
+import org.arend.resolving.ArendReferenceImpl
 import org.arend.typechecking.TypeCheckingService
-import org.arend.ui.cellRenderer.ArendDefinitionListCellRenderer
 import org.arend.ui.ArendDialog
-import javax.swing.*
 
 class ArendEditorSession(private val project: Project, private val editor: Editor) : ComponentSession() {
     private var singleListRequest: ListRequest<Any>? = null
 
-    private class ListRequest<T>(val message: String?, val list: List<T>, val default: T?) {
+    private class ListRequest<T : Any>(val message: String?, val list: List<T>, val default: T?) {
         val query = DelegateQuery<T>()
     }
 
@@ -50,48 +46,31 @@ class ArendEditorSession(private val project: Project, private val editor: Edito
             super.listQuery(message, options, defaultOption)
         }
 
-    private fun <T> showPopup(request: ListRequest<T>) {
+    private fun <T : Any> showPopup(request: ListRequest<T>) {
         val query = SimpleQuery<T>()
         request.query.setDelegate(query)
 
         val service = project.service<TypeCheckingService>()
-        val psiList = request.list.mapNotNull { if (it is Definition) service.getDefinitionPsiReferable(it) else null }
-        val usePsi = psiList.size == request.list.size
-        val builder = if (usePsi) {
-            JBPopupFactory.getInstance().createPopupChooserBuilder(psiList)
-                .setRenderer(ArendDefinitionListCellRenderer)
-                .setItemChosenCallback {
-                    query.result = request.list.getOrNull(psiList.indexOf(it))
-                    endSession(true)
-                }
-        } else {
-            JBPopupFactory.getInstance().createPopupChooserBuilder(request.list)
-                .setRenderer(object : ColoredListCellRenderer<T>() {
-                    override fun customizeCellRenderer(list: JList<out T>, value: T, index: Int, selected: Boolean, hasFocus: Boolean) {
-                        if (value is Definition) {
-                            icon = ArendIcons.definitionToIcon(value)
-                        }
-                        append(value.toString())
-                    }
-                })
-                .setItemChosenCallback {
-                    query.result = it
-                    endSession(true)
-                }
+        val lookupList = request.list.map {
+            val ref = it as? Referable ?: if (it is Definition) service.getDefinitionPsiReferable(it) else null
+            val element = (if (ref != null) ArendReferenceImpl.createArendLookUpElement(ref, null, false, null, false, "")?.withInsertHandler { _, _ -> } else null)
+                ?: LookupElementBuilder.create(it, "")
+            element.withPresentableText(ref?.refName ?: it.toString())
         }
-        builder.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
-            .setFont(EditorUtil.getEditorFont())
-        if (callback != null) {
-            builder.addListener(object : JBPopupListener {
-                override fun onClosed(event: LightweightWindowEvent) {
-                    if (!event.isOk) {
-                        endSession(false)
-                    }
-                }
-            })
+        val lookup = LookupManager.getInstance(project).showLookup(editor, *lookupList.toTypedArray())
+        lookup?.addLookupListener(object : LookupListener {
+            override fun itemSelected(event: LookupEvent) {
+                query.result = request.list.getOrNull(lookupList.indexOf(event.item))
+                endSession(true)
+            }
+
+            override fun lookupCanceled(event: LookupEvent) {
+                endSession(false)
+            }
+        })
+        if (request.message != null) {
+            (lookup as? LookupImpl)?.addAdvertisement(request.message, null)
         }
-        if (request.message != null) builder.setTitle(request.message)
-        builder.createPopup().showInBestPositionFor(editor)
     }
 
     override fun doStart() {
