8fd6afede83989827527e1d501499e15902ca130
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2021-04-02 16:18:50 +0300

    Show Prelude definitions in Navigate Symbol (#254)

--- src/main/kotlin/org/arend/navigation/ArendSymbolNavigationContributor.kt
+++ src/main/kotlin/org/arend/navigation/ArendSymbolNavigationContributor.kt
@@ -1,9 +1,16 @@
 package org.arend.navigation
 
+import org.arend.core.definition.Definition
+import org.arend.prelude.Prelude
 import org.arend.psi.ext.PsiReferable
 import org.arend.psi.stubs.index.ArendNamedElementIndex
 
 class ArendSymbolNavigationContributor : ArendNavigationContributorBase<PsiReferable>(
-        ArendNamedElementIndex.KEY,
-        PsiReferable::class.java
-)
+    ArendNamedElementIndex.KEY,
+    PsiReferable::class.java
+) {
+    override fun getPreludeDefinitions(): List<Definition> =
+        mutableListOf<Definition>().apply {
+            Prelude.forEach { def -> add(def) }
+        }
+}
