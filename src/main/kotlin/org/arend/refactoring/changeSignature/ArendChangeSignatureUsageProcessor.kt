626c13aae0cf0b145e2410991f1fa2c2c8ea2b3e
sloboegen98 <sloboegen98@ya.ru>
2021-08-06 19:42:40 +0300

    add ChangeInfo and UsageProcessors

--- src/main/kotlin/org/arend/refactoring/changeSignature/ArendChangeSignatureUsageProcessor.kt
+++ src/main/kotlin/org/arend/refactoring/changeSignature/ArendChangeSignatureUsageProcessor.kt
@@ -3,19 +3,31 @@ package org.arend.refactoring.changeSignature
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.refactoring.changeSignature.ChangeInfo
 import com.intellij.refactoring.changeSignature.ChangeSignatureUsageProcessor
 import com.intellij.refactoring.rename.ResolveSnapshotProvider
+import com.intellij.refactoring.suggested.endOffset
+import com.intellij.refactoring.suggested.startOffset
 import com.intellij.usageView.UsageInfo
 import com.intellij.util.containers.MultiMap
+import com.intellij.util.containers.toArray
 
 class ArendChangeSignatureUsageProcessor : ChangeSignatureUsageProcessor {
     override fun findUsages(info: ChangeInfo?): Array<UsageInfo> {
-        TODO("Not yet implemented")
+        info ?: return emptyArray()
+        val usages: MutableList<UsageInfo> = mutableListOf()
+        for (usage in ReferencesSearch.search(info.method)) {
+            with(usage.element) {
+                usages += UsageInfo(this, startOffset, endOffset)
+            }
+        }
+        return usages.toTypedArray()
     }
 
     override fun findConflicts(info: ChangeInfo?, refUsages: Ref<Array<UsageInfo>>?): MultiMap<PsiElement, String> {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        return MultiMap.empty()
     }
 
     override fun processUsage(
@@ -24,24 +36,25 @@ class ArendChangeSignatureUsageProcessor : ChangeSignatureUsageProcessor {
         beforeMethodChange: Boolean,
         usages: Array<out UsageInfo>?
     ): Boolean {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        return false
     }
 
     override fun processPrimaryMethod(changeInfo: ChangeInfo?): Boolean {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        return false
     }
 
     override fun shouldPreviewUsages(changeInfo: ChangeInfo?, usages: Array<out UsageInfo>?): Boolean {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        return false
     }
 
     override fun setupDefaultValues(
         changeInfo: ChangeInfo?,
         refUsages: Ref<Array<UsageInfo>>?,
         project: Project?
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
+    ): Boolean = false
 
     override fun registerConflictResolvers(
         snapshots: MutableList<ResolveSnapshotProvider.ResolveSnapshot>?,
@@ -49,6 +62,6 @@ class ArendChangeSignatureUsageProcessor : ChangeSignatureUsageProcessor {
         usages: Array<out UsageInfo>?,
         changeInfo: ChangeInfo?
     ) {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 }
