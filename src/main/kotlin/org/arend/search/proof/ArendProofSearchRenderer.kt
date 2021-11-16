f900da42915a93f472bde9057a8a04b99f141f52
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2021-11-16 14:31:05 +0300

    Arend Proof Search: Improve renderer
    Swing is hard

--- src/main/kotlin/org/arend/search/proof/ArendProofSearchRenderer.kt
+++ src/main/kotlin/org/arend/search/proof/ArendProofSearchRenderer.kt
@@ -6,27 +6,46 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.ui.ColoredListCellRenderer
 import com.intellij.ui.SimpleTextAttributes
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextArea
+import com.intellij.ui.components.panels.OpaquePanel
 import com.intellij.ui.scale.JBUIScale
 import com.intellij.ui.speedSearch.SpeedSearchUtil
-import com.intellij.util.SlowOperations
 import com.intellij.util.castSafelyTo
+import com.intellij.util.ui.JBInsets
+import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import org.arend.psi.ext.PsiReferable
 import org.arend.search.structural.PatternTree
 import org.arend.term.abs.Abstract
-import java.awt.BorderLayout
-import java.awt.Component
+import java.awt.*
+import java.awt.font.FontRenderContext
+import java.awt.font.LineMetrics
+import java.awt.geom.Rectangle2D
+import java.text.CharacterIterator
+import javax.swing.BorderFactory
 import javax.swing.JList
 import javax.swing.JPanel
 import javax.swing.ListCellRenderer
+import javax.swing.text.DefaultHighlighter
 
 data class ProofSearchEntry(val def : PsiReferable, val tree : PatternTree)
 
+private val BORDER = BorderFactory.createEmptyBorder(3, 0, 5, 0)
 
-class ArendProofSearchRenderer : JPanel(BorderLayout()), ListCellRenderer<Any> {
+class ArendProofSearchRenderer : ListCellRenderer<Any> {
+    val panel: JPanel = OpaquePanel(SearchEverywherePsiRenderer.SELayout())
+    val iconPanel: JPanel = JPanel(BorderLayout())
+    val label: JBLabel = JBLabel()
+    val textArea: JBTextArea = JBTextArea() // todo: html document
+
     init {
-        layout = SearchEverywherePsiRenderer.SELayout()
+        iconPanel.add(label, BorderLayout.NORTH)
+
+        panel.add(iconPanel, BorderLayout.WEST)
+        textArea.lineWrap = true
+        textArea.wrapStyleWord = true
+        panel.add(textArea, BorderLayout.CENTER)
     }
 
     override fun getListCellRendererComponent(
@@ -36,17 +55,41 @@ class ArendProofSearchRenderer : JPanel(BorderLayout()), ListCellRenderer<Any> {
         isSelected: Boolean,
         cellHasFocus: Boolean
     ): Component {
-        removeAll()
-        val leftRenderer: ListCellRenderer<Any> = CustomLeftRenderer()
-        var result: Component
-        SlowOperations.allowSlowOperations(SlowOperations.RENDERING).use { ignore ->
-            result = leftRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+        value as FoundItemDescriptor<ProofSearchEntry>
+        val def = value.item.def
+        val type =
+            def.castSafelyTo<Abstract.FunctionDefinition>()?.resultType?.castSafelyTo<PsiElement>() ?: return panel
+        textArea.text = def.name + " : " + type.text
+        val width = list.width
+        if (width > 0) {
+            textArea.setSize(width, Short.MAX_VALUE.toInt())
         }
-        val leftCellRendererComponent = result
-        add(leftCellRendererComponent, BorderLayout.WEST)
-        val bg = if (isSelected) UIUtil.getListSelectionBackground(true) else leftCellRendererComponent.background
-        background = bg
-        return this
+        textArea.border = BORDER
+
+
+        val bgColor = if (isSelected) UIUtil.getListSelectionBackground(true) else UIUtil.getListBackground()
+        val textColor = if (isSelected) list.selectionForeground else list.foreground
+        textArea.background = bgColor
+        panel.background = bgColor
+        label.background = bgColor
+        iconPanel.background = bgColor
+        textArea.foreground = textColor
+        textArea.rows
+        val icon = def.getIcon(0)
+        label.icon = icon
+        label.border = BorderFactory.createEmptyBorder(3 + textArea.getFontMetrics(textArea.font).height - icon.iconHeight, 0, 5, 2)
+        return panel
+//        removeAll()
+//        val leftRenderer: ListCellRenderer<Any> = CustomLeftRenderer()
+//        var result: Component
+//        SlowOperations.allowSlowOperations(SlowOperations.RENDERING).use { ignore ->
+//            result = leftRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+//        }
+//        val leftCellRendererComponent = result
+//        add(leftCellRendererComponent, BorderLayout.WEST)
+//        val bg = if (isSelected) UIUtil.getListSelectionBackground(true) else leftCellRendererComponent.background
+//        background = bg
+//        return this
     }
 
     private class CustomLeftRenderer : ColoredListCellRenderer<Any>() {
