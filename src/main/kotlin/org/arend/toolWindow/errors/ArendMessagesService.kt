6234b94433b68fcccc374ed5de2f258263c8d032
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2021-12-09 17:54:53 +0300

    Use word "message" instead of "errors" in the UI

--- src/main/kotlin/org/arend/toolWindow/errors/ArendMessagesService.kt
+++ src/main/kotlin/org/arend/toolWindow/errors/ArendMessagesService.kt
@@ -16,7 +16,7 @@ class ArendMessagesService(private val project: Project) {
 
     fun activate(project: Project, selectFirst: Boolean) {
         runInEdt {
-            ToolWindowManager.getInstance(project).getToolWindow("Arend Errors")?.activate(if (selectFirst) Runnable {
+            ToolWindowManager.getInstance(project).getToolWindow("Arend Messages")?.activate(if (selectFirst) Runnable {
                 val service = project.service<ArendMessagesService>()
                 val view = service.view
                 if (view != null) {
