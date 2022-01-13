3073d8fb67a7a1bf4b44291a9826a0f2f2e386fb
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-01-13 13:21:38 +0500

    [Arend Messages] Reorder and group actions

--- src/main/kotlin/org/arend/toolWindow/errors/ArendMessagesViewEditor.kt
+++ src/main/kotlin/org/arend/toolWindow/errors/ArendMessagesViewEditor.kt
@@ -26,11 +26,13 @@ class ArendMessagesViewEditor(project: Project, treeElement: ArendErrorTreeEleme
 
     private fun setupActions() {
         if (isGoalEditor) {
-            actionGroup.add(ActionManager.getInstance().getAction(ArendClearGoalAction.ID))
             actionGroup.add(ActionManager.getInstance().getAction(ArendPinGoalAction.ID))
+            actionGroup.add(ActionManager.getInstance().getAction(ArendClearGoalAction.ID))
+            actionGroup.addSeparator()
             actionGroup.add(createPrintOptionsActionGroup())
         } else {
             actionGroup.add(ActionManager.getInstance().getAction(ArendPinErrorAction.ID))
+            actionGroup.addSeparator()
             actionGroup.add(createPrintOptionsActionGroup())
             actionGroup.add(ArendShowGoalsInErrorsPanelAction())
         }
