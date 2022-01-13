1c4694152d8acc7ed6cf233639db5ed7a1abd126
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-01-13 12:47:40 +0500

    [Arend Messages] Add "Pin Error" action

---
+++ src/main/kotlin/org/arend/toolWindow/errors/ArendPinErrorAction.kt
@@ -0,0 +1,28 @@
+package org.arend.toolWindow.errors
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ToggleAction
+import com.intellij.openapi.components.service
+import org.arend.ArendIcons
+import org.arend.util.ArendBundle
+
+class ArendPinErrorAction : ToggleAction(
+        ArendBundle.message("arend.pin.error.action.name"),
+        ArendBundle.message("arend.pin.error.action.description"),
+        ArendIcons.PIN
+) {
+    override fun isSelected(e: AnActionEvent): Boolean =
+            e.project?.service<ArendMessagesService>()?.isErrorTextPinned ?: false
+
+    override fun setSelected(e: AnActionEvent, state: Boolean) {
+        val service = e.project?.service<ArendMessagesService>() ?: return
+        service.isErrorTextPinned = state
+        if (!state) {
+            service.updateEditors()
+        }
+    }
+
+    companion object {
+        const val ID = "Arend.PinError"
+    }
+}
