afee0e619476b92114968521f6c5001e8be5d8b1
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-01-13 11:57:19 +0500

    [Arend Messages] Refactor ArendPinGoalAction

--- src/main/kotlin/org/arend/toolWindow/errors/ArendPinGoalAction.kt
+++ src/main/kotlin/org/arend/toolWindow/errors/ArendPinGoalAction.kt
@@ -1,7 +1,6 @@
 package org.arend.toolWindow.errors
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.components.service
 import org.arend.ArendIcons
@@ -12,19 +11,16 @@ class ArendPinGoalAction : ToggleAction(
         ArendBundle.message("arend.pin.goal.action.description"),
         ArendIcons.PIN
 ) {
-    override fun isSelected(e: AnActionEvent): Boolean {
-        val service = getMessagesService(e)
-        return service?.isGoalTextPinned ?: false
-    }
+    override fun isSelected(e: AnActionEvent): Boolean =
+            e.project?.service<ArendMessagesService>()?.isGoalTextPinned ?: false
 
     override fun setSelected(e: AnActionEvent, state: Boolean) {
-        val service = getMessagesService(e)
-        service?.isGoalTextPinned = state
-        if (!state) service?.updateEditors()
+        val service = e.project?.service<ArendMessagesService>() ?: return
+        service.isGoalTextPinned = state
+        if (!state) {
+            service.updateEditors()
+        }
     }
-
-    private fun getMessagesService(e: AnActionEvent): ArendMessagesService? =
-            e.dataContext.getData(PlatformDataKeys.PROJECT)?.service()
 
     companion object {
         const val ID = "Arend.PinGoal"
