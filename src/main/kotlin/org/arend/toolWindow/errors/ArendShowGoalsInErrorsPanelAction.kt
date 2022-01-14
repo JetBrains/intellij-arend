7ec32ee74224596088030129e2cd4643292c4af9
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-01-14 11:26:51 +0500

    [Arend Messages] Different icon for goals

--- src/main/kotlin/org/arend/toolWindow/errors/ArendShowGoalsInErrorsPanelAction.kt
+++ src/main/kotlin/org/arend/toolWindow/errors/ArendShowGoalsInErrorsPanelAction.kt
@@ -1,20 +1,28 @@
 package org.arend.toolWindow.errors
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.components.service
-import org.arend.ArendIcons
+import com.intellij.ui.LayeredIcon
 import org.arend.util.ArendBundle
 
 class ArendShowGoalsInErrorsPanelAction : ToggleAction(
         ArendBundle.message("arend.show.goals.in.errors.panel.action.name"),
         ArendBundle.message("arend.show.goals.in.errors.panel.action.description"),
-        ArendIcons.GOAL
+        ICON
 ) {
     override fun isSelected(e: AnActionEvent): Boolean =
             e.project?.service<ArendMessagesService>()?.isShowGoalsInErrorsPanel?.get() ?: true
 
     override fun setSelected(e: AnActionEvent, state: Boolean) {
         e.project?.service<ArendMessagesService>()?.isShowGoalsInErrorsPanel?.set(state)
+    }
+
+    companion object {
+        private val ICON = LayeredIcon(2).apply {
+            setIcon(AllIcons.Actions.PrettyPrint, 0)
+            setIcon(AllIcons.Debugger.Question_badge, 1, 4, 3)
+        }
     }
 }
