5f70e345af978933fc2beaeecbfdcc8a2917bc40
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-02-15 13:03:33 +0300

    Arend Signature Search: Use editor for rendering results

--- src/main/kotlin/org/arend/search/proof/ArendSignatureSearchRenderer.kt
+++ src/main/kotlin/org/arend/search/proof/ArendSignatureSearchRenderer.kt
@@ -2,23 +2,24 @@ package org.arend.search.proof
 
 import com.intellij.icons.AllIcons
 import com.intellij.ide.actions.SearchEverywherePsiRenderer
-import com.intellij.openapi.vfs.newvfs.VfsPresentationUtil
-import com.intellij.psi.util.PsiUtilCore
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.editor.impl.DocumentImpl
+import com.intellij.openapi.editor.markup.HighlighterLayer
+import com.intellij.openapi.editor.markup.HighlighterTargetArea
+import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextArea
 import com.intellij.ui.components.panels.OpaquePanel
 import com.intellij.util.ui.UIUtil
 import okhttp3.internal.toHexString
-import org.apache.commons.lang.StringEscapeUtils.escapeHtml
-import org.arend.psi.ArendFile
 import org.arend.psi.ext.PsiReferable
-import org.arend.psi.ext.impl.*
-import org.arend.term.concrete.Concrete
+import org.arend.psi.ext.impl.CoClauseDefAdapter
 import java.awt.BorderLayout
 import java.awt.Color
 import java.awt.Component
+import java.awt.Font
 import javax.swing.*
-import javax.swing.border.Border
 
 class ArendSignatureSearchRenderer : ListCellRenderer<SignatureSearchUIEntry> {
     private val panel: JPanel = OpaquePanel(SearchEverywherePsiRenderer.SELayout())
@@ -26,10 +27,35 @@ class ArendSignatureSearchRenderer : ListCellRenderer<SignatureSearchUIEntry> {
     private val label: JBLabel = JBLabel()
     private val textArea = JEditorPane()
 
+    private val document = DocumentImpl("", true)
+
+    private val editor: EditorEx = EditorFactory.getInstance().createViewer(document) as EditorEx
+
     init {
+        textArea.contentType = "text/html"
+        textArea.border = BORDER
+        textArea.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
+        textArea.font = JBTextArea().font
         iconPanel.add(label, BorderLayout.NORTH)
         panel.add(iconPanel, BorderLayout.WEST)
         panel.add(textArea, BorderLayout.CENTER)
+        with(editor.settings) {
+            isRightMarginShown = false
+            isLineNumbersShown = false
+            isLineMarkerAreaShown = false
+            isRefrainFromScrolling = true
+            isCaretRowShown = false
+            isUseSoftWraps = true
+            setGutterIconsShown(false)
+            additionalLinesCount = 0
+            additionalColumnsCount = 1
+            isFoldingOutlineShown = false
+            isVirtualSpace = false
+        }
+        editor.headerComponent = null
+        editor.setHorizontalScrollbarVisible(false)
+        editor.setVerticalScrollbarVisible(false)
+        editor.isRendererMode = true
     }
 
     override fun getListCellRendererComponent(
@@ -39,77 +65,43 @@ class ArendSignatureSearchRenderer : ListCellRenderer<SignatureSearchUIEntry> {
         isSelected: Boolean,
         cellHasFocus: Boolean
     ): Component {
-        val textColor = if (isSelected) list.selectionForeground else list.foreground
-        val bgColor = getBackgroundColor(isSelected, value)
-
-        textArea.contentType = "text/html"
-        textArea.border = BORDER
-        textArea.foreground = textColor
-        textArea.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
-        textArea.font = JBTextArea().font
-        textArea.background = bgColor
-
-        panel.background = bgColor
-        label.background = bgColor
-        iconPanel.background = bgColor
-
-        addContent(value, textColor, isSelected)
-        return panel
-    }
-
-    fun addContent(value: SignatureSearchUIEntry, textColor: Color, isSelected: Boolean) = when (value) {
-        is MoreElement -> {
-            panel.font = UIUtil.getLabelFont().deriveFont(UIUtil.getFontSize(UIUtil.FontSize.SMALL))
-            textArea.text = buildMoreHTML(textColor)
-            panel.border = null
-            label.icon = null
-        }
-        is DefElement -> {
-            val (def, type) = value.entry
-            val auxiliaryTextColor = if (isSelected) textColor else UIUtil.getInactiveTextColor()
-            textArea.text = buildHtml(def, type, auxiliaryTextColor)
-            val icon = getIcon(def)
-            label.icon = icon
-            label.border = generateLabelBorder(textArea, icon)
+        val bgColor = if (isSelected) UIUtil.getListSelectionBackground(true) else UIUtil.getListBackground()
+        val fgColor = if (isSelected) list.selectionForeground else list.foreground
+        return when (value) {
+            is DefElement -> {
+                val (def, codomain) = value.entry
+                val text = def.name + " : " + codomain.toString()
+                editor.markupModel.removeAllHighlighters()
+                document.setText(text)
+                editor.backgroundColor = bgColor
+                val attrs = TextAttributes(fgColor, null, null, null, Font.PLAIN)
+                editor.markupModel.addRangeHighlighter(
+                    0,
+                    text.length,
+                    HighlighterLayer.SELECTION,
+                    attrs,
+                    HighlighterTargetArea.EXACT_RANGE
+                )
+                editor.component
+            }
+            is MoreElement -> {
+                textArea.background = bgColor
+                textArea.foreground = fgColor
+                panel.font = UIUtil.getLabelFont().deriveFont(UIUtil.getFontSize(UIUtil.FontSize.SMALL))
+                textArea.text = buildMoreHTML(fgColor)
+                panel.border = null
+                label.icon = null
+                panel
+            }
         }
     }
 }
 
 private val BORDER = BorderFactory.createEmptyBorder(3, 0, 5, 0)
 
-private fun buildHtml(
-    def: ReferableAdapter<*>,
-    type: Concrete.Expression,
-    nameColor: Color
-): String? {
-    val name = def.name ?: return null
-    val typeRepresentation = (if (def is FunctionDefinitionAdapter) def.resultType?.text else null) ?: type.toString()
-    val locationText = (def.containingFile as ArendFile).moduleLocation?.modulePath?.toString() ?: ""
-    return buildDefinitionHTML(name, typeRepresentation, locationText, nameColor)
-}
-
 private fun Color.asHex(): String {
     return "#${red.toHexString()}${blue.toHexString()}${green.toHexString()}"
 }
-
-private fun buildDefinitionHTML(
-    name: String,
-    type: String,
-    locationText: String,
-    secondaryColor: Color
-): String {
-    val locationRepresentation =
-        if (locationText != "") "<span style=\"color: ${secondaryColor.asHex()}\">(in ${escapeHtml(locationText)})</span>" else ""
-    return """
-           <html>
-           <body>
-           ${escapeHtml(name)} : ${optionalNewline(type)} <b>${escapeHtml(type).replace("\n", "<br>")}</b> $locationRepresentation
-           </body>
-           </html>
-        """.trimIndent()
-}
-
-fun optionalNewline(type: String): String = if (type.contains('\n')) "\n" else ""
 
 private fun buildMoreHTML(nameColor: Color): String = """
        <html>
@@ -119,27 +111,7 @@ private fun buildMoreHTML(nameColor: Color): String = """
        </html>
     """.trimIndent()
 
-private fun getBackgroundColor(isSelected: Boolean, element: SignatureSearchUIEntry): Color =
-    when (isSelected) {
-        true -> UIUtil.getListSelectionBackground(true)
-        false -> when (element) {
-            is DefElement -> {
-                val file = PsiUtilCore.getVirtualFile(element.entry.def)
-                if (file != null) {
-                    VfsPresentationUtil.getFileBackgroundColor(element.entry.def.project, file)
-                        ?: UIUtil.getListBackground()
-                } else {
-                    UIUtil.getListBackground()
-                }
-            }
-            is MoreElement -> UIUtil.getListBackground()
-        }
-    }
-
 private fun getIcon(def: PsiReferable): Icon = when (def) {
     is CoClauseDefAdapter -> AllIcons.General.Show_to_implement
     else -> def.getIcon(0)
 }
-
-private fun generateLabelBorder(textArea: JEditorPane, icon: Icon): Border =
-    BorderFactory.createEmptyBorder(1 + textArea.getFontMetrics(textArea.font).height - icon.iconHeight, 0, 5, 2)
