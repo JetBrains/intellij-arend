ac4d1cec2d8993fcc21b8afa5968f9caac57fbbf
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-02-17 17:11:31 +0300

    Add pretty printing options for tracer

--- src/main/kotlin/org/arend/tracer/ArendTraceContextView.kt
+++ src/main/kotlin/org/arend/tracer/ArendTraceContextView.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
 import com.intellij.psi.SmartPointerManager
 import org.arend.injection.InjectedArendEditor
+import org.arend.toolWindow.errors.ArendPrintOptionsActionGroup
 import org.arend.toolWindow.errors.PrintOptionKind
 import org.arend.toolWindow.errors.tree.ArendErrorTreeElement
 import org.arend.typechecking.error.ArendError
@@ -11,6 +12,10 @@ import org.arend.util.ArendBundle
 
 class ArendTraceContextView(project: Project) : InjectedArendEditor(project, "Arend Trace Context", null) {
     override val printOptionKind: PrintOptionKind = PrintOptionKind.TRACER_PRINT_OPTIONS
+
+    init {
+        actionGroup.add(ArendPrintOptionsActionGroup(project, PrintOptionKind.TRACER_PRINT_OPTIONS, ::updateErrorText))
+    }
 
     fun update(traceEntry: ArendTraceEntry) {
         val psiElement = traceEntry.psiElement
