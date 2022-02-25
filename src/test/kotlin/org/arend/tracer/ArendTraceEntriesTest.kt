47cc90cabad7c7fd44e633bfd43bb04ace9ce0b8
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-02-25 12:00:58 +0300

    Extract base class from trace test

--- src/test/kotlin/org/arend/tracer/ArendTraceEntriesTest.kt
+++ src/test/kotlin/org/arend/tracer/ArendTraceEntriesTest.kt
@@ -1,74 +1,55 @@
 package org.arend.tracer
 
 import com.intellij.util.castSafelyTo
-import org.arend.ArendTestBase
-import org.arend.error.DummyErrorReporter
 import org.arend.psi.ArendDefFunction
 import org.arend.psi.ArendFile
-import org.arend.resolving.PsiConcreteProvider
-import org.arend.term.concrete.Concrete
 
-class ArendTraceActionTest : ArendTestBase() {
+class ArendTraceEntriesTest : ArendTraceTestBase() {
     companion object {
         const val pmap =
             """\func pmap {A B : \Type} (f : A -> B) {a a' : A} (p : a = a') : f a = f a' => path (\lam i => f (p @ i))"""
     }
 
     fun `test first trace entry 1`() {
-        InlineFile(
+        val tracingData = doTrace(
             """
             $pmap
             \func test (a b : Nat) (p : a = b) => pmap suc (pmap suc {-caret-}p)
         """
-        ).withCaret()
-        val tracingData = doTrace()
+        )
         assertEquals("p", getFirstEntry(tracingData)?.psiElement?.text)
     }
 
     fun `test first trace entry 2`() {
-        InlineFile(
+        val tracingData = doTrace(
             """
             $pmap
             \func test (a b : Nat) (p : a = b) => pmap suc (pmap s{-caret-}uc p)
         """
-        ).withCaret()
-        val tracingData = doTrace()
+        )
         assertEquals("suc", getFirstEntry(tracingData)?.psiElement?.text)
     }
 
     fun `test first trace entry 3`() {
-        InlineFile(
+        val tracingData = doTrace(
             """
             $pmap
             \func test (a b : Nat) (p : a = b) => pmap suc (p{-caret-}map suc p)
         """
-        ).withCaret()
-        val tracingData = doTrace()
+        )
         assertEquals("pmap suc p", getFirstEntry(tracingData)?.psiElement?.text)
     }
 
     fun `test no trace entry outside function signature`() {
-        InlineFile(
+        val tracingData = doTrace(
             """
             $pmap
             \func test (a b : Nat) (p : a = b) => {-caret-}pmap suc (pmap suc p)
         """
-        ).withCaret()
-        val tracingData = doTrace()
+        )
         assertEquals("pmap suc (pmap suc p)", getFirstEntry(tracingData)?.psiElement?.text)
         val funcKw = myFixture.file.castSafelyTo<ArendFile>()!!.statements[1]
             .definition!!.castSafelyTo<ArendDefFunction>()!!.functionKw
         assertEquals(-1, tracingData.trace.indexOfEntry(funcKw))
     }
-
-    private fun doTrace(): ArendTracingData {
-        val (expression, definitionRef) =
-            ArendTraceAction.getElementAtCursor(myFixture.file, myFixture.editor)!!
-        val definition = PsiConcreteProvider(project, DummyErrorReporter.INSTANCE, null, true)
-            .getConcrete(definitionRef).castSafelyTo<Concrete.Definition>()!!
-        return ArendTraceAction.runTracingTypechecker(project, definition, expression)
-    }
-
-    private fun getFirstEntry(tracingData: ArendTracingData) =
-        tracingData.trace.entries.getOrNull(tracingData.firstEntryIndex)
 }
---
+++ src/test/kotlin/org/arend/tracer/ArendTraceTestBase.kt
@@ -0,0 +1,22 @@
+package org.arend.tracer
+
+import com.intellij.util.castSafelyTo
+import org.arend.ArendTestBase
+import org.arend.error.DummyErrorReporter
+import org.arend.resolving.PsiConcreteProvider
+import org.arend.term.concrete.Concrete
+import org.intellij.lang.annotations.Language
+
+abstract class ArendTraceTestBase : ArendTestBase() {
+    protected fun doTrace(@Language("Arend") code: String): ArendTracingData {
+        InlineFile(code).withCaret()
+        val (expression, definitionRef) =
+            ArendTraceAction.getElementAtCursor(myFixture.file, myFixture.editor)!!
+        val definition = PsiConcreteProvider(project, DummyErrorReporter.INSTANCE, null, true)
+            .getConcrete(definitionRef).castSafelyTo<Concrete.Definition>()!!
+        return ArendTraceAction.runTracingTypechecker(project, definition, expression)
+    }
+
+    protected fun getFirstEntry(tracingData: ArendTracingData) =
+        tracingData.trace.entries.getOrNull(tracingData.firstEntryIndex)
+}
