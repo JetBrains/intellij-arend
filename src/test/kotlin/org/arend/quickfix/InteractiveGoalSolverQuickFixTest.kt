855f037325e142f8ec7b624bb01dac27c151e065
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-03-16 16:26:42 +0300

    Fix formatting

--- src/test/kotlin/org/arend/quickfix/InteractiveGoalSolverQuickFixTest.kt
+++ src/test/kotlin/org/arend/quickfix/InteractiveGoalSolverQuickFixTest.kt
@@ -15,31 +15,39 @@ import org.intellij.lang.annotations.Language
 import java.util.function.Consumer
 
 class InteractiveGoalSolverQuickFixTest : QuickFixTestBase() {
-    fun `test caret is moved to the first goal`() = doTestWithGoalSolver(TestGoalSolver("suc {?}", project), """
-        \func test : Nat => {-caret-}{?}
-    """, """
-        \func test : Nat => suc {-caret-}{?}
-    """)
+    fun `test caret is moved to the first goal`() = doTestWithGoalSolver(
+        TestGoalSolver("suc {?}", project), """
+            \func test : Nat => {-caret-}{?}
+        """, """
+            \func test : Nat => suc {-caret-}{?}
+        """
+    )
 
-    fun `test caret is not moved when no goals`() = doTestWithGoalSolver(TestGoalSolver("zero", project), """
-        \func test : Nat => {-caret-}{?}
-    """, """
-        \func test : Nat => {-caret-}zero
-    """)
+    fun `test caret is not moved when no goals`() = doTestWithGoalSolver(
+        TestGoalSolver("zero", project), """
+            \func test : Nat => {-caret-}{?}
+        """, """
+            \func test : Nat => {-caret-}zero
+        """
+    )
 
-    fun `test caret is moved when result expression is wrapped in parens`() = doTestWithGoalSolver(TestGoalSolver("suc {?}", project), """
-        \func test : Nat => suc {-caret-}{?}
-    """, """
-        \func test : Nat => suc (suc {-caret-}{?})
-    """)
+    fun `test caret is moved when result expression is wrapped in parens`() = doTestWithGoalSolver(
+        TestGoalSolver("suc {?}", project), """
+            \func test : Nat => suc {-caret-}{?}
+        """, """
+            \func test : Nat => suc (suc {-caret-}{?})
+        """
+    )
 
-    private fun doTestWithGoalSolver(solver: InteractiveGoalSolver,
-                                     @Language("Arend") before: String,
-                                     @Language("Arend") after: String) {
+    private fun doTestWithGoalSolver(
+        solver: InteractiveGoalSolver,
+        @Language("Arend") before: String,
+        @Language("Arend") after: String
+    ) {
         library.arendExtension = object : ArendExtension {
             override fun getGoalSolver() = object : GoalSolver {
                 override fun getAdditionalSolvers(): MutableCollection<out InteractiveGoalSolver> =
-                        mutableListOf(solver)
+                    mutableListOf(solver)
             }
         }
         typedQuickFixTest(solver.shortDescription, before, after)
@@ -50,11 +58,13 @@ class InteractiveGoalSolverQuickFixTest : QuickFixTestBase() {
 
         override fun isApplicable(goalExpression: ConcreteGoalExpression, expectedType: CoreExpression?): Boolean = true
 
-        override fun solve(typechecker: ExpressionTypechecker,
-                           goalExpression: ConcreteGoalExpression,
-                           expectedType: CoreExpression?,
-                           ui: ArendUI,
-                           callback: Consumer<ConcreteExpression>) {
+        override fun solve(
+            typechecker: ExpressionTypechecker,
+            goalExpression: ConcreteGoalExpression,
+            expectedType: CoreExpression?,
+            ui: ArendUI,
+            callback: Consumer<ConcreteExpression>
+        ) {
             val exprPsi = ArendPsiFactory(project).createExpression(solution)
             val expr = ConcreteBuilder.convertExpression(exprPsi)
             callback.accept(expr)
