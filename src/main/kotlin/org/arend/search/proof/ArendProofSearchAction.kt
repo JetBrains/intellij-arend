c72b4fa5eab7a1e797bee7ac5bf6a5c95ec71d5f
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-04-10 10:14:47 +0300

    Show proof search only on Arend files

--- src/main/kotlin/org/arend/search/proof/ArendProofSearchAction.kt
+++ src/main/kotlin/org/arend/search/proof/ArendProofSearchAction.kt
@@ -2,10 +2,16 @@ package org.arend.search.proof
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.components.service
 import org.arend.ArendIcons
+import org.arend.ArendLanguage
 
 class ArendProofSearchAction : AnAction(ArendIcons.AREND) {
+    override fun update(e: AnActionEvent) {
+        e.presentation.isEnabledAndVisible = e.getData(LangDataKeys.LANGUAGE) == ArendLanguage.INSTANCE
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         e.project?.service<ProofSearchService>()?.show(e)
     }
