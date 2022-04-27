95b9290e3c98678ce216d57825af3e135a85ea70
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2022-04-27 19:24:28 +0400

    Add a test for ArendFile.isBackgroundTypecheckingFinished

---
+++ src/test/kotlin/org/arend/psi/ArendFileModificationCountTest.kt
@@ -0,0 +1,30 @@
+package org.arend.psi
+
+import com.intellij.psi.PsiDocumentManager
+import org.arend.ArendTestBase
+
+class ArendFileModificationCountTest : ArendTestBase() {
+    fun `test background type checking finished`() {
+        val file = InlineFile("""\func foo => 1{-caret-}""").withCaret() as ArendFile
+        myFixture.doHighlighting()
+        assertTrue(file.isBackgroundTypecheckingFinished)
+        type("2")
+        assertFalse(file.isBackgroundTypecheckingFinished)
+        myFixture.doHighlighting()
+        assertTrue(file.isBackgroundTypecheckingFinished)
+    }
+
+    fun `test background type checking not finished between typing and highlighting`() {
+        val file = InlineFile("""\func foo => 1{-caret-}""").withCaret() as ArendFile
+        type("2")
+        myFixture.doHighlighting()
+        assertTrue(file.isBackgroundTypecheckingFinished)
+        type("3")
+        assertFalse(file.isBackgroundTypecheckingFinished)
+    }
+
+    private fun type(str: String) {
+        myFixture.type(str)
+        PsiDocumentManager.getInstance(project).commitAllDocuments()
+    }
+}
