73804431b35a5759dbae384e76ae8da08fa00890
valis <valery.isaev@gmail.com>
2022-05-03 23:50:49 +0300

    v1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 
 val projectArend = gradle.includedBuild("Arend")
 group = "org.arend.lang"
-version = "1.7.0.2"
+version = "1.8.0"
 
 plugins {
     idea
