d85bfc387b1935a89f5f475ab8a6cf6c159e8d13
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2022-08-09 13:03:09 +0300

    Recognize patterns as lambda parameters

--- src/main/kotlin/org/arend/psi/parser/api/ArendPattern.kt
+++ src/main/kotlin/org/arend/psi/parser/api/ArendPattern.kt
@@ -4,12 +4,13 @@ import com.intellij.psi.PsiElement
 import org.arend.naming.reference.UnresolvedReference
 import org.arend.psi.ArendAsPattern
 import org.arend.psi.ArendExpr
+import org.arend.psi.ArendLamParam
 import org.arend.psi.ArendLongName
 import org.arend.psi.ext.ArendCompositeElement
 import org.arend.psi.ext.ArendReferenceContainer
 import org.arend.term.abs.Abstract
 
-interface ArendPattern : ArendCompositeElement, Abstract.Pattern {
+interface ArendPattern : ArendCompositeElement, ArendLamParam, Abstract.Pattern {
     override fun getSequence(): List<ArendPattern>
 
     override fun getType(): ArendExpr?
