e6635fd5fd10c71e791c4bd5245e5a8359eb739a
Valis <valery.isaev@gmail.com>
2022-08-24 02:45:14 +0200

    Implement inlay for \where-variables

--- src/main/kotlin/org/arend/codeInsight/hints/ArendParametersInlayProvider.kt
+++ src/main/kotlin/org/arend/codeInsight/hints/ArendParametersInlayProvider.kt
@@ -11,6 +11,8 @@ import com.intellij.refactoring.suggested.endOffset
 import org.arend.ArendLanguage
 import org.arend.core.context.binding.LevelVariable
 import org.arend.core.context.binding.ParamLevelVariable
+import org.arend.core.context.param.DependentLink
+import org.arend.ext.prettyprinting.PrettyPrinterConfig
 import org.arend.naming.reference.TCDefReferable
 import org.arend.psi.ArendDefIdentifier
 import org.arend.psi.ArendDefinition
@@ -21,18 +23,26 @@ import org.arend.term.prettyprint.ToAbstractVisitor
 import javax.swing.JPanel
 
 @Suppress("UnstableApiUsage")
-class ArendLevelParametersInlayProvider : InlayHintsProvider<NoSettings> {
+class ArendParametersInlayProvider : InlayHintsProvider<NoSettings> {
     override val key: SettingsKey<NoSettings>
         get() = SettingsKey("arend.inlays")
 
     override val name
-        get() = "Level parameters"
+        get() = "Parameters"
 
     override val group
         get() = InlayGroup.TYPES_GROUP
 
     override val previewText
-        get() = "\\class C \\plevels p1 >= p2\n\n\\func test (c : C) => c"
+        get() = """
+            \class C \plevels p1 >= p2
+            
+            \func test (c : C) => c
+            
+            \func f (x : Nat) => x
+              \where
+                \func g => x
+        """.trimIndent()
 
     override val description
         get() = "Shows inferred level parameters"
@@ -48,26 +58,34 @@ class ArendLevelParametersInlayProvider : InlayHintsProvider<NoSettings> {
                 if (element !is ArendDefIdentifier) return true
                 val arendDef = element.parent as? ArendDefinition ?: return true
                 val def = (arendDef.tcReferable as? TCDefReferable)?.typechecked ?: return true
-                if (def.levelParameters == null || def.levelParameters.isEmpty()) return true
-
-                val builder = StringBuilder()
                 val levelParams = def.levelParameters
-                if (levelParams[0].type == LevelVariable.LvlType.PLVL && levelParams[0] is ParamLevelVariable && PsiTreeUtil.getChildOfType(arendDef, ArendPLevelParams::class.java) == null) {
-                    builder.append(" ")
-                    val ppv = PrettyPrintVisitor(builder, 0)
-                    ppv.prettyPrintLevelParameters(ToAbstractVisitor.visitLevelParameters(levelParams.subList(0, def.numberOfPLevelParameters), true), true)
+                if ((levelParams == null || levelParams.isEmpty()) && def.parametersOriginalDefinitions.isEmpty()) return true
+                val builder = StringBuilder()
+
+                if (levelParams != null && levelParams.isNotEmpty()) {
+                    if (levelParams[0].type == LevelVariable.LvlType.PLVL && levelParams[0] is ParamLevelVariable && PsiTreeUtil.getChildOfType(arendDef, ArendPLevelParams::class.java) == null) {
+                        builder.append(" ")
+                        val ppv = PrettyPrintVisitor(builder, 0)
+                        ppv.prettyPrintLevelParameters(ToAbstractVisitor.visitLevelParameters(levelParams.subList(0, def.numberOfPLevelParameters), true), true)
+                    }
+                    val lastVar = levelParams[levelParams.size - 1]
+                    if (lastVar.type == LevelVariable.LvlType.HLVL && lastVar is ParamLevelVariable && PsiTreeUtil.getChildOfType(arendDef, ArendHLevelParams::class.java) == null) {
+                        builder.append(" ")
+                        val ppv = PrettyPrintVisitor(builder, 0)
+                        ppv.prettyPrintLevelParameters(ToAbstractVisitor.visitLevelParameters(levelParams.subList(def.numberOfPLevelParameters, levelParams.size), false), false)
+                    }
                 }
-                val lastVar = levelParams[levelParams.size - 1]
-                if (lastVar.type == LevelVariable.LvlType.HLVL && lastVar is ParamLevelVariable && PsiTreeUtil.getChildOfType(arendDef, ArendHLevelParams::class.java) == null) {
+
+                if (def.parametersOriginalDefinitions.isNotEmpty()) {
                     builder.append(" ")
                     val ppv = PrettyPrintVisitor(builder, 0)
-                    ppv.prettyPrintLevelParameters(ToAbstractVisitor.visitLevelParameters(levelParams.subList(def.numberOfPLevelParameters, levelParams.size), false), false)
+                    ppv.prettyPrintParameters(ToAbstractVisitor.convert(DependentLink.Helper.take(def.parameters, def.parametersOriginalDefinitions.size), PrettyPrinterConfig.DEFAULT))
                 }
 
                 val str = builder.toString()
                 if (str.isEmpty()) return true
                 sink.addInlineElement(element.endOffset, true, MenuOnClickPresentation(factory.text(str), project) {
-                    val provider = this@ArendLevelParametersInlayProvider
+                    val provider = this@ArendParametersInlayProvider
                     listOf(InlayProviderDisablingAction(provider.name, file.language, project, provider.key))
                 }, false)
                 return true
