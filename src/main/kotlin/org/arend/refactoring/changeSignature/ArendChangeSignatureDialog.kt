2d52a648ccf9bf4fb1f46e02c415ae20863171b2
sxhya <sinchukss@gmail.com>
2022-11-18 19:14:27 +0100

    Run highlighting pass for all parameter code fragments on dialog creation

--- src/main/kotlin/org/arend/refactoring/changeSignature/ArendChangeSignatureDialog.kt
+++ src/main/kotlin/org/arend/refactoring/changeSignature/ArendChangeSignatureDialog.kt
@@ -1,14 +1,21 @@
 package org.arend.refactoring.changeSignature
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.psi.*
 import com.intellij.refactoring.BaseRefactoringProcessor
 import com.intellij.refactoring.changeSignature.ChangeSignatureDialogBase
+import com.intellij.refactoring.ui.CodeFragmentTableCellEditorBase
 import com.intellij.refactoring.ui.ComboBoxVisibilityPanel
+import com.intellij.ui.EditorTextField
 import com.intellij.ui.treeStructure.Tree
 import com.intellij.util.Consumer
 import org.arend.ArendFileType
 import org.arend.psi.ext.*
+import java.util.Collections.singletonList
+import javax.swing.JPanel
 
 class ArendChangeSignatureDialog(project: Project, val descriptor: ArendChangeSignatureDescriptor) :
     ChangeSignatureDialogBase<ArendParameterInfo, PsiElement, String, ArendChangeSignatureDescriptor, ArendChangeSignatureDialogParameterTableModelItem, ArendParameterTableModel>(project, descriptor, false, descriptor.method.context) {
@@ -44,4 +51,23 @@ class ArendChangeSignatureDialog(project: Project, val descriptor: ArendChangeSi
 
 
     override fun createVisibilityControl() = object : ComboBoxVisibilityPanel<String>("", arrayOf()) {}
+
+    override fun createParametersPanel(hasTabsInDialog: Boolean): JPanel {
+        val result = super.createParametersPanel(hasTabsInDialog)
+        val codeAnalyzer = DaemonCodeAnalyzer.getInstance(project)
+        if (codeAnalyzer is DaemonCodeAnalyzerImpl) {
+            for ((index, item) in myParametersTableModel.items.withIndex()) {
+                val fragment = item.typeCodeFragment
+                val document = PsiDocumentManager.getInstance(project).getDocument(fragment)
+                val cellEditor = this.myParametersTable.getCellEditor(index, 1) as? CodeFragmentTableCellEditorBase
+                if (cellEditor != null && document != null) {
+                    val textFieldEditor = cellEditor.getTableCellEditorComponent(myParametersTable, fragment, false, 0, 0) as? EditorTextField
+                    textFieldEditor?.addNotify()
+                    val textFieldTextEditor = textFieldEditor?.editor?.let{ TextEditorProvider.getInstance().getTextEditor(it) }
+                    if (textFieldTextEditor != null) codeAnalyzer.runPasses(fragment, document, singletonList(textFieldTextEditor), IntArray(0), true, null)
+                }
+            }
+        }
+        return result
+    }
 }
--- src/main/kotlin/org/arend/refactoring/changeSignature/ArendChangeSignatureDialogParameterTableModelItem.kt
+++ src/main/kotlin/org/arend/refactoring/changeSignature/ArendChangeSignatureDialogParameterTableModelItem.kt
@@ -2,10 +2,10 @@ package org.arend.refactoring.changeSignature
 
 import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase
 
-class ArendChangeSignatureDialogParameterTableModelItem(val resultParameterInfo: ArendParameterInfo, val typeCodeFragment: ArendChangeSignatureDialogCodeFragment):
+class ArendChangeSignatureDialogParameterTableModelItem(val resultParameterInfo: ArendParameterInfo, typeCodeFragment: ArendChangeSignatureDialogCodeFragment):
     ParameterTableModelItemBase<ArendParameterInfo>(resultParameterInfo, typeCodeFragment, null) {
     val associatedReferable = ArendChangeSignatureDialogParameter(this)
-    val dependencies = HashSet<ArendChangeSignatureDialogParameterTableModelItem>()
+    val dependencies = LinkedHashSet<ArendChangeSignatureDialogParameterTableModelItem>()
 
     override fun isEllipsisType(): Boolean = false
 }
