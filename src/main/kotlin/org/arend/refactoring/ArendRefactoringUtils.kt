2f25b68fb8bafd7ac9eb7a2fa9a958f38e7835a2
valis <valery.isaev@gmail.com>
2022-12-28 21:27:06 +0100

    Fix local namespaces

--- src/main/kotlin/org/arend/refactoring/ArendRefactoringUtils.kt
+++ src/main/kotlin/org/arend/refactoring/ArendRefactoringUtils.kt
@@ -26,7 +26,7 @@ import org.arend.naming.reference.Referable
 import org.arend.naming.renamer.StringRenamer
 import org.arend.naming.resolving.visitor.ExpressionResolveNameVisitor
 import org.arend.naming.scope.local.ElimScope
-import org.arend.naming.scope.local.ListScope
+import org.arend.naming.scope.local.LocalListScope
 import org.arend.prelude.Prelude
 import org.arend.psi.*
 import org.arend.psi.ArendElementTypes.*
@@ -729,7 +729,7 @@ fun collectDefinedVariables(startElement: ArendCompositeElement): List<Variable>
 
     val scope = when {
         excluded.isNotEmpty() -> ElimScope(elementScope, excluded)
-        added.isNotEmpty() -> ListScope(elementScope, added)
+        added.isNotEmpty() -> LocalListScope(elementScope, added)
         else -> elementScope
     }
     return scope.elements.mapNotNull { if (it is GlobalReferable) null else VariableImpl(it.refName) }
