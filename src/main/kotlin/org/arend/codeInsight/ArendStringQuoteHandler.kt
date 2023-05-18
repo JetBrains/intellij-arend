9be9b5a8b778a7ba21614402631e9e5ee2e2d316
sxhya <sxh@ya.ru>
2023-05-18 20:02:48 +0200

    Fix quotes

--- src/main/kotlin/org/arend/codeInsight/ArendStringQuoteHandler.kt
+++ src/main/kotlin/org/arend/codeInsight/ArendStringQuoteHandler.kt
@@ -4,4 +4,4 @@ import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
 import com.intellij.psi.TokenType
 import org.arend.psi.ArendElementTypes
 
-class ArendStringQuoteHandler : SimpleTokenSetQuoteHandler(ArendElementTypes.STRING, TokenType.BAD_CHARACTER)
+class ArendStringQuoteHandler : SimpleTokenSetQuoteHandler(ArendElementTypes.STRING, ArendElementTypes.QNAME, TokenType.BAD_CHARACTER)
