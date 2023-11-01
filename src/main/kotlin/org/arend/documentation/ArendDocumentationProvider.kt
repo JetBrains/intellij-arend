60eddc40137fe0f15ad3844bc8bb5aa3fa552e32
Aleksei.Luchinin <alex999990009@gmail.com>
2023-11-01 17:02:55 +0200

    Fix #343

---
+++ src/main/kotlin/org/arend/documentation/ArendDocumentationProvider.kt
@@ -0,0 +1,382 @@
+package org.arend.documentation
+
+import com.intellij.lang.documentation.AbstractDocumentationProvider
+import com.intellij.lang.documentation.DocumentationMarkup.*
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.TokenType.WHITE_SPACE
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.util.elementType
+import org.arend.ext.module.LongName
+import org.arend.naming.reference.FieldReferable
+import org.arend.naming.reference.RedirectingReferable
+import org.arend.naming.scope.Scope
+import org.arend.parser.ParserMixin.*
+import org.arend.psi.ArendFile
+import org.arend.psi.ArendKeyword
+import org.arend.psi.ArendKeyword.*
+import org.arend.psi.ArendKeyword.Companion.isArendKeyword
+import org.arend.psi.ArendKeyword.Companion.toArendKeyword
+import org.arend.psi.childrenWithLeaves
+import org.arend.psi.doc.ArendDocCodeBlock
+import org.arend.psi.doc.ArendDocComment
+import org.arend.psi.doc.ArendDocReference
+import org.arend.psi.ext.*
+import org.arend.term.abs.Abstract
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.net.UnknownHostException
+
+
+class ArendDocumentationProvider : AbstractDocumentationProvider() {
+
+    private data class ArendKeywordHtmlSection(val id: String, val index: Int)
+
+    private inner class ArendKeywordHtml(val chapter: String, val folder: String?) {
+        var isUnknownHostException = false
+        private fun initParagraphs(doc: Document?) = doc?.select("section")?.first()?.children() ?: emptyList()
+
+        private fun initSections(paragraphs: List<Element>) = paragraphs.mapIndexedNotNull { index: Int, element: Element? ->
+            if (element?.id().isNullOrEmpty()) {
+                null
+            } else {
+                ArendKeywordHtmlSection(element?.id()!!, index)
+            }
+        }
+
+        var paragraphs = initParagraphs(
+            try {
+                Jsoup.connect(AREND_DOCUMENTATION_BASE_PATH + chapter + (folder ?: "")).get()
+            } catch (e: UnknownHostException) {
+                isUnknownHostException = true
+                null
+            } catch (e: Throwable) {
+                null
+            }
+        )
+        var sections = initSections(paragraphs)
+
+        fun updateConnection() {
+            try {
+                Jsoup.connect(AREND_DOCUMENTATION_BASE_PATH + chapter + (folder ?: "")).get()
+            } catch (e: Throwable) {
+                null
+            }?.let {
+                isUnknownHostException = false
+                paragraphs = initParagraphs(it)
+                sections = initSections(paragraphs)
+            }
+
+        }
+    }
+
+    private val functionHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "functions")
+    private val moduleHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "modules")
+    private val dataHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "data")
+    private val typesHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "types")
+    private val classesHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "classes")
+    private val recordsHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "records")
+    private val metasHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "metas")
+    private val parametersHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "parameters")
+    private val definitionsHtml = ArendKeywordHtml(DEFINITION_CHAPTER, null)
+    private val coercionHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "coercion")
+    private val levelHtml = ArendKeywordHtml(DEFINITION_CHAPTER, "level")
+    private val universesHtml = ArendKeywordHtml(EXPRESSION_CHAPTER, "universes")
+    private val classExtHtml = ArendKeywordHtml(EXPRESSION_CHAPTER, "class-ext")
+    private val piHtml = ArendKeywordHtml(EXPRESSION_CHAPTER, "pi")
+    private val sigmaHtml = ArendKeywordHtml(EXPRESSION_CHAPTER, "sigma")
+    private val letHtml = ArendKeywordHtml(EXPRESSION_CHAPTER, "let")
+    private val caseHtml = ArendKeywordHtml(EXPRESSION_CHAPTER, "case")
+
+    override fun getQuickNavigateInfo(element: PsiElement, originalElement: PsiElement?) = generateDoc(element, originalElement, false)
+
+    override fun generateDoc(element: PsiElement, originalElement: PsiElement?) = generateDoc(element, originalElement, true)
+
+    override fun getDocumentationElementForLink(psiManager: PsiManager?, link: String, context: PsiElement?): PsiElement? {
+        val longName = link.removePrefix(FULL_PREFIX)
+        val scope = ArendDocComment.getScope((context as? ArendDocComment)?.owner ?: context) ?: return null
+        val ref = RedirectingReferable.getOriginalReferable(Scope.resolveName(scope, LongName.fromString(longName).toList()))
+        return if (ref is PsiReferable && longName.length != link.length) ref.documentation else ref as? PsiElement
+    }
+
+    override fun getCustomDocumentationElement(editor: Editor, file: PsiFile, contextElement: PsiElement?): PsiElement? {
+        if (contextElement?.isArendKeyword() == true) {
+            return contextElement
+        }
+        return null
+    }
+
+    private fun getStartAndFinishSection(html: ArendKeywordHtml?, arendKeyword: ArendKeyword?): List<Element> {
+        val sectionName = arendKeyword?.section?.sectionName ?: return emptyList()
+        val paragraphs = html?.paragraphs ?: return emptyList()
+        val sections = html.sections
+
+        val indexOfLemmaSection = sections.indexOf(sections.find { it.id == sectionName })
+        val sectionStart = sections[indexOfLemmaSection].index + 1
+        val sectionFinish = if (indexOfLemmaSection == sections.lastIndex) {
+            paragraphs.size
+        } else {
+            sections[indexOfLemmaSection + 1].index
+        }
+        return paragraphs.subList(sectionStart, sectionFinish)
+    }
+
+    private fun getArendKeywordHtml(arendKeyword: ArendKeyword?) =
+        when (arendKeyword) {
+            OPEN, HIDING, AS, USING, IMPORT, MODULE, WHERE -> moduleHtml
+            TRUNCATED, DATA, CONS -> dataHtml
+            FUNC, LEMMA, AXIOM, SFUNC, EVAL, PEVAL, WITH, ELIM, COWITH -> functionHtml
+            TYPE -> typesHtml
+            CLASS, CLASSIFYING, NO_CLASSIFYING, INSTANCE -> classesHtml
+            RECORD, FIELD, PROPERTY, OVERRIDE, DEFAULT, EXTENDS, THIS -> recordsHtml
+            META -> metasHtml
+            STRICT -> parametersHtml
+            ALIAS, INFIX, INFIX_LEFT, INFIX_RIGHT, FIX, FIX_LEFT, FIX_RIGHT -> definitionsHtml
+            USE, COERCE -> coercionHtml
+            LEVEL -> levelHtml
+            LEVELS, PLEVELS, HLEVELS, LP, LH, SUC, MAX, OO, PROP, SET, UNIVERSE, TRUNCATED_UNIVERSE -> universesHtml
+            NEW -> classExtHtml
+            PI, LAM -> piHtml
+            SIGMA -> sigmaHtml
+            LET, LETS, HAVE, HAVES, IN -> letHtml
+            CASE, SCASE, RETURN -> caseHtml
+            BOX, PRIVATE, PROTECTED, null -> null
+        }
+
+    private fun StringBuilder.addLink(arendKeywordHtml: ArendKeywordHtml?, arendKeyword: ArendKeyword?) {
+        append("See the documentation: <a href=\"${AREND_DOCUMENTATION_BASE_PATH + (arendKeywordHtml?.chapter ?: "") + (arendKeywordHtml?.folder ?: "") 
+                + (arendKeyword?.section?.sectionName?.let { "#$it" } ?: "")}\">${arendKeyword?.type?.debugName}</a>")
+    }
+
+    private fun StringBuilder.getDescriptionForKeyword(psiElement: PsiElement) {
+        val arendKeyword = psiElement.toArendKeyword()
+        val arendKeywordHtml = getArendKeywordHtml(arendKeyword)
+        val paragraphs = arendKeywordHtml?.paragraphs
+        val sections = arendKeywordHtml?.sections
+
+        val sectionElements = if (arendKeywordHtml?.isUnknownHostException == true) {
+            emptyList()
+        } else {
+            when (arendKeyword) {
+                DATA, TYPE, CLASS, RECORD, META, FIELD, USE, COERCE, PI, SIGMA, LAM, LET, IN, CASE, RETURN, PROP, SET, UNIVERSE, TRUNCATED_UNIVERSE ->
+                    paragraphs?.subList(AREND_SECTION_START, sections?.firstOrNull()?.index ?: paragraphs.size)
+                FUNC -> paragraphs?.subList(AREND_SECTION_START, sections?.firstOrNull()?.index?.minus(1) ?: paragraphs.size)
+                LEVEL, NEW -> paragraphs?.subList(AREND_SECTION_START, paragraphs.size)
+
+                else -> getStartAndFinishSection(arendKeywordHtml, arendKeyword)
+            } ?: emptyList()
+        }
+
+        for (element in sectionElements) {
+            processElement(psiElement.project, element, arendKeywordHtml?.chapter, arendKeywordHtml?.folder)
+        }
+        if (arendKeywordHtml?.isUnknownHostException == true) {
+            append("There is no internet connection to get the documentation")
+            arendKeywordHtml.updateConnection()
+        }
+        when (arendKeyword) {
+            BOX, PRIVATE, PROTECTED, null -> return
+            else -> {
+                wrapTag("hr") {
+                    addLink(arendKeywordHtml, arendKeyword)
+                }
+            }
+        }
+    }
+
+    private fun generateDocForKeywords(element: PsiElement): String {
+        return buildString { wrapTag("html") {
+            wrapTag("head") {
+                wrapTag("style") {
+                    append(".normal_text { white_space: nowrap; }.code { white_space: pre; }")
+                }
+            }
+            wrapTag("body") {
+                wrap(DEFINITION_START, DEFINITION_END) {
+                    wrapTag("b") {
+                        html(element.text)
+                    }
+                }
+
+                wrap(CONTENT_START, CONTENT_END) {
+                    getDescriptionForKeyword(element)
+                }
+            }
+        } }
+    }
+
+    private fun generateDoc(element: PsiElement, originalElement: PsiElement?, withDocComments: Boolean): String? {
+        val ref = element as? PsiReferable ?: (element as? ArendDocComment)?.owner
+        ?: return if (element.isArendKeyword()) generateDocForKeywords(element) else null
+        return buildString { wrapTag("html") {
+            wrapTag("head") {
+                wrapTag("style") {
+                    append(".normal_text { white_space: nowrap; }.code { white_space: pre; }")
+                }
+            }
+
+            wrapTag("body") {
+                wrap(DEFINITION_START, DEFINITION_END) {
+                    generateDefinition(ref)
+                }
+
+                wrap(CONTENT_START, CONTENT_END) {
+                    generateContent(ref, originalElement)
+                }
+
+                if (withDocComments) {
+                    val doc = element as? ArendDocComment ?: ref.documentation
+                    if (doc != null) {
+                        append(CONTENT_START)
+                        generateDocComments(ref, doc, element is ArendDocComment)
+                        append(CONTENT_END)
+                    }
+                }
+            } } }
+    }
+
+    private fun StringBuilder.generateDocComments(element: PsiReferable, doc: PsiElement, full: Boolean) {
+        for (docElement in doc.childrenWithLeaves) {
+            val elementType = (docElement as? LeafPsiElement)?.elementType
+            when {
+                elementType == DOC_LBRACKET -> append("[")
+                elementType == DOC_RBRACKET -> append("]")
+                elementType == DOC_TEXT -> html(docElement.text)
+                elementType == WHITE_SPACE -> append(" ")
+                elementType == DOC_CODE -> append("<code>${docElement.text.htmlEscape()}</code>")
+                elementType == DOC_PARAGRAPH_SEP -> {
+                    append("<br>")
+                    if (!full) {
+                        append("<a href=\"psi_element://$FULL_PREFIX${element.refName}\">more...</a>")
+                        return
+                    }
+                }
+                docElement is ArendDocReference -> {
+                    val longName = docElement.longName
+                    val link = longName.refIdentifierList.joinToString(".") { it.id.text.htmlEscape() }
+                    val isLink = longName.resolve is PsiReferable
+                    if (isLink) {
+                        append("<a href=\"psi_element://$link\">")
+                    }
+
+                    append("<code>")
+                    val text = docElement.docReferenceText
+                    if (text != null) {
+                        for (child in text.childrenWithLeaves) {
+                            if (child.elementType == DOC_TEXT) html(child.text)
+                        }
+                    } else {
+                        append(link)
+                    }
+                    append("</code>")
+
+                    if (isLink) {
+                        append("</a>")
+                    }
+                }
+                docElement is ArendDocCodeBlock -> {
+                    append("<pre>")
+                    for (child in docElement.childrenWithLeaves) {
+                        when ((child as? LeafPsiElement)?.elementType) {
+                            DOC_CODE_LINE -> html(child.text)
+                            WHITE_SPACE -> append("\n")
+                        }
+                    }
+                    append("</pre>")
+                }
+            }
+        }
+    }
+
+    private fun StringBuilder.generateDefinition(element: PsiReferable) {
+        wrapTag("b") {
+            html(element.textRepresentation())
+        }
+        (element as? ReferableBase<*>)?.alias?.aliasIdentifier?.id?.text?.let {
+            html(" $it")
+        }
+
+        for (parameter in (element as? Abstract.ParametersHolder)?.parameters ?: emptyList()) {
+            if (parameter is PsiElement) {
+                html(" ${parameter.text}")
+            }
+        }
+
+        element.psiElementType?.let { html(" : ${it.text}") }
+    }
+
+    private fun StringBuilder.generatePrecedence(prec: ArendPrec) {
+        append(", ")
+        append(prec.firstChild.text.drop(1))
+        prec.number?.let { priority ->
+            append(" ")
+            append(priority.text)
+        }
+    }
+
+    private fun StringBuilder.generateContent(element: PsiElement, originalElement: PsiElement?) {
+        wrapTag("em") {
+            getType(element)?.let { append(it) }
+        }
+        getSuperType(element)?.let { append(it) }
+
+        (element as? ReferableBase<*>)?.prec?.let { generatePrecedence(it) }
+
+        (element as? ReferableBase<*>)?.alias?.let { alias ->
+            alias.prec?.let {
+                generatePrecedence(it)
+                html(" ${alias.aliasIdentifier?.id?.text ?: ""}")
+            }
+        }
+
+        if (element !is PsiFile) {
+            getSourceFileName(element, originalElement)?.let {
+                val fileName = it.htmlEscape()
+                append(", defined in ")
+                wrapTag("b") {
+                    append(fileName)
+                }
+            }
+        }
+    }
+
+    private fun getType(element: PsiElement): String? = when (element) {
+        is ArendDefClass -> if (element.isRecord) "record" else "class"
+        is FieldReferable -> "field"
+        is ArendDefInstance -> "instance"
+        is ArendClassImplement -> "implementation"
+        is ArendDefData -> "data"
+        is ArendConstructor -> "constructor"
+        is ArendDefFunction, is ArendCoClauseDef -> "function"
+        is ArendDefModule -> "module"
+        is ArendDefMeta -> "meta"
+        is ArendLetClause -> "let"
+        is ArendDefIdentifier -> if (element.parent is ArendLetClause) "let" else "variable"
+        is ArendLevelIdentifier -> "level"
+        is PsiFile -> "file"
+        else -> null
+    }
+
+    private fun getSuperType(element: PsiElement): String? = when (element) {
+        is FieldReferable ->
+            (element.locatedReferableParent as? ArendDefClass)?.let {
+                " of ${if (it.isRecord) "record" else "class"} <b>${it.name ?: return@let null}</b>"
+            }
+        is ArendConstructor -> (element.locatedReferableParent as? ArendDefData)?.name?.let { " of data <b> $it </b>" }
+        else -> null
+    }
+
+    private fun getSourceFileName(element: PsiElement, originalElement: PsiElement?): String? {
+        if (element is PsiLocatedReferable) {
+            val file = element.containingFile.originalFile
+            if (file != originalElement?.containingFile?.originalFile) {
+                return (file as? ArendFile)?.fullName ?: file.name
+            }
+        }
+
+        return null
+    }
+}
--- src/main/kotlin/org/arend/psi/ArendTokenType.kt
+++ src/main/kotlin/org/arend/psi/ArendTokenType.kt
@@ -1,26 +1,140 @@
 package org.arend.psi
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.util.elementType
 import org.arend.ArendLanguage
 import org.arend.parser.ParserMixin.DOC_COMMENT
 import org.arend.parser.ParserMixin.DOC_TEXT
 import org.arend.psi.ArendElementTypes.*
+import org.arend.psi.ArendKeywordSection.*
 
 class ArendTokenType(debugName: String) : IElementType(debugName, ArendLanguage.INSTANCE)
 
-val AREND_KEYWORDS: TokenSet = TokenSet.create(
-        OPEN_KW, IMPORT_KW, USING_KW, AS_KW, HIDING_KW, FUNC_KW, SFUNC_KW, LEMMA_KW, TYPE_KW, CONS_KW, AXIOM_KW,
-        STRICT_KW, CLASSIFYING_KW, NO_CLASSIFYING_KW, FIELD_KW, PROPERTY_KW, DEFAULT_KW, OVERRIDE_KW,
-        NON_ASSOC_KW, LEFT_ASSOC_KW, RIGHT_ASSOC_KW, INFIX_NON_KW, INFIX_LEFT_KW, INFIX_RIGHT_KW, ALIAS_KW,
-        PROP_KW, WHERE_KW, WITH_KW, USE_KW, COWITH_KW, ELIM_KW, NEW_KW, PI_KW, SIGMA_KW, LAM_KW, HAVE_KW, HAVES_KW, LET_KW, LETS_KW,
-        IN_KW, CASE_KW, SCASE_KW, DATA_KW, CLASS_KW, RECORD_KW, MODULE_KW, META_KW, EXTENDS_KW,
-        RETURN_KW, COERCE_KW, INSTANCE_KW, TRUNCATED_KW,
-        LP_KW, LH_KW, OO_KW, SUC_KW, MAX_KW, LEVEL_KW, LEVELS_KW, PLEVELS_KW, HLEVELS_KW,
-        SET, UNIVERSE, TRUNCATED_UNIVERSE, THIS_KW,
-        EVAL_KW, PEVAL_KW, BOX_KW, PRIVATE_KW, PROTECTED_KW
-)
+enum class ArendKeywordSection(val sectionName: String) {
+    OPEN_SECTION("open-commands"),
+    IMPORT_SECTION("import-commands"),
+    TRUNCATED_SECTION("truncation"),
+    CONS_SECTION("constructor-synonyms"),
+    LEMMA_SECTION("lemmas"),
+    SFUNC_SECTION("sfunc"),
+    CLASSIFYING_SECTION("classifying-fields"),
+    STRICT_SECTION("strict-parameters"),
+    ALIAS_SECTION("aliases"),
+    PROPERTY_SECTION("properties"),
+    OVERRIDE_SECTION("override"),
+    DEFAULT_SECTION("default"),
+    EXTENDS_SECTION("extensions"),
+    MODULE_SECTION("modules"),
+    INSTANCES_SECTION("instances"),
+    LEVELS_SECTION("level-polymorphism"),
+    PLEVELS_SECTION("level-parameters"),
+    WITH_SECTION("pattern-matching"),
+    ELIM_SECTION("elim"),
+    COWITH_SECTION("copattern-matching"),
+    WHERE_SECTION("where-blocks"),
+    INFIX_SECTION("infix-operators"),
+    FIX_SECTION("precedence"),
+    THIS_SECTION("this"),
+    LETS_SECTION("strict-let-expressions"),
+    HAVE_SECTION("have"),
+    SCASE_SECTION("scase"),
+    LP_SECTION("level-polymorphism")
+}
+
+enum class ArendKeyword(val type: IElementType, val section: ArendKeywordSection?) {
+    OPEN(OPEN_KW, OPEN_SECTION),
+    IMPORT(IMPORT_KW, IMPORT_SECTION),
+    HIDING(HIDING_KW, OPEN_SECTION),
+    AS(AS_KW, OPEN_SECTION),
+    USING(USING_KW, OPEN_SECTION),
+    TRUNCATED(TRUNCATED_KW, TRUNCATED_SECTION),
+    DATA(DATA_KW, null),
+    CONS(CONS_KW, CONS_SECTION),
+    FUNC(FUNC_KW, null),
+    LEMMA(LEMMA_KW, LEMMA_SECTION),
+    AXIOM(AXIOM_KW, LEMMA_SECTION),
+    SFUNC(SFUNC_KW, SFUNC_SECTION),
+    TYPE(TYPE_KW, null),
+    CLASS(CLASS_KW, null),
+    RECORD(RECORD_KW, null),
+    META(META_KW, null),
+    CLASSIFYING(CLASSIFYING_KW, CLASSIFYING_SECTION),
+    NO_CLASSIFYING(NO_CLASSIFYING_KW, CLASSIFYING_SECTION),
+    STRICT(STRICT_KW, STRICT_SECTION),
+    ALIAS(ALIAS_KW, ALIAS_SECTION),
+    FIELD(FIELD_KW, null),
+    PROPERTY(PROPERTY_KW, PROPERTY_SECTION),
+    OVERRIDE(OVERRIDE_KW, OVERRIDE_SECTION),
+    DEFAULT(DEFAULT_KW, DEFAULT_SECTION),
+    EXTENDS(EXTENDS_KW, EXTENDS_SECTION),
+    MODULE(MODULE_KW, MODULE_SECTION),
+    INSTANCE(INSTANCE_KW, INSTANCES_SECTION),
+    USE(USE_KW, null),
+    COERCE(COERCE_KW, null),
+    LEVEL(LEVEL_KW, null),
+    LEVELS(LEVELS_KW, LEVELS_SECTION),
+    PLEVELS(PLEVELS_KW, PLEVELS_SECTION),
+    HLEVELS(HLEVELS_KW, PLEVELS_SECTION),
+    BOX(BOX_KW, null),
+    EVAL(EVAL_KW, SFUNC_SECTION),
+    PEVAL(PEVAL_KW, SFUNC_SECTION),
+    WITH(WITH_KW, WITH_SECTION),
+    ELIM(ELIM_KW, ELIM_SECTION),
+    COWITH(COWITH_KW, COWITH_SECTION),
+    WHERE(WHERE_KW, WHERE_SECTION),
+    INFIX(INFIX_NON_KW, INFIX_SECTION),
+    INFIX_LEFT(INFIX_LEFT_KW, INFIX_SECTION),
+    INFIX_RIGHT(INFIX_RIGHT_KW, INFIX_SECTION),
+    FIX(NON_ASSOC_KW, FIX_SECTION),
+    FIX_LEFT(LEFT_ASSOC_KW, FIX_SECTION),
+    FIX_RIGHT(RIGHT_ASSOC_KW, FIX_SECTION),
+    NEW(NEW_KW, null),
+    THIS(THIS_KW, THIS_SECTION),
+    PI(PI_KW, null),
+    SIGMA(SIGMA_KW, null),
+    LAM(LAM_KW, null),
+    LET(LET_KW, null),
+    LETS(LETS_KW, LETS_SECTION),
+    HAVE(HAVE_KW, HAVE_SECTION),
+    HAVES(HAVES_KW, HAVE_SECTION),
+    IN(IN_KW, null),
+    CASE(CASE_KW, null),
+    SCASE(SCASE_KW, SCASE_SECTION),
+    RETURN(RETURN_KW, null),
+    LP(LP_KW, LP_SECTION),
+    LH(LH_KW, LP_SECTION),
+    SUC(SUC_KW, LP_SECTION),
+    MAX(MAX_KW, LP_SECTION),
+    OO(OO_KW, LP_SECTION),
+    PROP(PROP_KW, null),
+    SET(ArendElementTypes.SET, null),
+    UNIVERSE(ArendElementTypes.UNIVERSE, null),
+    TRUNCATED_UNIVERSE(ArendElementTypes.TRUNCATED_UNIVERSE, null),
+    PRIVATE(PRIVATE_KW, null),
+    PROTECTED(PROTECTED_KW, null)
+    ;
+
+    companion object {
+        private val keywordTypes = values().associateBy { it.type }
+
+        fun PsiElement?.toArendKeyword() = keywordTypes[this.elementType]
+
+        fun PsiElement?.isArendKeyword(): Boolean = ArendKeyword.keywordTypes.contains(this.elementType)
+
+        val AREND_KEYWORDS = initTokenSet(values().map { it.type })
+    }
+}
+
+fun initTokenSet(collection: Collection<IElementType>): TokenSet {
+    var tokenSet = TokenSet.create()
+    collection.forEach {
+        tokenSet = TokenSet.orSet(tokenSet, TokenSet.create(it))
+    }
+    return tokenSet
+}
 
 val AREND_COMMENTS: TokenSet = TokenSet.create(LINE_COMMENT, BLOCK_COMMENT, DOC_COMMENT, DOC_TEXT)
 
