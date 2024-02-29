3751fca735ef1957f8b2a4ac094b1719e1e42ca3
valis <valery.isaev@gmail.com>
2024-02-29 23:48:39 +0300

    Create a subclass in CheckTypeVisitor

--- src/main/kotlin/org/arend/tracer/ArendTracingTypechecker.kt
+++ src/main/kotlin/org/arend/tracer/ArendTracingTypechecker.kt
@@ -1,19 +1,30 @@
 package org.arend.tracer
 
-import org.arend.core.expr.*
+import org.arend.core.context.binding.Binding
+import org.arend.core.expr.Expression
 import org.arend.ext.ArendExtension
 import org.arend.ext.error.ErrorReporter
+import org.arend.extImpl.userData.UserDataHolderImpl
+import org.arend.naming.reference.Referable
 import org.arend.term.concrete.Concrete
+import org.arend.typechecking.instance.pool.GlobalInstancePool
 import org.arend.typechecking.result.TypecheckingResult
 import org.arend.typechecking.visitor.CheckTypeVisitor
 
-class ArendTracingTypechecker(errorReporter: ErrorReporter, extension: ArendExtension?) :
-    CheckTypeVisitor(errorReporter, null, extension) {
+class ArendTracingTypechecker private constructor(localContext: MutableMap<Referable, Binding>?, errorReporter: ErrorReporter?, pool: GlobalInstancePool?, arendExtension: ArendExtension?, holder: UserDataHolderImpl?) :
+    CheckTypeVisitor(localContext, errorReporter, pool, arendExtension, holder) {
+
+    constructor(errorReporter: ErrorReporter, extension: ArendExtension?)
+        : this(LinkedHashMap<Referable, Binding>(), errorReporter, null, extension, null)
 
     private val traceEntries: MutableList<ArendTraceEntry> = mutableListOf()
     private val entriesStack: ArrayDeque<ArendTraceEntry> = ArrayDeque()
 
     val trace: ArendTrace = ArendTrace(traceEntries)
+
+    override fun copy(localContext: MutableMap<Referable, Binding>?, errorReporter: ErrorReporter?, pool: GlobalInstancePool?, arendExtension: ArendExtension?, holder: UserDataHolderImpl?): CheckTypeVisitor {
+        return super.copy(localContext, errorReporter, pool, arendExtension, holder)
+    }
 
     override fun checkExpr(expr: Concrete.Expression, expectedType: Expression?): TypecheckingResult? {
         val traceEntry = ArendTraceEntry(entriesStack.lastOrNull())
