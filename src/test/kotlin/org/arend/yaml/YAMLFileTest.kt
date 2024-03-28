ddf2d3acbb9ecc8fe002a1618fc6e863f9de8f1f
Aleksei.Luchinin <alex999990009@gmail.com>
2024-03-28 14:48:16 +0200

    Fix a bug with comparing a modified yaml file with a saved structure

---
+++ src/test/kotlin/org/arend/yaml/YAMLFileTest.kt
@@ -0,0 +1,51 @@
+package org.arend.yaml
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.components.service
+import org.arend.ArendTestBase
+import java.io.File
+
+class YAMLFileTest : ArendTestBase() {
+    override var dataPath = "org/arend/yaml"
+
+    fun testDir() {
+        File("$testDataPath/arend.yaml").writeText("""
+            langVersion: 1.9
+            sourcesDir: src
+            dependencies: [arend-lib]
+            binariesDir: bin<caret>
+        """.trimIndent())
+
+        val yamlFileService = project.service<YamlFileService>()
+        val yamlFile = myFixture.configureByFile("arend.yaml")
+
+        yamlFileService.removeChangedFile(yamlFile.virtualFile)
+        yamlFileService.updateIdea(yamlFile.virtualFile)
+        myFixture.type("/")
+
+        assertTrue(!yamlFileService.containsChangedFile(yamlFile.virtualFile))
+
+        myFixture.type("abc")
+        assertTrue(yamlFileService.containsChangedFile(yamlFile.virtualFile))
+    }
+
+    fun testList() {
+        File("$testDataPath/arend.yaml").writeText("""
+            langVersion: 1.9
+            sourcesDir: src
+            dependencies: []
+            binariesDir: bin
+        """.trimIndent())
+
+        val yamlFileService = project.service<YamlFileService>()
+        val yamlFile = myFixture.configureByFile("arend.yaml")
+
+        yamlFileService.removeChangedFile(yamlFile.virtualFile)
+        yamlFileService.updateIdea(yamlFile.virtualFile)
+        WriteCommandAction.runWriteCommandAction(project) {
+            myFixture.editor.document.deleteString(33, 50)
+        }
+
+        assertTrue(!yamlFileService.containsChangedFile(yamlFile.virtualFile))
+    }
+}
