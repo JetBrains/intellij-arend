f279061f15a4f6a9d010ff7f58fa6eb86cd84000
Aleksei.Luchinin <alex999990009@gmail.com>
2024-09-13 14:59:19 +0300

    Fix GenerateMissingClauses

--- src/main/kotlin/org/arend/intention/generating/GenerateMissingClausesUtils.kt
+++ src/main/kotlin/org/arend/intention/generating/GenerateMissingClausesUtils.kt
@@ -7,46 +7,18 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.SmartPointerManager
 import com.intellij.psi.util.elementType
-import com.intellij.refactoring.suggested.endOffset
-import com.intellij.refactoring.suggested.startOffset
+import com.intellij.psi.util.endOffset
+import com.intellij.psi.util.startOffset
 import org.arend.ext.error.MissingClausesError
 import org.arend.psi.ArendElementTypes
 import org.arend.psi.ArendFile
-import org.arend.psi.ArendPsiFactory
 import org.arend.psi.ext.*
 import org.arend.quickfix.ImplementMissingClausesQuickFix
 import org.arend.typechecking.ArendTypechecking
 import org.arend.typechecking.error.ErrorService
 
-internal fun checkMissingClauses(element: PsiElement, editor: Editor): Boolean {
-    if (element.elementType != ArendElementTypes.TGOAL) {
-        return false
-    }
-    var fileText = (element.containingFile as ArendFile).text
-
-    var parent: PsiElement? = element
-    while (parent !is ArendFunctionBody) {
-        parent = parent?.parent
-        if (parent == null) {
-            return false
-        }
-    }
-    fileText = fileText.removeRange(parent.startOffset, parent.endOffset)
-
-    val project = editor.project ?: return false
-    val psiFactory = ArendPsiFactory(project)
-    val newFile = psiFactory.createFromText(fileText) ?: return false
-
-    ArendTypechecking.create(project).typecheckModules(listOf(newFile), null)
-
-    val errorService = project.service<ErrorService>()
-    val error = errorService.getErrors(newFile).filter { it.error is MissingClausesError }.find {
-        it.definition?.endOffset?.plus(1) == parent.startOffset
-    }
-
-    errorService.clearNameResolverErrors(newFile)
-    errorService.clearTypecheckingErrors(newFile)
-    return error != null
+internal fun checkMissingClauses(element: PsiElement): Boolean {
+    return element.elementType == ArendElementTypes.TGOAL
 }
 
 internal fun deleteFunctionBody(element: PsiElement): Pair<ArendGroup, Int>? {
