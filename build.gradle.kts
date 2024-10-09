7a6608a2e44369e11c5baad3ef2928d6f9c971b2
sxhya <sinchukss@gmail.com>
2024-10-09 15:14:12 +0200

    Update java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
 }
 
 java {
-    // toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
 }
