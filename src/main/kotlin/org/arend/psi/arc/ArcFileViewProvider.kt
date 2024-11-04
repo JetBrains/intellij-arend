9a74463978282c2c034ce69e73c85cc10739d4fd
sxhya <sinchukss@gmail.com>
2024-11-04 20:36:56 +0100

    Use default ArendFile in FileViewProvider

---
+++ src/main/kotlin/org/arend/psi/arc/ArcFileViewProvider.kt
@@ -0,0 +1,26 @@
+package org.arend.psi.arc
+
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.findDocument
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.SingleRootFileViewProvider
+import org.arend.ArendLanguage
+import org.arend.psi.ArendFile
+
+class ArcFileViewProvider(manager: PsiManager, virtualFile: VirtualFile, eventSystemEnabled: Boolean = true) :
+    SingleRootFileViewProvider(manager, virtualFile, eventSystemEnabled, ArendLanguage.INSTANCE) {
+
+    override fun createFile(project: Project, file: VirtualFile, fileType: FileType): PsiFile? {
+        if (fileType is ArcFileType) {
+            return ArendFile(this)
+        }
+        return super.createFile(project, file, fileType)
+    }
+
+    override fun getContents(): CharSequence {
+        return virtualFile.findDocument()?.text ?: ""
+    }
+}
