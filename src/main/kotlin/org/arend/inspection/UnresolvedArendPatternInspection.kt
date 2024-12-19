649eb53114b0705d853e83b2a1b11e95619a28ae
Aleksei Luchinin <alex999990009@gmail.com>
2024-12-19 17:06:56 +0100

    Fix #388

--- src/main/kotlin/org/arend/inspection/UnresolvedArendPatternInspection.kt
+++ src/main/kotlin/org/arend/inspection/UnresolvedArendPatternInspection.kt
@@ -3,24 +3,54 @@ package org.arend.inspection
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.stubs.StubIndex
+import com.intellij.psi.util.childrenOfType
+import org.arend.psi.ArendFile
+import org.arend.psi.ArendFileScope
 import org.arend.psi.ext.*
+import org.arend.psi.stubs.index.ArendDefinitionIndex
+import org.arend.refactoring.isPrelude
+import org.arend.term.abs.Abstract
+import org.arend.term.group.Group
 import org.arend.util.ArendBundle
 
 class UnresolvedArendPatternInspection : ArendInspectionBase() {
     override fun buildArendVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : PsiElementVisitor() {
+
+            fun registerProblem(element: PsiElement) {
+                val message = ArendBundle.message("arend.inspection.unresolved.pattern", element.text)
+                holder.registerProblem(element, message)
+            }
+
             override fun visitElement(element: PsiElement) {
                 super.visitElement(element)
-                if (element is ArendPattern && element.parent !is ArendPattern) {
-                    val parent = element.parent
-                    if ((parent is ArendClause && parent.patterns.size > 1) || parent.parent is ArendFunctionClauses ||
-                        (parent.parent is ArendDataBody && (parent.parent as ArendDataBody).elim != null)) {
-                        return
-                    }
-                    val resolve = element.singleReferable?.reference?.resolve() ?: return
-                    if (resolve !is ArendConstructor && resolve !is ArendDefInstance) {
-                        val message = ArendBundle.message("arend.inspection.unresolved.pattern", element.text)
-                        holder.registerProblem(element, message)
+                if (element !is ArendPattern) {
+                    return
+                }
+
+                val identifier = element.singleReferable ?: return
+                val name = identifier.refName
+                val project = element.project
+                val constructors = StubIndex.getElements(ArendDefinitionIndex.KEY, name, project, ArendFileScope(project), PsiReferable::class.java).filterIsInstance<ArendConstructor>()
+                val resolve = identifier.reference?.resolve() ?: return
+
+                if (constructors.isNotEmpty() && (!constructors.contains(resolve) &&
+                    (resolve.containingFile as? ArendFile?)?.let { isPrelude(it) } == false)) {
+                    val patterns = element.parent?.childrenOfType<ArendPattern>()
+                    val firstPattern = patterns?.first()
+                    if (firstPattern != element) {
+                        val index = patterns?.indexOf(element)
+                        val firstPatternResolve = firstPattern?.singleReferable?.reference?.resolve()
+                        if (firstPatternResolve is Abstract.ParametersHolder) {
+                            val parameter = index?.let { firstPatternResolve.parameters[it - 1] }
+                            val typeConstructors = ((parameter?.type as? ArendLiteral?)?.longName?.resolve as? Group?)?.constructors ?: emptyList()
+                            if (typeConstructors.any { constructors.contains(it) }) {
+                                registerProblem(element)
+                            }
+                        }
+                    } else {
+                        registerProblem(element)
                     }
                 }
             }
--- src/main/kotlin/org/arend/psi/ArendFileScope.kt
+++ src/main/kotlin/org/arend/psi/ArendFileScope.kt
@@ -4,12 +4,16 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.ProjectAndLibrariesScope
+import org.arend.prelude.Prelude
 
 class ArendFileScope(project: Project): ProjectAndLibrariesScope(project) {
     override fun contains(file: VirtualFile): Boolean {
-        if (!super.contains(file)) return false
         val project = project ?: return false
-        val psiFile = PsiManager.getInstance(project).findFile(file)
-        return psiFile is ArendFile && psiFile.moduleLocation != null
+        val psiFile = PsiManager.getInstance(project).findFile(file) as? ArendFile ?: return false
+        if (psiFile.libraryName == Prelude.LIBRARY_NAME) {
+            return true
+        }
+        if (!super.contains(file)) return false
+        return psiFile.moduleLocation != null
     }
 }
