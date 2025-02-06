90f3a2f44224634dee6329557c2766b1d05a0714
valis <valery.isaev@gmail.com>
2025-02-06 19:12:23 +0300

    Do not highlight resolved modules

--- src/main/kotlin/org/arend/highlight/ArendHighlightingPassFactory.kt
+++ src/main/kotlin/org/arend/highlight/ArendHighlightingPassFactory.kt
@@ -1,5 +1,6 @@
 package org.arend.highlight
 
+import com.intellij.codeHighlighting.TextEditorHighlightingPass
 import com.intellij.codeHighlighting.TextEditorHighlightingPassFactoryRegistrar
 import com.intellij.codeHighlighting.TextEditorHighlightingPassRegistrar
 import com.intellij.openapi.components.service
@@ -7,6 +8,8 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import org.arend.IArendFile
+import org.arend.psi.ArendFile
+import org.arend.server.ArendServerService
 
 class ArendHighlightingPassFactory : BasePassFactory<IArendFile>(IArendFile::class.java), TextEditorHighlightingPassFactoryRegistrar {
     private var myPassId = -1
@@ -16,8 +19,15 @@ class ArendHighlightingPassFactory : BasePassFactory<IArendFile>(IArendFile::cla
         project.service<ArendPassFactoryService>().highlightingPassId = myPassId
     }
 
-    override fun createPass(file: IArendFile, editor: Editor, textRange: TextRange) =
-        ArendHighlightingPass(file, editor, textRange)
+    override fun createPass(file: IArendFile, editor: Editor, textRange: TextRange): TextEditorHighlightingPass {
+        val project = file.project
+        val module = (file as? ArendFile)?.moduleLocation
+        val data = if (module != null) project.service<ArendServerService>().server.getGroupData(module) else null
+        return if (data == null || data.timestamp != file.modificationStamp || !data.isResolved)
+            ArendHighlightingPass(file, editor, textRange)
+        else
+            EmptyHighlightingPass(project, editor.document)
+    }
 
     override fun getPassId() = myPassId
 }
--- src/main/kotlin/org/arend/highlight/BasePassFactory.kt
+++ src/main/kotlin/org/arend/highlight/BasePassFactory.kt
@@ -5,10 +5,7 @@ import com.intellij.codeHighlighting.TextEditorHighlightingPass
 import com.intellij.codeInsight.daemon.impl.FileStatusMap
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiWhiteSpace
-import com.intellij.psi.util.PsiUtil
 
 abstract class BasePassFactory<T : PsiFile>(private val clazz: Class<T>) : DirtyScopeTrackingHighlightingPassFactory {
     abstract fun createPass(file: T, editor: Editor, textRange: TextRange): TextEditorHighlightingPass?
@@ -21,15 +18,6 @@ abstract class BasePassFactory<T : PsiFile>(private val clazz: Class<T>) : Dirty
         }
 
         val textRange = FileStatusMap.getDirtyTextRange(editor.document, file, passId)
-        return if (textRange == null) {
-            EmptyHighlightingPass(file.project, editor.document)
-        } else {
-            val psi = PsiUtil.getElementInclusiveRange(file, textRange)
-            if (psi !is PsiComment && (psi !is PsiWhiteSpace || allowWhiteSpaces())) {
-                createPass(clazz.cast(file), editor, textRange)
-            } else {
-                EmptyHighlightingPass(file.project, editor.document)
-            }
-        }
+        return if (textRange != null) createPass(clazz.cast(file), editor, textRange) else EmptyHighlightingPass(file.project, editor.document)
     }
 }
